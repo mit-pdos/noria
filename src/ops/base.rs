5c0876f564516a8ebff142e602af417573d7dd89
Jon Gjengset <jon@thesquareplanet.com>
2016-06-15 13:00:07 -0400

    Nicer interface for creating a Node

--- src/ops/base.rs
+++ src/ops/base.rs
@@ -13,25 +13,37 @@ pub type AQ =
     Vec<Box<Fn(mpsc::Sender<Vec<query::DataType>>) -> mpsc::Sender<Params> + Send + Sync>>;
 pub type Datas = Box<Iterator<Item = Vec<query::DataType>>>;
 
-// C is the node's internal configuration (e.g., what fields to join on, how to aggregate).
-// FF is the forward function (i.e., a new update comes in, what does it result in?).
-// QF is the query function, which is called when a query hits us and we're not materialized.
-// FF and QF *should* be mergeable somehow, but I haven't found a nice interface yet.
-pub struct Node<C: 'static + Send + Sync,
-                FF: 'static + Sync + Fn(&C, ops::Update, &AQ) -> ops::Update,
-                QF: 'static + Send + Sync + Fn(&C, Option<query::Query>, &AQ) -> Datas>
-{
-    fields: Vec<String>,
-    data: sync::Arc<Option<sync::RwLock<shortcut::Store<query::DataType>>>>,
-    forward: FF,
-    query: sync::Arc<QF>,
-    config: sync::Arc<C>,
+/// `NodeOp` represents the internal operations performed by a node. This trait is very similar to
+/// `flow::View`, and for good reason. This is effectively the behavior of a node when there is no
+/// materialization, and no multithreading. Those features are both added on by Node to expose a
+/// `flow::View`. A NodeOp should not have, nor need, any mutable state (which is why all receivers
+/// are `&`, not `&mut`). Instead, `self` should hold the node's internal configuration (e.g., what
+/// fields to join on, how to aggregate).
+///
+/// It *might* be possible to merge forward and query (after all, they do very similar things), but
+/// I haven't found a nice interface for that yet.
+pub trait NodeOp {
+    /// When a new update comes in to a node, this function is called with that update. The
+    /// resulting update (if any) is sent to all child nodes. If the node is materialized, and the
+    /// resulting update contains positive or negative records, the materialized state is updated
+    /// appropriately.
+    fn forward(&self, ops::Update, &AQ) -> Option<ops::Update>;
+
+    /// Called whenever this node is being queried for records, and it is not materialized. The
+    /// node should use the list of ancestor query functions to fetch relevant data from upstream,
+    /// and emit resulting records as they come in. Note that there may be no query, in which case
+    /// all records should be returned.
+    fn query(&self, Option<query::Query>, &AQ) -> Datas;
 }
 
-impl<C, FF, QF> flow::View<query::Query> for Node<C, FF, QF>
-    where C: 'static + Send + Sync,
-          FF: 'static + Sync + Fn(&C, ops::Update, &AQ) -> ops::Update,
-          QF: 'static + Send + Sync + Fn(&C, Option<query::Query>, &AQ) -> Datas
+pub struct Node<O: NodeOp + Sized + 'static + Send + Sync> {
+    fields: Vec<String>,
+    data: sync::Arc<Option<sync::RwLock<shortcut::Store<query::DataType>>>>,
+    inner: sync::Arc<O>,
+}
+
+impl<O> flow::View<query::Query> for Node<O>
+    where O: NodeOp + Sized + 'static + Send + Sync
 {
     type Update = ops::Update;
     type Data = Vec<query::DataType>;
@@ -47,8 +59,7 @@ impl<C, FF, QF> flow::View<query::Query> for Node<C, FF, QF>
 
         // and the thread is going to need to access a bunch of state
         let q = q.and_then(|q| Some(q.clone()));
-        let s = self.config.clone();
-        let qf = self.query.clone();
+        let inner = self.inner.clone();
         let data = self.data.clone();
 
         // TODO: this should be a thread pool
@@ -83,7 +94,7 @@ impl<C, FF, QF> flow::View<query::Query> for Node<C, FF, QF>
                     }
                 } else {
                     // we are not materialized --- query
-                    for r in qf(&*s, q_cur, &*aqs) {
+                    for r in inner.query(q_cur, &*aqs) {
                         tx.send(r).unwrap()
                     }
                 }
@@ -93,35 +104,30 @@ impl<C, FF, QF> flow::View<query::Query> for Node<C, FF, QF>
     }
 
     fn process(&self, u: Self::Update, aqs: sync::Arc<AQ>) -> Option<Self::Update> {
-        let new_u = (self.forward)(&*self.config, u, &*aqs);
-        match new_u {
-            ops::Update::Records(ref rs) => {
-                if let Some(ref data) = *self.data {
-                    let mut w = data.write().unwrap();
-                    for r in rs.iter() {
-                        if let ops::Record::Positive(ref d) = *r {
-                            w.insert(d.clone());
-                        } else {
-                            unimplemented!();
+        let new_u = self.inner.forward(u, &*aqs);
+        if let Some(ref new_u) = new_u {
+            match *new_u {
+                ops::Update::Records(ref rs) => {
+                    if let Some(ref data) = *self.data {
+                        let mut w = data.write().unwrap();
+                        for r in rs.iter() {
+                            if let ops::Record::Positive(ref d) = *r {
+                                w.insert(d.clone());
+                            } else {
+                                unimplemented!();
+                            }
                         }
                     }
                 }
             }
         }
-        Some(new_u)
+        new_u
     }
 }
 
-pub fn new<'a, S: ?Sized, C, FF, QF>(fields: &[&'a S],
-                                     materialized: bool,
-                                     c: C,
-                                     ff: FF,
-                                     qf: QF)
-                                     -> Node<C, FF, QF>
+pub fn new<'a, S: ?Sized, O>(fields: &[&'a S], materialized: bool, inner: O) -> Node<O>
     where &'a S: Into<String>,
-          C: 'static + Send + Sync,
-          FF: 'static + Sync + Fn(&C, ops::Update, &AQ) -> ops::Update,
-          QF: 'static + Send + Sync + Fn(&C, Option<query::Query>, &AQ) -> Datas
+          O: NodeOp + Sized + 'static + Send + Sync
 {
     let mut data = None;
     if materialized {
@@ -131,10 +137,7 @@ pub fn new<'a, S: ?Sized, C, FF, QF>(fields: &[&'a S],
     Node {
         fields: fields.iter().map(|&s| s.into()).collect(),
         data: sync::Arc::new(data),
-
-        forward: ff,
-        query: sync::Arc::new(qf),
-        config: sync::Arc::new(c),
+        inner: sync::Arc::new(inner),
     }
 }
 
@@ -149,42 +152,50 @@ mod tests {
     use std::thread;
     use std::sync::mpsc;
 
-    fn fw(c: &usize, u: ops::Update, _: &AQ) -> ops::Update {
-        // forward
-        match u {
-            ops::Update::Records(mut rs) => {
-                if let Some(ops::Record::Positive(r)) = rs.pop() {
-                    if let query::DataType::Usize(r) = r[0] {
-                        ops::Update::Records(vec![ops::Record::Positive(vec![(r + *c).into()])])
+    struct Tester(usize);
+
+    impl NodeOp for Tester {
+        fn forward(&self, u: ops::Update, _: &AQ) -> Option<ops::Update> {
+            // forward
+            match u {
+                ops::Update::Records(mut rs) => {
+                    if let Some(ops::Record::Positive(r)) = rs.pop() {
+                        if let query::DataType::Usize(r) = r[0] {
+                            Some(
+                                ops::Update::Records(
+                                    vec![ops::Record::Positive(vec![(r + self.0).into()])]
+                                )
+                            )
+                        } else {
+                            unreachable!();
+                        }
                     } else {
                         unreachable!();
                     }
-                } else {
-                    unreachable!();
                 }
             }
         }
-    }
 
-    fn q(c: &usize, _: Option<query::Query>, aqs: &AQ) -> Datas {
-        // query all ancestors, emit r + c for each
-        let (tx, rx) = mpsc::channel();
-        for aq in aqs.iter() {
-            aq(tx.clone()).send(vec![]).unwrap();
-        }
-
-        let (ptx, prx) = mpsc::channel();
-        let c = *c;
-        thread::spawn(move || {
-            for r in rx {
-                if let query::DataType::Usize(r) = r[0] {
-                    ptx.send(vec![(r + c).into()]).unwrap();
-                } else {
-                    unreachable!();
-                }
+        fn query(&self, _: Option<query::Query>, aqs: &AQ) -> Datas {
+            // query all ancestors, emit r + c for each
+            let (tx, rx) = mpsc::channel();
+            for aq in aqs.iter() {
+                aq(tx.clone()).send(vec![]).unwrap();
             }
-        });
-        Box::new(prx.into_iter())
+
+            let (ptx, prx) = mpsc::channel();
+            let c = self.0;
+            thread::spawn(move || {
+                for r in rx {
+                    if let query::DataType::Usize(r) = r[0] {
+                        ptx.send(vec![(r + c).into()]).unwrap();
+                    } else {
+                        unreachable!();
+                    }
+                }
+            });
+            Box::new(prx.into_iter())
+        }
     }
 
     #[test]
@@ -197,11 +208,11 @@ mod tests {
             select: vec![true],
             having: vec![],
         };
-        let a = g.incorporate(new(&["x"], true, 1 as usize, fw, q), vec![]);
-        let b = g.incorporate(new(&["x"], true, 2 as usize, fw, q), vec![]);
-        let c = g.incorporate(new(&["x"], true, 4 as usize, fw, q),
+        let a = g.incorporate(new(&["x"], true, Tester(1)), vec![]);
+        let b = g.incorporate(new(&["x"], true, Tester(2)), vec![]);
+        let c = g.incorporate(new(&["x"], true, Tester(4)),
                               vec![(all.clone(), a), (all.clone(), b)]);
-        let d = g.incorporate(new(&["x"], true, 8 as usize, fw, q), vec![(all.clone(), c)]);
+        let d = g.incorporate(new(&["x"], true, Tester(8)), vec![(all.clone(), c)]);
         let (put, get) = g.run(10);
 
         // send a value
@@ -273,12 +284,11 @@ mod tests {
             select: vec![true],
             having: vec![],
         };
-        let a = g.incorporate(new(&["x"], true, 1 as usize, fw, q), vec![]);
-        let b = g.incorporate(new(&["x"], true, 2 as usize, fw, q), vec![]);
-        let c = g.incorporate(new(&["x"], false, 4 as usize, fw, q),
+        let a = g.incorporate(new(&["x"], true, Tester(1)), vec![]);
+        let b = g.incorporate(new(&["x"], true, Tester(2)), vec![]);
+        let c = g.incorporate(new(&["x"], false, Tester(4)),
                               vec![(all.clone(), a), (all.clone(), b)]);
-        let d = g.incorporate(new(&["x"], false, 8 as usize, fw, q),
-                              vec![(all.clone(), c)]);
+        let d = g.incorporate(new(&["x"], false, Tester(8)), vec![(all.clone(), c)]);
         let (put, get) = g.run(10);
 
         // send a value
