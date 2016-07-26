abf9a291608a15b29529d8eae9acbde33b25d8b9
Jon Gjengset <jon@thesquareplanet.com>
2016-07-26 16:36:45 -0400

    Remove old note

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ extern crate petgraph;
 extern crate shortcut;
 extern crate bus;
 
-// NOTE: these shouldn't actually be pub -- just useful to silence some warnings for now
 mod flow;
 mod query;
 mod ops;
