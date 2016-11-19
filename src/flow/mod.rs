mit-pdos/noria

commit 73a1a08be6b48008c8df9ae3b0625bb55ce9f5f4

Enable nodes to buffer within a timestamp
This commit changes the way `process()` works, so that a node can
indicate that refuse to release an update for a timestamp until it has
been sent more updates within that same timestamp.

This became necessary as a result of
0f56911991020168f27dea760f6f5370018f316f, which enforced the property
that a node may only emit a single update per timestamp. This broke
Union, which depends on being able to emit all the updates it receives
from its ancestors for a given timestamp. Following
0f56911991020168f27dea760f6f5370018f316f, Union would need to *query*
its ancestors for all its updates *at a given timestamp* when it
received the first update for a timestamp, and then throw away the same
updates when it receives them a short while later. This is inefficient.

With this patch, a node (such as Union), can declare that it is not yet
ready to produce an update for a timestamp. If it does, the node will
not send anything to its children, but instead continue consuming
updates from its parents for the same timestamp. Only when the node
indicates, for some later update, that it is willing to commit to an
update for the timestamp (i.e., when it has heard from all the ancestors
for that timestamp) will the node forward the resulting update.

src/flow/mod.rs
@@ -18,6 +18,51 @@ use ops;
 
 type Graph<Q, U, D> = petgraph::Graph<Option<sync::Arc<View<Q, Update = U, Data = D>>>, ()>;
 
+/// A `ProcessingResult` is used to indicate the result of processing a single update at a node.
+pub enum ProcessingResult<U: Clone + Send> {
+    /// The update was accepted, and the node is done with the current timestamp. No more updates
+    /// should be sent to this node with the timestamp given, and the wrapped update should be
+    /// emitted to all children.
+    Done(U),
+
+    /// The update was accepted, but the node needs more updates for the current timestamp before
+    /// it is willing to produce an update.
+    Accepted,
+
+    /// The node does not wish to perform additional computation at this timestamp, and no update
+    /// should be sent to the node's children. This should translate into sending a single None to
+    /// every child to inform then that no update was emitted for this timestamp from this node.
+    Skip,
+}
+
+impl<U: Clone + Send> ProcessingResult<U> {
+    pub fn is_done(&self) -> bool {
+        match *self {
+            ProcessingResult::Done(_) |
+            ProcessingResult::Skip => true,
+            _ => false,
+        }
+    }
+
+    #[cfg(test)]
+    pub fn unwrap(self) -> U {
+        match self {
+            ProcessingResult::Done(u) => u,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl<U: Clone + Send> From<Option<U>> for ProcessingResult<U> {
+    fn from(o: Option<U>) -> Self {
+        if let Some(u) = o {
+            ProcessingResult::Done(u)
+        } else {
+            ProcessingResult::Skip
+        }
+    }
+}
+
 pub trait View<Q: Clone + Send>: Debug + 'static + Send + Sync {
     type Update: Clone + Send;
     type Data: Clone + Send;
@@ -37,9 +82,26 @@ pub trait View<Q: Clone + Send>: Debug + 'static + Send + Sync {
     /// Execute a single query, producing all matching records.
     fn find(&self, Option<&Q>, Option<i64>) -> Vec<(Self::Data, i64)>;
 
-    /// Process a new update. This may optionally produce a new update to propagate to child nodes
-    /// in the data flow graph.
-    fn process(&self, Self::Update, NodeIndex, i64) -> Option<Self::Update>;
+    /// Process a single update from an ancestor node.
+    ///
+    /// The update may be None if the ancestor did not send any update for this timestamp. In the
+    /// common case, `process` will not even be called in this instance. However, if `process`
+    /// returns `ProcessingResult::Accepted` for all previous updates, and the last ancestor sends
+    /// a `None`, `process` is called with a `None`.
+    ///
+    /// The node is passed the update, the update's source (i.e., which ancestor it arrived from),
+    /// the current timestamp, and a boolean indicating whether this is the last update for the
+    /// given timestamp.
+    ///
+    /// If a node does not return `ProcessingResult::Done` or `ProcessingResult::Skip` when the
+    /// boolean argument is true (i.e., when there are no more updates for this timestamp), the
+    /// node will panic, and terminate.
+    fn process(&self,
+               Option<Self::Update>,
+               NodeIndex,
+               i64,
+               bool)
+               -> ProcessingResult<Self::Update>;
 
     /// Suggest fields of this view, or its ancestors, that would benefit from having an index.
     /// The passed node index is the index of the current node.
@@ -606,6 +668,7 @@ impl<Q, U, D> FlowGraph<Q, U, D>
 
         let NodeState { name, inner, srcs, input, context, processed_ts, init_ts } = state;
 
+        let nsrcs = srcs.len();
         let mut delayed = BinaryHeap::new();
         let mut freshness: HashMap<_, _> = srcs.into_iter().map(|ni| (ni, 0i64)).collect();
         let mut min = 0i64;
@@ -691,10 +754,10 @@ impl<Q, U, D> FlowGraph<Q, U, D>
                 }
             }
 
+            // this *may* be taken out again immediately if the min is raised to the
+            // given ts, but meh, we accept that overhead for the simplicity of the
+            // code (for now).
             if let Some(u) = u {
-                // this *may* be taken out again immediately if the min is raised to the
-                // given ts, but meh, we accept that overhead for the simplicity of the
-                // code (for now).
                 delayed.push(Delayed {
                     data: (src, u),
                     ts: ts,
@@ -731,46 +794,91 @@ impl<Q, U, D> FlowGraph<Q, U, D>
             // hold on to the context, as we're going to interact with our children for a while
             let mut ctx = context.lock().unwrap();
 
-            // keep looking for a candidate to send
-            let mut prev_ts = 0;
-            loop {
-                // find the smallest in `delay`
-                let ts = delayed.peek().and_then(|d| Some(d.ts)).unwrap_or(min + 1);
+            {
+                let mut term = |(src, u), last| -> bool {
+                    // do the processing dicated by our inner
+                    let u = inner.process(u, src, ts, last);
+                    if !u.is_done() {
+                        if last {
+                            unreachable!("last update for a given timestamp must end computation");
+                        }
+                        return false;
+                    }
 
-                // stop if no delayed message has a timestamp <= min
-                if ts > min {
-                    break;
-                }
+                    // we're done with this time -- let the world know!
+                    processed_ts.store(ts as isize, sync::atomic::Ordering::Release);
 
-                // otherwise, deal with the next delayed update
-                let d = delayed.pop().unwrap();
+                    // we only forward Some() things for efficiency. sending None's is unnecessary
+                    // as long as an update will be sent later with a later timestamp. we use
+                    // `forwarded` below to figure out whether we need to also send a final None.
+                    if let ProcessingResult::Done(u) = u {
+                        let mut u = Some(u);
+                        forwarded = ts;
 
-                // we only ever need to process a single update per timestamp.
-                if ts == prev_ts {
-                    continue;
-                }
-                prev_ts = ts;
-
-                // do the processing dicated by our inner
-                let mut u = inner.process(d.data.1, d.data.0, ts);
-                processed_ts.store(ts as isize, sync::atomic::Ordering::Release);
-
-                // we only forward Some() things for efficiency. sending None's is unnecessary as
-                // long as an update will be sent later with a later timestamp. we use `forwarded`
-                // below to figure out whether we need to also send a final None.
-                if u.is_some() {
-                    forwarded = ts;
-
-                    // avoid cloning on the last send. this will avoid clone altogether for
-                    // nodes with only one child, which is a common case.
-                    let mut txit = ctx.txs.iter_mut().peekable();
-                    while let Some(tx) = txit.next() {
-                        if txit.peek().is_some() {
-                            tx.send((name, u.clone(), ts)).unwrap();
-                        } else {
-                            tx.send((name, u.take(), ts)).unwrap();
+                        // avoid cloning on the last send. this will avoid clone altogether for
+                        // nodes with only one child, which is a common case.
+                        let mut txit = ctx.txs.iter_mut().peekable();
+                        while let Some(tx) = txit.next() {
+                            if txit.peek().is_some() {
+                                tx.send((name, u.clone(), ts)).unwrap();
+                            } else {
+                                tx.send((name, u.take(), ts)).unwrap();
+                            }
                         }
                     }
+
+                    return true;
+                };
+
+                // keep looking for a candidate to send
+                let mut prev_ts = -1;
+                let mut nrcv = nsrcs;
+                let mut done = true;
+                let mut lastsrc = *freshness.keys().next().unwrap();
+                loop {
+                    // find the smallest in `delay`
+                    let ts = delayed.peek().and_then(|d| Some(d.ts)).unwrap_or(min + 1);
+
+                    // stop if no delayed message has a timestamp <= min
+                    if ts > min {
+                        break;
+                    }
+
+                    // otherwise, deal with the next delayed update
+                    let d = delayed.pop().unwrap();
+
+                    // keep track of how many parents we've received from
+                    if ts == prev_ts {
+                        nrcv += 1;
+                    } else {
+                        // we've moved to the next timestamp. if we didn't finish the previous
+                        // timestamp, and didn't run it with last == true, then do so now.
+                        if !done {
+                            term((lastsrc, None), true);
+                        }
+                        // start the next timestamp
+                        done = false;
+                        nrcv = 1;
+                    }
+                    prev_ts = ts;
+
+                    // we're already done with this timestamp
+                    if done {
+                        continue;
+                    }
+
+                    // track the sender, in case we have to retroactively mark it as the last sender
+                    lastsrc = d.data.0;
+
+                    // process the update
+                    done = term((d.data.0, Some(d.data.1)), nrcv == nsrcs);
+                }
+
+                // we could have received only updates from a few ancestors for the last timestamp,
+                // and thus not have called term with last == true. if that were the case, call it
+                // one last time now.
+                if !done {
+                    term((lastsrc, None), true);
                 }
             }
 
@@ -913,11 +1021,20 @@ mod tests {
             }
         }
 
-        fn process(&self, u: Self::Update, _: NodeIndex, _: i64) -> Option<Self::Update> {
+        fn process(&self,
+                   u: Option<Self::Update>,
+                   _: NodeIndex,
+                   _: i64,
+                   _: bool)
+                   -> ProcessingResult<Self::Update> {
             use std::ops::AddAssign;
-            let mut x = self.2.lock().unwrap();
-            x.add_assign(u);
-            Some(u)
+            if let Some(u) = u {
+                let mut x = self.2.lock().unwrap();
+                x.add_assign(u);
+                ProcessingResult::Done(u)
+            } else {
+                ProcessingResult::Skip
+            }
         }
 
         fn suggest_indexes(&self, _: NodeIndex) -> HashMap<NodeIndex, Vec<usize>> {

src/ops/grouped/mod.rs
@@ -154,13 +154,21 @@ impl<T: GroupedOperation> NodeOp for GroupedOperator<T> {
     }
 
     fn forward(&self,
-               u: ops::Update,
+               u: Option<ops::Update>,
                src: flow::NodeIndex,
                _: i64,
+               last: bool,
                db: Option<&backlog::BufferedStore>)
-               -> Option<ops::Update> {
+               -> flow::ProcessingResult<ops::Update> {
 
         assert_eq!(src, self.src);
+
+        if u.is_none() {
+            // we only have one ancestor, so this must be last, and our ancestor sent nothing
+            debug_assert!(last);
+            return u.into();
+        }
+        let u = u.unwrap();
 
         // Construct the query we'll need to query into ourselves
         let mut q = self.cond.clone();
@@ -168,7 +176,7 @@ impl<T: GroupedOperation> NodeOp for GroupedOperator<T> {
         match u {
             ops::Update::Records(rs) => {
                 if rs.is_empty() {
-                    return None;
+                    return flow::ProcessingResult::Skip;
                 }
 
                 // First, we want to be smart about multiple added/removed rows with same group.
@@ -263,7 +271,7 @@ impl<T: GroupedOperation> NodeOp for GroupedOperator<T> {
                     out.push(ops::Record::Positive(rec, new_ts));
                 }
 
-                Some(ops::Update::Records(out))
+                flow::ProcessingResult::Done(ops::Update::Records(out))
             }
         }
     }

src/ops/mod.rs
@@ -124,13 +124,14 @@ pub trait NodeOp: Debug {
     /// When a new update comes in to a node, this function is called with that update. The
     /// resulting update (if any) is sent to all child nodes. If the node is materialized, and the
     /// resulting update contains positive or negative records, the materialized state is updated
-    /// appropriately.
+    /// appropriately. See `View::process` for more documentation.
     fn forward(&self,
-               Update,
-               flow::NodeIndex,
-               i64,
-               Option<&backlog::BufferedStore>)
-               -> Option<Update>;
+               u: Option<Update>,
+               src: flow::NodeIndex,
+               ts: i64,
+               last: bool,
+               mat: Option<&backlog::BufferedStore>)
+               -> flow::ProcessingResult<Update>;
 
     /// Called whenever this node is being queried for records, and it is not materialized. The
     /// node should use the list of ancestor query functions to fetch relevant data from upstream,
@@ -193,23 +194,24 @@ impl NodeOp for NodeType {
     }
 
     fn forward(&self,
-               u: Update,
+               u: Option<Update>,
                src: flow::NodeIndex,
                ts: i64,
+               last: bool,
                db: Option<&backlog::BufferedStore>)
-               -> Option<Update> {
+               -> flow::ProcessingResult<Update> {
         match *self {
-            NodeType::Base(ref n) => n.forward(u, src, ts, db),
-            NodeType::Aggregate(ref n) => n.forward(u, src, ts, db),
-            NodeType::Join(ref n) => n.forward(u, src, ts, db),
-            NodeType::Latest(ref n) => n.forward(u, src, ts, db),
-            NodeType::Union(ref n) => n.forward(u, src, ts, db),
-            NodeType::Identity(ref n) => n.forward(u, src, ts, db),
-            NodeType::GroupConcat(ref n) => n.forward(u, src, ts, db),
+            NodeType::Base(ref n) => n.forward(u, src, ts, last, db),
+            NodeType::Aggregate(ref n) => n.forward(u, src, ts, last, db),
+            NodeType::Join(ref n) => n.forward(u, src, ts, last, db),
+            NodeType::Latest(ref n) => n.forward(u, src, ts, last, db),
+            NodeType::Union(ref n) => n.forward(u, src, ts, last, db),
+            NodeType::Identity(ref n) => n.forward(u, src, ts, last, db),
+            NodeType::GroupConcat(ref n) => n.forward(u, src, ts, last, db),
             #[cfg(test)]
-            NodeType::Test(ref n) => n.forward(u, src, ts, db),
+            NodeType::Test(ref n) => n.forward(u, src, ts, last, db),
             #[cfg(test)]
-            NodeType::GatedIdentity(ref n) => n.forward(u, src, ts, db),
+            NodeType::GatedIdentity(ref n) => n.forward(u, src, ts, last, db),
         }
     }
 
@@ -374,14 +376,19 @@ impl flow::View<query::Query> for Node {
         }
     }
 
-    fn process(&self, u: Self::Update, src: flow::NodeIndex, ts: i64) -> Option<Self::Update> {
+    fn process(&self,
+               u: Option<Self::Update>,
+               src: flow::NodeIndex,
+               ts: i64,
+               last: bool)
+               -> flow::ProcessingResult<Self::Update> {
         use std::ops::Deref;
 
         // TODO: the incoming update has not been projected through the query, and so does not fit
         // the expected input format. let's fix that.
 
-        let mut new_u = self.inner.forward(u, src, ts, self.data.deref().as_ref());
-        if let Some(ref mut new_u) = new_u {
+        let mut new_u = self.inner.forward(u, src, ts, last, self.data.deref().as_ref());
+        if let flow::ProcessingResult::Done(ref mut new_u) = new_u {
             match *new_u {
                 Update::Records(ref mut rs) => {
                     if let Some(data) = self.data.deref().as_ref() {
@@ -513,18 +520,22 @@ mod tests {
         }
 
         fn forward(&self,
-                   u: Update,
+                   u: Option<Update>,
                    _: flow::NodeIndex,
                    _: i64,
+                   _: bool,
                    _: Option<&backlog::BufferedStore>)
-                   -> Option<Update> {
+                   -> flow::ProcessingResult<Update> {
             // forward
             match u {
-                Update::Records(mut rs) => {
+                Some(Update::Records(mut rs)) => {
                     if let Some(Record::Positive(r, ts)) = rs.pop() {
                         if let query::DataType::Number(r) = r[0] {
-                            Some(Update::Records(vec![Record::Positive(vec![(r + self.0).into()],
-                                                                       ts)]))
+                            flow::ProcessingResult::Done(
+                                Update::Records(
+                                    vec![Record::Positive(vec![(r + self.0).into()], ts)]
+                                )
+                            )
                         } else {
                             unreachable!();
                         }
@@ -532,6 +543,7 @@ mod tests {
                         unreachable!();
                     }
                 }
+                None => flow::ProcessingResult::Skip,
             }
         }
 

src/ops/base.rs
@@ -27,24 +27,26 @@ impl NodeOp for Base {
     }
 
     fn forward(&self,
-               mut u: ops::Update,
+               mut u: Option<ops::Update>,
                _: flow::NodeIndex,
                ts: i64,
+               last: bool,
                _: Option<&backlog::BufferedStore>)
-               -> Option<ops::Update> {
+               -> flow::ProcessingResult<ops::Update> {
 
         // basically our only job is to record timestamps
-        match u {
-            ops::Update::Records(ref mut rs) => {
-                for r in rs.iter_mut() {
-                    match *r {
-                        ops::Record::Positive(_, ref mut rts) |
-                        ops::Record::Negative(_, ref mut rts) => *rts = ts,
-                    }
+        if let Some(ops::Update::Records(ref mut rs)) = u {
+            for r in rs.iter_mut() {
+                match *r {
+                    ops::Record::Positive(_, ref mut rts) |
+                    ops::Record::Negative(_, ref mut rts) => *rts = ts,
                 }
             }
         }
-        Some(u)
+
+        // we should only have one ancestor
+        debug_assert!(last);
+        u.into()
     }
 
     fn query(&self, _: Option<&query::Query>, _: i64) -> ops::Datas {

src/ops/gatedid.rs
@@ -49,13 +49,17 @@ impl NodeOp for GatedIdentity {
     }
 
     fn forward(&self,
-               update: ops::Update,
+               update: Option<ops::Update>,
                _: flow::NodeIndex,
                _: i64,
+               _: bool,
                _: Option<&backlog::BufferedStore>)
-               -> Option<ops::Update> {
-        self.rx.lock().unwrap().recv().unwrap();
-        Some(update)
+               -> flow::ProcessingResult<ops::Update> {
+
+        if update.is_some() {
+            self.rx.lock().unwrap().recv().unwrap();
+        }
+        update.into()
     }
 
     fn query(&self, q: Option<&query::Query>, ts: i64) -> ops::Datas {

src/ops/identity.rs
@@ -39,12 +39,13 @@ impl NodeOp for Identity {
 
     #[allow(unused_variables)]
     fn forward(&self,
-               update: ops::Update,
+               update: Option<ops::Update>,
                src: flow::NodeIndex,
                timestamp: i64,
+               last: bool,
                materialized_view: Option<&backlog::BufferedStore>)
-               -> Option<ops::Update> {
-        Some(update)
+               -> flow::ProcessingResult<ops::Update> {
+        update.into()
     }
 
     fn query(&self, q: Option<&query::Query>, ts: i64) -> ops::Datas {

src/ops/join.rs
@@ -286,11 +286,26 @@ impl NodeOp for Joiner {
     }
 
     fn forward(&self,
-               u: ops::Update,
+               u: Option<ops::Update>,
                from: flow::NodeIndex,
                ts: i64,
+               last: bool,
                _: Option<&backlog::BufferedStore>)
-               -> Option<ops::Update> {
+               -> flow::ProcessingResult<ops::Update> {
+
+        if u.is_none() {
+            return if last {
+                // all our ancestors sent a None, so nothing relevant for us to do
+                flow::ProcessingResult::Skip
+            } else {
+                // we should only be called with None on the last ancestor
+                debug_assert!(false);
+                // technically, it's not a problem though, we *could* just ignore it
+                // we keep the unreachable!() in place above anyway, just to catch this
+                flow::ProcessingResult::Accepted
+            };
+        }
+        let u = u.unwrap();
 
         match u {
             ops::Update::Records(rs) => {
@@ -301,7 +316,7 @@ impl NodeOp for Joiner {
 
                 // TODO: we should be clever here, and only query once per *distinct join value*,
                 // instead of once per received record.
-                Some(ops::Update::Records(rs.into_iter()
+                flow::ProcessingResult::Done(ops::Update::Records(rs.into_iter()
                     .flat_map(|rec| {
                         let (r, pos, lts) = rec.extract();
 

src/ops/latest.rs
@@ -55,13 +55,21 @@ impl NodeOp for Latest {
     }
 
     fn forward(&self,
-               u: ops::Update,
+               u: Option<ops::Update>,
                src: flow::NodeIndex,
                _: i64,
+               last: bool,
                db: Option<&backlog::BufferedStore>)
-               -> Option<ops::Update> {
+               -> flow::ProcessingResult<ops::Update> {
 
         assert_eq!(src, self.src);
+
+        if u.is_none() {
+            // we only have one ancestor, so this must be last, and our ancestor sent nothing
+            debug_assert!(last);
+            return u.into();
+        }
+        let u = u.unwrap();
 
         // Construct the query we'll need to query into ourselves to find current latest
         let mut q = self.key
@@ -152,7 +160,7 @@ impl NodeOp for Latest {
                     assert!(handled.contains(&group));
                 }
 
-                Some(ops::Update::Records(out))
+                flow::ProcessingResult::Done(ops::Update::Records(out))
             }
         }
     }

commit fd45c5586b94aee102454944dca33f10c9d55ee7

Fix incorrect timestamps on delayed updates
This fixes a bug introduced in 73a1a08be6b48008c8df9ae3b0625bb55ce9f5f4,
which manifested itself as a non-deterministic crash in the "vote"
benchmark. The problem was that delayed updates at a node would *all* be
processed at the current (later) incoming update's timestamp, rather
than their original timestamps. This led to `process()` being called
with the wrong timestamp, which caused early absorption to the (later)
current incoming update timestamp, and a subsequent crash when trying to
add more delayed items to the node's backlog at older timestamps.

In terms of the code, this happened because the `term()` closure
captured `ts` from its environment, rather than using the `ts` value at
its call sites. By passing the correct `ts` as an explicit argument, we
get the right behaviour. This fixes the "vote" benchmark, which used to
crash in the prepopulation phase.

src/flow/mod.rs
@@ -796,9 +796,9 @@ impl<Q, U, D> FlowGraph<Q, U, D>
             let mut ctx = context.lock().unwrap();
 
             {
-                let mut term = |(src, u), last| -> bool {
+                let mut term = |(src, u, uts), last| -> bool {
                     // do the processing dicated by our inner
-                    let u = inner.process(u, src, ts, last);
+                    let u = inner.process(u, src, uts, last);
                     if !u.is_done() {
                         if last {
                             unreachable!("last update for a given timestamp must end computation");
@@ -807,23 +807,23 @@ impl<Q, U, D> FlowGraph<Q, U, D>
                     }
 
                     // we're done with this time -- let the world know!
-                    processed_ts.store(ts as isize, sync::atomic::Ordering::Release);
+                    processed_ts.store(uts as isize, sync::atomic::Ordering::Release);
 
                     // we only forward Some() things for efficiency. sending None's is unnecessary
                     // as long as an update will be sent later with a later timestamp. we use
                     // `forwarded` below to figure out whether we need to also send a final None.
                     if let ProcessingResult::Done(u) = u {
                         let mut u = Some(u);
-                        forwarded = ts;
+                        forwarded = uts;
 
                         // avoid cloning on the last send. this will avoid clone altogether for
                         // nodes with only one child, which is a common case.
                         let mut txit = ctx.txs.iter_mut().peekable();
                         while let Some(tx) = txit.next() {
                             if txit.peek().is_some() {
-                                tx.send((name, u.clone(), ts)).unwrap();
+                                tx.send((name, u.clone(), uts)).unwrap();
                             } else {
-                                tx.send((name, u.take(), ts)).unwrap();
+                                tx.send((name, u.take(), uts)).unwrap();
                             }
                         }
                     }
@@ -855,7 +855,7 @@ impl<Q, U, D> FlowGraph<Q, U, D>
                         // we've moved to the next timestamp. if we didn't finish the previous
                         // timestamp, and didn't run it with last == true, then do so now.
                         if !done {
-                            term((lastsrc, None), true);
+                            term((lastsrc, None, ts), true);
                         }
                         // start the next timestamp
                         done = false;
@@ -872,14 +872,14 @@ impl<Q, U, D> FlowGraph<Q, U, D>
                     lastsrc = d.data.0;
 
                     // process the update
-                    done = term((d.data.0, Some(d.data.1)), nrcv == nsrcs);
+                    done = term((d.data.0, Some(d.data.1), ts), nrcv == nsrcs);
                 }
 
                 // we could have received only updates from a few ancestors for the last timestamp,
                 // and thus not have called term with last == true. if that were the case, call it
                 // one last time now.
                 if !done {
-                    term((lastsrc, None), true);
+                    term((lastsrc, None, prev_ts), true);
                 }
             }
 