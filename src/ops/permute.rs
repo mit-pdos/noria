f81c2a7ff2774a12529271860a9764619f2ace64
Jon Gjengset <jon@thesquareplanet.com>
2016-11-29 22:02:51 -0500

    stop being clever about permute

--- src/ops/permute.rs
+++ src/ops/permute.rs
@@ -28,108 +28,6 @@ impl Permute {
     fn resolve_col(&self, col: usize) -> usize {
         self.emit.as_ref().map_or(col, |emit| emit[col])
     }
-
-    fn permute(&self, data: &mut Vec<query::DataType>) {
-        if let Some(ref emit) = self.emit {
-            use std::iter;
-            // http://stackoverflow.com/a/1683662/472927
-            // TODO: compute the swaps in advance instead
-            let mut done: Vec<_> = iter::repeat(false).take(self.cols).collect();
-            for mut i in 0..emit.len() {
-                // all data[i' < i] are in the right place
-                if done[i] {
-                    // data[i] has been made correct in previou swap cycle
-                    continue;
-                }
-                if emit[i] == i {
-                    // data[i] does not need to be swapped
-                    continue;
-                }
-
-                // if we reach here, this means that we're supposed to hold the data from some
-                // position i' = self.emit[i] at data[i]. but, we first need to make sure that
-                // no-one else is trying to use the current value at data[i] (since we'd be
-                // overwriting it). we *could* do a scan through all i' > i, but that can get quite
-                // expensive. instead, we're going to do the following:
-                //
-                //  - swap the value at our position (j = i) with the one we want (emit[j]).
-                //    emit[j] will thus hold the original data[i].
-                //  - if output j wanted the value at i, we're done
-                //  - if not, swap again
-                //
-                // as we do this, we also mark off the swaps we do. for each j we pass in the loop
-                // above, we've put the correct value into data[j], so we should't swap again when
-                // we get around to i == j.
-                //
-                // there is one bit of trickery compared to the original algorithm, and that is
-                // that not all columns may be emitted. if we reach the case where j is beyond the
-                // boundaries of emit, then what do we do? we can't just stop iterating, because
-                // now data[i] will be at data[j], but someone might still need data[i]!
-                // in this case, the only thing we can really do is *search* for the i' such that
-                // emit[i'] = i. if there is one, then we swap it data[j] directly there. if there
-                // isn't, that's great! we can just leave data[j] (which is data[i]) right where it
-                // is -- in some remote location that no-one will look at again.
-
-                // the original pseudocode calls for
-                //
-                //   let t = data[i].clone();
-                //
-                // but we can instead just swap this element along as we traverse the cycles. this
-                // works if we assume that each source index is only emitted once.
-                let mut j = i;
-                loop {
-                    done[j] = true;
-                    if j >= emit.len() {
-                        // this is the tricky situation described above.
-                        // we need to search for the thing that emits data[i] (if any)
-                        let mut swapped = false;
-                        for k in (i + 1)..emit.len() {
-                            if emit[k] == i {
-                                done[k] = true;
-                                data.swap(k, j);
-                                swapped = true;
-                                // now we're in another tough spot. we've given data[i] to data[k],
-                                // as desired, but data[k] is now in data[j]! what if someone
-                                // wanted data[k]? well, we're in luck. we have effectively just
-                                // finished with i -- it has been assigned to its final
-                                // destination, and everyone along the cycle there has been given
-                                // the right value. we can therefore act as though we just stepped
-                                // to j with i = k instead!
-                                i = k;
-                                break;
-                            }
-                        }
-
-                        // note that if we never emit data[i], we also don't need to do any swaps
-                        if swapped {
-                            // still more work to do
-                        } else {
-                            break;
-                        }
-                    } else if emit[j] != i {
-                        // we are supposed to put another column here. put that other value here,
-                        // and then figure out what goes there to replace what we just took.
-                        // the original pseudocode calls for
-                        //
-                        //   data[j] = data[emit[j]].clone();
-                        //
-                        // but this shouldn't be necessary if we assume that each source index is
-                        // only emitted once.
-                        data.swap(j, emit[j]);
-                        j = emit[j];
-                    } else {
-                        // the original value is supposed to go here, and since we've been swapping
-                        // values as we went, it's already there! the cycle is complete!
-                        // the original pseudocode calls for
-                        //
-                        //   data[j] = t;
-                        break;
-                    }
-                }
-            }
-            data.truncate(emit.len());
-        }
-    }
 }
 
 impl Ingredient for Permute {
@@ -174,7 +72,16 @@ impl Ingredient for Permute {
             match input.data {
                 ops::Update::Records(ref mut rs) => {
                     for r in rs {
-                        self.permute(r);
+                        if self.emit.is_none() {
+                            continue;
+                        }
+
+                        let mut new_r = Vec::with_capacity(r.len());
+                        let e = self.emit.as_ref().unwrap();
+                        for i in e {
+                            new_r.push(r[*i].clone());
+                        }
+                        **r = new_r;
                     }
                 }
             }
