946b0079d3a102f629944ae2cd8e9a5e2ec46ef5
Jon Gjengset <jon@thesquareplanet.com>
2016-11-29 22:01:31 -0500

    fix local map insertion when not in order

--- src/flow/domain/local.rs
+++ src/flow/domain/local.rs
@@ -21,7 +21,11 @@ impl<T> Map<T> {
         let i = addr.id();
 
         if i >= self.things.len() {
-            let diff = self.things.len() - i + 1;
+            let diff = if self.things.is_empty() {
+                i + 1
+            } else {
+                self.things.len() - i + 1
+            };
             self.things.reserve(diff);
             for _ in 0..diff {
                 self.things.push(None);
