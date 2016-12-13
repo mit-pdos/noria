d22b383f04761c385cdc21deb01d17c74f7f63a8
Malte Schwarzkopf <malte@csail.mit.edu>
2016-12-13 00:11:51 -0500

    Expose leaf view per SQL query, add named views
    This adds a final missing component to building graphs from SQL
    queries: the generation of the leaf/edge view that actually represents
    the entire query and its results. This is where readers ought to read
    the results of this query; all other nodes are anoymous internal nodes.
    
    SQL SELECT queries have no natural identifier, so we return a generated
    one by default now. However, generated identifiers are painful when
    constructing graphs from SQL queries, so we also support passing an
    optimal explicit view name with `to_flow_parts`. This name is used for
    the leaf node of the query only.

--- src/flow/sql_to_flow.rs
+++ src/flow/sql_to_flow.rs
@@ -1,6 +1,6 @@
 use nom_sql::parser as sql_parser;
 use flow;
-use flow::sql::query_graph::{QueryGraphEdge, QueryGraphNode, to_query_graph};
+use flow::sql::query_graph::{QueryGraph, QueryGraphEdge, QueryGraphNode, to_query_graph};
 use FlowGraph;
 use nom_sql::{Column, ConditionBase, ConditionExpression, ConditionTree, Operator, SqlQuery};
 use nom_sql::{InsertStatement, SelectStatement};
@@ -152,7 +152,10 @@ fn make_join_node(name: &str,
     n
 }
 
-fn make_nodes_for_selection(st: &SelectStatement, g: &mut FG) -> Vec<NodeIndex> {
+fn make_nodes_for_selection(st: &SelectStatement,
+                            name: &str,
+                            g: &mut FG)
+                            -> (QueryGraph, Vec<NodeIndex>) {
     use std::collections::HashMap;
 
     let qg = match to_query_graph(st) {
@@ -160,77 +163,113 @@ fn make_nodes_for_selection(st: &SelectStatement, g: &mut FG) -> Vec<NodeIndex> 
         Err(e) => panic!(e),
     };
 
-    let mut i = 0;
-    // 1. Generate a filter node for each relation node in the query graph.
-    let mut filter_nodes = HashMap::new();
-    // Need to iterate over relations in a deterministic order, as otherwise nodes will be added in
-    // a different order every time, which will yield different node identifiers and make it
-    // difficult for applications to check what's going on.
-    let mut sorted_rels: Vec<&String> = qg.relations.keys().collect();
-    sorted_rels.sort();
-    for rel in sorted_rels.iter() {
-        let qgn = qg.relations.get(*rel).unwrap();
-        // the following conditional is required to avoid "empty" nodes (without any projected
-        // columns) that are required as inputs to joins
-        if qgn.columns.len() > 0 || qgn.predicates.len() > 0 {
-            // add a basic filter/permute node for each query graph node if it either has:
-            // 1. projected columns, or
-            // 2. a filter condition
-            let n = make_filter_node(&format!("query-{:x}:{}", qg.signature().hash, i), qgn, g);
-            let ni = g.incorporate(n);
-            filter_nodes.insert(rel.clone(), ni);
-        } else {
-            // otherwise, just record the node index of the base node for the relation that is
-            // being selected from
-            filter_nodes.insert(rel.clone(), lookup_nodeindex(rel, g));
-        }
-        i += 1;
-    }
+    let nodes_added;
 
-    // 2. Generate join nodes for the query. This starts out by joining two of the filter nodes
-    //    corresponding to relations in the first join predicate, and then continues to join the
-    //    result against previously unseen tables from the remaining predicates.
-    //    Note that no (src, dst) pair ever occurs twice, since we've already previously moved all
-    //    predicates pertaining to src/dst joins onto a single edge.
-    let mut join_nodes = Vec::new();
-    let mut joined_tables = HashSet::new();
-    let mut sorted_edges: Vec<(&(String, String), &QueryGraphEdge)> = qg.edges.iter().collect();
-    sorted_edges.sort_by_key(|ref k| &(k.0).0);
-    let mut edge_iter = sorted_edges.iter();
-    let mut prev_ni = None;
-    while let Some(&(&(ref src, ref dst), edge)) = edge_iter.next() {
-        let left_ni = match prev_ni {
-            None => {
+    let mut i = 0;
+    {
+        // 1. Generate a filter node for each relation node in the query graph.
+        let mut filter_nodes = HashMap::<String, NodeIndex>::new();
+        // Need to iterate over relations in a deterministic order, as otherwise nodes will be added in
+        // a different order every time, which will yield different node identifiers and make it
+        // difficult for applications to check what's going on.
+        let mut sorted_rels: Vec<&String> = qg.relations.keys().collect();
+        sorted_rels.sort();
+        for rel in sorted_rels.iter() {
+            let qgn = qg.relations.get(*rel).unwrap();
+            // the following conditional is required to avoid "empty" nodes (without any projected
+            // columns) that are required as inputs to joins
+            if qgn.columns.len() > 0 || qgn.predicates.len() > 0 {
+                // add a basic filter/permute node for each query graph node if it either has:
+                // 1. projected columns, or
+                // 2. a filter condition
+                let n = make_filter_node(&format!("q_{:x}_n{}", qg.signature().hash, i), qgn, g);
+                let ni = g.incorporate(n);
+                filter_nodes.insert((*rel).clone(), ni);
+            } else {
+                // otherwise, just record the node index of the base node for the relation that is
+                // being selected from
+                filter_nodes.insert((*rel).clone(), lookup_nodeindex(rel, g));
+            }
+            i += 1;
+        }
+
+        // 2. Generate join nodes for the query. This starts out by joining two of the filter nodes
+        //    corresponding to relations in the first join predicate, and then continues to join the
+        //    result against previously unseen tables from the remaining predicates.
+        //    Note that no (src, dst) pair ever occurs twice, since we've already previously moved all
+        //    predicates pertaining to src/dst joins onto a single edge.
+        let mut join_nodes = Vec::new();
+        let mut joined_tables = HashSet::new();
+        let mut sorted_edges: Vec<(&(String, String), &QueryGraphEdge)> = qg.edges.iter().collect();
+        sorted_edges.sort_by_key(|ref k| &(k.0).0);
+        let mut edge_iter = sorted_edges.iter();
+        let mut prev_ni = None;
+        while let Some(&(&(ref src, ref dst), edge)) = edge_iter.next() {
+            let left_ni = match prev_ni {
+                None => {
+                    joined_tables.insert(src);
+                    filter_nodes.get(src).unwrap().clone()
+                }
+                Some(ni) => ni,
+            };
+            let right_ni = if joined_tables.contains(src) {
+                joined_tables.insert(dst);
+                filter_nodes.get(dst).unwrap().clone()
+            } else if joined_tables.contains(dst) {
                 joined_tables.insert(src);
                 filter_nodes.get(src).unwrap().clone()
-            }
-            Some(ni) => ni,
-        };
-        let right_ni = if joined_tables.contains(src) {
-            joined_tables.insert(dst);
-            filter_nodes.get(dst).unwrap().clone()
-        } else if joined_tables.contains(dst) {
-            joined_tables.insert(src);
-            filter_nodes.get(src).unwrap().clone()
-        } else {
-            // We have already handled *both* tables that are part of the join. This should never
-            // occur, because their join predicates must be associated with the same query graph
-            // edge.
-            unreachable!();
-        };
-        let n = make_join_node(&format!("query-{:x}:{}", qg.signature().hash, i),
-                               edge,
-                               left_ni,
-                               right_ni,
-                               g);
-        let ni = g.incorporate(n);
-        join_nodes.push(ni);
-        i += 1;
-        prev_ni = Some(ni);
-    }
+            } else {
+                // We have already handled *both* tables that are part of the join. This should never
+                // occur, because their join predicates must be associated with the same query graph
+                // edge.
+                unreachable!();
+            };
+            let n = make_join_node(&format!("q_{:x}_n{}", qg.signature().hash, i),
+                                   edge,
+                                   left_ni,
+                                   right_ni,
+                                   g);
+            let ni = g.incorporate(n);
+            join_nodes.push(ni);
+            i += 1;
+            prev_ni = Some(ni);
+        }
 
-    // finally, we output all the nodes we generated
-    filter_nodes.into_iter().map(|(_, n)| n).chain(join_nodes.into_iter()).collect()
+        // 3. Generate leaf views that expose the query result
+        let n;
+        {
+            let final_join_ni = if !join_nodes.is_empty() {
+                join_nodes.last().unwrap()
+            } else {
+                assert!(filter_nodes.len() == 1);
+                filter_nodes.iter().next().as_ref().unwrap().1
+            };
+            let final_join_view = lookup_view_by_nodeindex(*final_join_ni, g);
+            let projected_columns: Vec<String> = qg.relations
+                .iter()
+                .fold(Vec::new(), |mut v, (_, qgn)| {
+                    v.extend(qgn.columns.clone().into_iter());
+                    v
+                });
+            let projected_column_ids: Vec<usize> = projected_columns.iter()
+                .map(|c| field_to_columnid(final_join_view, &c).unwrap())
+                .collect();
+            n = ops::new(String::from(name),
+                         projected_columns.iter()
+                             .map(String::as_ref)
+                             .collect::<Vec<&str>>()
+                             .as_slice(),
+                         true,
+                         Permute::new(*final_join_ni, projected_column_ids.as_slice()));
+        }
+        let ni = g.incorporate(n);
+        filter_nodes.insert(String::from(name), ni);
+
+        // finally, we output all the nodes we generated
+        nodes_added =
+            filter_nodes.into_iter().map(|(_, n)| n).chain(join_nodes.into_iter()).collect();
+    }
+    (qg, nodes_added)
 }
 
 /// Long-lived struct that holds information about the SQL queries that have been incorporated into
@@ -249,7 +288,18 @@ impl<'a> SqlIncorporator<'a> {
         }
     }
 
-    fn nodes_for_query(&mut self, q: SqlQuery) -> Vec<NodeIndex> {
+    fn nodes_for_query(&mut self, q: SqlQuery) -> (String, Vec<NodeIndex>) {
+        let name = match q {
+            SqlQuery::Insert(ref iq) => iq.table.name.clone(),
+            SqlQuery::Select(_) => format!("q_{}", self.query_graphs.len()),
+        };
+        self.nodes_for_named_query(q, name)
+    }
+
+    fn nodes_for_named_query(&mut self,
+                             q: SqlQuery,
+                             query_name: String)
+                             -> (String, Vec<NodeIndex>) {
         use flow::sql::passes::alias_removal::AliasRemoval;
         use flow::sql::passes::implied_tables::ImpliedTableExpansion;
         use flow::sql::passes::star_expansion::StarExpansion;
@@ -266,40 +316,61 @@ impl<'a> SqlIncorporator<'a> {
                     println!("WARNING: base table for write typye {} already exists: ignoring \
                               query.",
                              iq.table.name);
-                    vec![]
+                    (iq.table.name.clone(), vec![])
                 } else {
+                    assert_eq!(query_name, iq.table.name);
                     let n = make_base_node(&iq);
-                    self.write_schemas.insert(iq.table.name,
+                    self.write_schemas.insert(iq.table.name.clone(),
                                               iq.fields
                                                   .iter()
                                                   .map(|&(ref c, _)| c.name.clone())
                                                   .collect());
-                    vec![self.graph.incorporate(n)]
+                    (query_name, vec![self.graph.incorporate(n)])
                 }
             }
-            SqlQuery::Select(sq) => make_nodes_for_selection(&sq, self.graph),
+            SqlQuery::Select(sq) => {
+                let (qg, nodes) = make_nodes_for_selection(&sq, &query_name, self.graph);
+                // Store the query graph for later reference
+                self.query_graphs.push(qg);
+                // Return new nodes
+                (query_name, nodes)
+            }
         }
     }
 }
 
-trait ToFlowParts {
-    fn to_flow_parts<'a>(&self, &mut SqlIncorporator) -> Result<Vec<NodeIndex>, String>;
+pub trait ToFlowParts {
+    fn to_flow_parts<'a>(&self,
+                         &mut SqlIncorporator,
+                         Option<String>)
+                         -> Result<(String, Vec<NodeIndex>), String>;
 }
 
 impl<'a> ToFlowParts for &'a String {
-    fn to_flow_parts(&self, inc: &mut SqlIncorporator) -> Result<Vec<NodeIndex>, String> {
-        self.as_str().to_flow_parts(inc)
+    fn to_flow_parts(&self,
+                     inc: &mut SqlIncorporator,
+                     name: Option<String>)
+                     -> Result<(String, Vec<NodeIndex>), String> {
+        self.as_str().to_flow_parts(inc, name)
     }
 }
 
 impl<'a> ToFlowParts for &'a str {
-    fn to_flow_parts(&self, inc: &mut SqlIncorporator) -> Result<Vec<NodeIndex>, String> {
+    fn to_flow_parts(&self,
+                     inc: &mut SqlIncorporator,
+                     name: Option<String>)
+                     -> Result<(String, Vec<NodeIndex>), String> {
         // try parsing the incoming SQL
         let parsed_query = sql_parser::parse_query(self);
 
         // if ok, manufacture a node for the query structure we got
         let nodes = match parsed_query {
-            Ok(q) => Ok(inc.nodes_for_query(q)),
+            Ok(q) => {
+                match name {
+                    Some(name) => Ok(inc.nodes_for_named_query(q, name)),
+                    None => Ok(inc.nodes_for_query(q)),
+                }
+            }
             Err(e) => Err(String::from(e)),
         };
         nodes
@@ -349,21 +420,24 @@ mod tests {
         let mut inc = SqlIncorporator::new(&mut g);
 
         // Must have a base node for type inference to work, so make one manually
-        assert!("INSERT INTO users (id, name) VALUES (?, ?);".to_flow_parts(&mut inc).is_ok());
+        assert!("INSERT INTO users (id, name) VALUES (?, ?);"
+            .to_flow_parts(&mut inc, None)
+            .is_ok());
 
         // Should have two nodes: source and "users" base table
         assert_eq!(inc.graph.graph.node_count(), 2);
         assert_eq!(get_view(&inc.graph, "users").name(), "users");
 
 
-        assert!("SELECT users.id from users;".to_flow_parts(&mut inc).is_ok());
-        // Should now have source, "users" and the new selection
-        assert_eq!(inc.graph.graph.node_count(), 3);
+        assert!("SELECT users.id from users;".to_flow_parts(&mut inc, None).is_ok());
+        // Should now have source, "users", and two nodes for the new selection: one filter node
+        // and one edge view node
+        assert_eq!(inc.graph.graph.node_count(), 4);
 
         // Invalid query should fail parsing and add no nodes
-        assert!("foo bar from whatever;".to_flow_parts(&mut inc).is_err());
-        // Should still only have source, "users" and the new selection
-        assert_eq!(inc.graph.graph.node_count(), 3);
+        assert!("foo bar from whatever;".to_flow_parts(&mut inc, None).is_err());
+        // Should still only have source, "users" and the two nodes for the above selection
+        assert_eq!(inc.graph.graph.node_count(), 4);
     }
 
     #[test]
@@ -375,7 +449,9 @@ mod tests {
         let mut inc = SqlIncorporator::new(&mut g);
 
         // Establish a base write type for "users"
-        assert!("INSERT INTO users (id, name) VALUES (?, ?);".to_flow_parts(&mut inc).is_ok());
+        assert!("INSERT INTO users (id, name) VALUES (?, ?);"
+            .to_flow_parts(&mut inc, None)
+            .is_ok());
         // Should have source and "users" base table node
         assert_eq!(inc.graph.graph.node_count(), 2);
         assert_eq!(get_view(&inc.graph, "users").name(), "users");
@@ -385,7 +461,7 @@ mod tests {
 
         // Establish a base write type for "articles"
         assert!("INSERT INTO articles (id, author, title) VALUES (?, ?, ?);"
-            .to_flow_parts(&mut inc)
+            .to_flow_parts(&mut inc, None)
             .is_ok());
         // Should have source and "users" base table node
         assert_eq!(inc.graph.graph.node_count(), 3);
@@ -398,23 +474,23 @@ mod tests {
         // Try a simple equi-JOIN query
         assert!("SELECT users.name, articles.title FROM articles, users WHERE users.id = \
                  articles.author;"
-            .to_flow_parts(&mut inc)
+            .to_flow_parts(&mut inc, None)
             .is_ok());
         println!("{}", inc.graph);
         let qid = query_id_hash(&["articles", "users"],
                                 &[&Column::from("articles.author"), &Column::from("users.id")]);
         // permute node 1 (for articles)
-        let new_view1 = get_view(&inc.graph, &format!("query-{}:0", qid));
+        let new_view1 = get_view(&inc.graph, &format!("q_{:x}_n0", qid));
         assert_eq!(new_view1.args(), &["title", "author"]);
         assert_eq!(new_view1.node().unwrap().operator().description(),
                    format!("π[2, 1]"));
         // permute node 2 (for users)
-        let new_view2 = get_view(&inc.graph, &format!("query-{}:1", qid));
+        let new_view2 = get_view(&inc.graph, &format!("q_{:x}_n1", qid));
         assert_eq!(new_view2.args(), &["name", "id"]);
         assert_eq!(new_view2.node().unwrap().operator().description(),
                    format!("π[1, 0]"));
         // join node
-        let new_view3 = get_view(&inc.graph, &format!("query-{}:2", qid));
+        let new_view3 = get_view(&inc.graph, &format!("q_{:x}_n2", qid));
         assert_eq!(new_view3.args(), &["title", "author", "name", "id"]);
     }
 
@@ -427,7 +503,9 @@ mod tests {
         let mut inc = SqlIncorporator::new(&mut g);
 
         // Establish a base write type
-        assert!("INSERT INTO users (id, name) VALUES (?, ?);".to_flow_parts(&mut inc).is_ok());
+        assert!("INSERT INTO users (id, name) VALUES (?, ?);"
+            .to_flow_parts(&mut inc, None)
+            .is_ok());
         // Should have source and "users" base table node
         assert_eq!(inc.graph.graph.node_count(), 2);
         assert_eq!(get_view(&inc.graph, "users").name(), "users");
@@ -436,10 +514,10 @@ mod tests {
                    "B");
 
         // Try a simple query
-        let res = "SELECT users.name FROM users WHERE users.id = 42;".to_flow_parts(&mut inc);
+        let res = "SELECT users.name FROM users WHERE users.id = 42;".to_flow_parts(&mut inc, None);
         assert!(res.is_ok());
         let qid = query_id_hash(&["users"], &[&Column::from("users.id")]);
-        let new_view = get_view(&inc.graph, &format!("query-{}:0", qid));
+        let new_view = get_view(&inc.graph, &format!("q_{:x}_n0", qid));
         assert_eq!(new_view.args(), &["name"]);
         assert_eq!(new_view.node().unwrap().operator().description(),
                    format!("π[1]"));
