1e99b9b413d7e19e38f1cf70b6e58859392f87ba
Jon Gjengset <jon@thesquareplanet.com>
2017-01-19 13:06:55 -0500

    Share benchmarking code between getter and putter

--- benchmarks/exercise.rs
+++ benchmarks/exercise.rs
@@ -70,6 +70,74 @@ impl RuntimeConfig {
     }
 }
 
+fn driver<I, F>(start: time::Instant, config: RuntimeConfig, init: I, desc: String) -> Vec<f64>
+    where I: FnOnce() -> Box<F>,
+          F: ?Sized + FnMut(i64, i64) -> bool
+{
+    let mut count = 0usize;
+    let mut samples = Histogram::<u64>::new_with_bounds(1, 100000, 3).unwrap();
+    let mut last_reported = start;
+    let mut throughputs = Vec::new();
+
+    let mut t_rng = rand::thread_rng();
+    let mut v_rng = Rng::from_seed(42);
+    let zipf_dist = Zipf::new(1.07).unwrap();
+    let uniform_dist = Uniform::new(1.0, config.narticles as f64).unwrap();
+
+    {
+        let mut f = init();
+        while start.elapsed() < config.runtime {
+            let uid = t_rng.gen::<i64>();
+
+            // what article to vote for/retrieve?
+            let aid = match config.distribution {
+                Distribution::Uniform => uniform_dist.sample(&mut v_rng) as isize,
+                Distribution::Zipf => zipf_dist.sample(&mut v_rng) as isize,
+            };
+            let aid = std::cmp::min(aid, config.narticles - 1) as i64;
+            assert!(aid > 0);
+
+            let register = if config.cdf {
+                let t = time::Instant::now();
+                let reg = f(uid, aid);
+                let t = (dur_to_ns!(t.elapsed()) / 1000) as i64;
+                if samples.record(t).is_err() {
+                    println!("failed to record slow {} ({}ns)", desc, t);
+                }
+                reg
+            } else {
+                f(uid, aid)
+            };
+            if register {
+                count += 1;
+            }
+
+            // check if we should report
+            if last_reported.elapsed() > time::Duration::from_secs(1) {
+                let ts = last_reported.elapsed();
+                let throughput = count as f64 /
+                                 (ts.as_secs() as f64 +
+                                  ts.subsec_nanos() as f64 / 1_000_000_000f64);
+                println!("{:?} {}: {:.2}",
+                         dur_to_ns!(start.elapsed()),
+                         desc,
+                         throughput);
+                throughputs.push(throughput);
+
+                last_reported = time::Instant::now();
+                count = 0;
+            }
+        }
+    }
+
+    if config.cdf {
+        for (v, p, _, _) in samples.iter_percentiles(1) {
+            println!("percentile {} {:.2} {:.2}", desc, v, p);
+        }
+    }
+    throughputs
+}
+
 pub fn launch<B: targets::Backend + 'static>(mut target: B,
                                              mut config: RuntimeConfig)
                                              -> (Vec<f64>, Vec<Vec<f64>>) {
@@ -100,60 +168,14 @@ pub fn launch<B: targets::Backend + 'static>(mut target: B,
     // start putting
     let mut putter = Some({
         thread::spawn(move || -> Vec<f64> {
-            let mut count = 0usize;
-            let mut samples = Histogram::<u64>::new_with_bounds(1, 100000, 3).unwrap();
-            let mut last_reported = start;
-            let mut throughputs = Vec::new();
-
-            let mut t_rng = rand::thread_rng();
-            let mut v_rng = Rng::from_seed(42);
-            let zipf_dist = Zipf::new(1.07).unwrap();
-
-            {
-                let mut vote = putter.vote();
-                while start.elapsed() < config.runtime {
-                    let uniform_dist = Uniform::new(1.0, config.narticles as f64).unwrap();
-                    let vote_user = t_rng.gen::<i64>();
-                    let vote_rnd_id = match config.distribution {
-                        Distribution::Uniform => uniform_dist.sample(&mut v_rng) as isize,
-                        Distribution::Zipf => zipf_dist.sample(&mut v_rng) as isize,
-                    };
-                    let vote_rnd_id = std::cmp::min(vote_rnd_id, config.narticles - 1) as i64;
-                    assert!(vote_rnd_id > 0);
-
-                    if config.cdf {
-                        let t = time::Instant::now();
-                        vote(vote_user, vote_rnd_id);
-                        let t = (dur_to_ns!(t.elapsed()) / 1000) as i64;
-                        if samples.record(t).is_err() {
-                            println!("failed to record slow put ({}ns)", t);
-                        }
-                    } else {
-                        vote(vote_user, vote_rnd_id);
-                    }
-                    count += 1;
-
-                    // check if we should report
-                    if last_reported.elapsed() > time::Duration::from_secs(1) {
-                        let ts = last_reported.elapsed();
-                        let throughput = count as f64 /
-                                         (ts.as_secs() as f64 +
-                                          ts.subsec_nanos() as f64 / 1_000_000_000f64);
-                        println!("{:?} PUT: {:.2}", dur_to_ns!(start.elapsed()), throughput);
-                        throughputs.push(throughput);
-
-                        last_reported = time::Instant::now();
-                        count = 0;
-                    }
-                }
-            }
-
-            if config.cdf {
-                for (v, p, _, _) in samples.iter_percentiles(1) {
-                    println!("percentile PUT {:.2} {:.2}", v, p);
-                }
-            }
-            throughputs
+            let mut vote = putter.vote();
+            let init = move || {
+                Box::new(move |uid, aid| -> bool {
+                    vote(uid, aid);
+                    true
+                })
+            };
+            driver(start, config, init, "PUT".to_string())
         })
     });
 
@@ -178,57 +200,13 @@ pub fn launch<B: targets::Backend + 'static>(mut target: B,
         .map(|(i, g)| {
             println!("Starting getter #{}", i);
             thread::spawn(move || -> Vec<f64> {
-                let mut count = 0 as u64;
-                let mut samples = Histogram::<u64>::new_with_bounds(1, 100000, 3).unwrap();
-                let mut last_reported = start;
-                let mut throughputs = Vec::new();
-
-                let mut v_rng = Rng::from_seed(42);
-
-                let zipf_dist = Zipf::new(1.07).unwrap();
-                let uniform_dist = Uniform::new(1.0, config.narticles as f64).unwrap();
-
-                {
-                    let mut get = g.get();
-                    while start.elapsed() < config.runtime {
-                        // what article to vote for?
-                        let id = match config.distribution {
-                            Distribution::Uniform => uniform_dist.sample(&mut v_rng) as isize,
-                            Distribution::Zipf => zipf_dist.sample(&mut v_rng) as isize,
-                        };
-                        let id = std::cmp::min(id, config.narticles - 1) as i64;
-
-                        let t = time::Instant::now();
-                        if let Some(..) = get(id) {
-                            if config.cdf {
-                                samples += (dur_to_ns!(t.elapsed()) / 1000) as i64;
-                            }
-                            count += 1;
-                        }
-
-                        if last_reported.elapsed() > time::Duration::from_secs(1) {
-                            let ts = last_reported.elapsed();
-                            let throughput = count as f64 /
-                                             (ts.as_secs() as f64 +
-                                              ts.subsec_nanos() as f64 / 1_000_000_000f64);
-                            println!("{:?} GET{}: {:.2}",
-                                     dur_to_ns!(start.elapsed()),
-                                     i,
-                                     throughput);
-                            throughputs.push(throughput);
-
-                            last_reported = time::Instant::now();
-                            count = 0;
-                        }
-                    }
-                }
-
-                if config.cdf {
-                    for (v, p, _, _) in samples.iter_percentiles(1) {
-                        println!("percentile GET{} {:.2} {:.2}", i, v, p);
-                    }
-                }
-                throughputs
+                let mut get = g.get();
+                let init = move || {
+                    Box::new(move |_, aid| -> bool {
+                        get(aid).is_some()
+                    })
+                };
+                driver(start, config, init, format!("GET{}", i))
             })
         })
         .collect::<Vec<_>>();
