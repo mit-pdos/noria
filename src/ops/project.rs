2fde33e331401afabf953e6a7d31558839968d30
Malte Schwarzkopf <malte@csail.mit.edu>
2017-02-17 16:24:44 -0500

    Don't break the build.

--- src/ops/project.rs
+++ src/ops/project.rs
@@ -137,11 +137,11 @@ impl Ingredient for Project {
     }
 
     fn parent_columns(&self, column: usize) -> Vec<(NodeAddress, Option<usize>)> {
-        let result = if self.emit.is_some() && col >= self.emit.as_ref().unwrap().len() {
+        let result = if self.emit.is_some() && column >= self.emit.as_ref().unwrap().len() {
             None
         } else {
             Some(self.resolve_col(column))
-        }
+        };
         vec![(self.src, result)]
     }
 }
