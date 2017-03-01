57f28e1ad55137a9810c098c6594a6a421dfe551
Jon Gjengset <jon@thesquareplanet.com>
2017-02-28 19:26:55 -0500

    Multi-compound-index State (only in local for now)

--- src/flow/domain/local.rs
+++ src/flow/domain/local.rs
@@ -99,18 +99,73 @@ use fnv::FnvHashMap;
 use std::hash::Hash;
 use std::sync::Arc;
 
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum KeyType<'a, T: 'a> {
+    Single(&'a T),
+    Double(&'a (T, T)),
+    Tri(&'a (T, T, T)),
+    Quad(&'a (T, T, T, T)),
+}
+
+#[derive(Clone)]
+enum KeyedState<T: Eq + Hash> {
+    Single(FnvHashMap<T, Vec<Arc<Vec<T>>>>),
+    Double(FnvHashMap<(T, T), Vec<Arc<Vec<T>>>>),
+    Tri(FnvHashMap<(T, T, T), Vec<Arc<Vec<T>>>>),
+    Quad(FnvHashMap<(T, T, T, T), Vec<Arc<Vec<T>>>>),
+}
+
+impl<T: Eq + Hash> KeyedState<T> {
+    pub fn is_empty(&self) -> bool {
+        match *self {
+            KeyedState::Single(ref m) => m.is_empty(),
+            KeyedState::Double(ref m) => m.is_empty(),
+            KeyedState::Tri(ref m) => m.is_empty(),
+            KeyedState::Quad(ref m) => m.is_empty(),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        match *self {
+            KeyedState::Single(ref m) => m.len(),
+            KeyedState::Double(ref m) => m.len(),
+            KeyedState::Tri(ref m) => m.len(),
+            KeyedState::Quad(ref m) => m.len(),
+        }
+    }
+
+    pub fn lookup(&self, key: KeyType<T>) -> Option<&Vec<Arc<Vec<T>>>> {
+        match (*self, key) {
+            (KeyedState::Single(ref m), KeyType::Single(k)) => m.get(k),
+            (KeyedState::Double(ref m), KeyType::Double(k)) => m.get(k),
+            (KeyedState::Tri(ref m), KeyType::Tri(k)) => m.get(k),
+            (KeyedState::Quad(ref m), KeyType::Quad(k)) => m.get(k),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl<'a, T: Eq + Hash> Into<KeyedState<T>> for &'a [usize] {
+    fn into(self) -> KeyedState<T> {
+        match self.len() {
+            0 => unreachable!(),
+            1 => KeyedState::Single(FnvHashMap::default()),
+            2 => KeyedState::Double(FnvHashMap::default()),
+            3 => KeyedState::Tri(FnvHashMap::default()),
+            4 => KeyedState::Quad(FnvHashMap::default()),
+            _ => unimplemented!(),
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct State<T: Hash + Eq + Clone> {
-    pkey: usize,
-    state: FnvHashMap<T, Vec<Arc<Vec<T>>>>,
+    state: Vec<(Vec<usize>, KeyedState<T>)>,
 }
 
 impl<T: Hash + Eq + Clone> Default for State<T> {
     fn default() -> Self {
-        State {
-            pkey: usize::max_value(),
-            state: FnvHashMap::default(),
-        }
+        State { state: Vec::new() }
     }
 }
 
@@ -120,58 +175,136 @@ impl<T: Hash + Eq + Clone> State<T> {
         Self::default()
     }
 
-    pub fn set_pkey(&mut self, key: usize) {
-        if self.pkey != usize::max_value() && self.pkey != key {
-            unreachable!("asked to index {} when already indexing {}", key, self.pkey);
-        }
-        self.pkey = key;
+    fn state_for(&self, cols: &[usize]) -> Option<usize> {
+        self.state.iter().position(|s| &s.0[..] == cols)
     }
 
-    pub fn get_pkey(&self) -> usize {
-        self.pkey
+    pub fn add_key(&mut self, columns: &[usize]) {
+        if self.state_for(columns).is_some() {
+            // already keyed
+            return;
+        }
+
+        if !self.state.is_empty() || !self.state[0].1.is_empty() {
+            // we'd need to *construct* the index!
+            unimplemented!();
+        }
+
+        self.state.push((Vec::from(columns), columns.into()));
+    }
+
+    pub fn get_keys(&self) -> Vec<Vec<usize>> {
+        self.state.iter().map(|s| &s.0).cloned().collect()
     }
 
     pub fn is_useful(&self) -> bool {
-        self.pkey != usize::max_value()
+        !self.state.is_empty()
     }
 
     pub fn insert(&mut self, r: Arc<Vec<T>>) {
-        let k = self.pkey;
-        // i *wish* we could use the entry API here, but it would mean an extra clone in the common
-        // case of an entry already existing for the given key...
-        if let Some(ref mut rs) = self.state.get_mut(&r[k]) {
-            rs.push(r);
-            return;
+        for s in &self.state {
+            match s.1 {
+                KeyedState::Single(ref mut map) => {
+                    // treat this specially to avoid the extra Vec
+                    debug_assert_eq!(s.0.len(), 1);
+                    // i *wish* we could use the entry API here, but it would mean an extra clone
+                    // in the common case of an entry already existing for the given key...
+                    if let Some(ref mut rs) = map.get_mut(&r[s.0[0]]) {
+                        rs.push(r);
+                        return;
+                    }
+                    map.insert(r[s.0[0]].clone(), vec![r]);
+                }
+                _ => {
+                    match s.1 {
+                        KeyedState::Double(ref mut map) => {
+                            let key = (r[s.0[0]].clone(), r[s.0[1]].clone());
+                            map.entry(key).or_insert_with(Vec::new).push(r)
+                        }
+                        KeyedState::Tri(ref mut map) => {
+                            let key = (r[s.0[0]].clone(), r[s.0[1]].clone(), r[s.0[2]].clone());
+                            map.entry(key).or_insert_with(Vec::new).push(r)
+                        }
+                        KeyedState::Quad(ref mut map) => {
+                            let key = (r[s.0[0]].clone(),
+                                       r[s.0[1]].clone(),
+                                       r[s.0[2]].clone(),
+                                       r[s.0[3]].clone());
+                            map.entry(key).or_insert_with(Vec::new).push(r)
+                        }
+                        KeyedState::Single(..) => unreachable!(),
+                    }
+                }
+            }
         }
-        self.state.insert(r[k].clone(), vec![r]);
     }
 
     pub fn remove(&mut self, r: &[T]) {
-        let k = self.pkey;
-        if let Some(ref mut rs) = self.state.get_mut(&r[k]) {
-            rs.retain(|rsr| &rsr[..] != r);
+        for s in &self.state {
+            match s.1 {
+                KeyedState::Single(ref mut map) => {
+                    if let Some(ref mut rs) = map.get_mut(&r[s.0[0]]) {
+                        rs.retain(|rsr| &rsr[..] != r);
+                    }
+                }
+                _ => {
+                    match s.1 {
+                        KeyedState::Double(ref mut map) => {
+                            // TODO: can we avoid the Clone here?
+                            let key = (r[s.0[0]].clone(), r[s.0[1]].clone());
+                            if let Some(ref mut rs) = map.get_mut(&key) {
+                                rs.retain(|rsr| &rsr[..] != r);
+                            }
+                        }
+                        KeyedState::Tri(ref mut map) => {
+                            let key = (r[s.0[0]].clone(), r[s.0[1]].clone(), r[s.0[2]].clone());
+                            if let Some(ref mut rs) = map.get_mut(&key) {
+                                rs.retain(|rsr| &rsr[..] != r);
+                            }
+                        }
+                        KeyedState::Quad(ref mut map) => {
+                            let key = (r[s.0[0]].clone(),
+                                       r[s.0[1]].clone(),
+                                       r[s.0[2]].clone(),
+                                       r[s.0[3]].clone());
+                            if let Some(ref mut rs) = map.get_mut(&key) {
+                                rs.retain(|rsr| &rsr[..] != r);
+                            }
+                        }
+                        KeyedState::Single(..) => unreachable!(),
+                    }
+                }
+            }
         }
     }
 
     pub fn iter(&self) -> hash_map::Values<T, Vec<Arc<Vec<T>>>> {
-        self.state.values()
+        for &(ref cols, ref state) in &self.state {
+            if let KeyedState::Single(ref map) = *state {
+                return map.values();
+            }
+        }
+        // TODO: allow iter without single key (breaks return type)
+        unimplemented!();
     }
 
     pub fn is_empty(&self) -> bool {
-        self.state.is_empty()
+        self.state.is_empty() || self.state[0].1.is_empty()
     }
 
     pub fn len(&self) -> usize {
-        self.state.len()
+        if self.state.is_empty() {
+            0
+        } else {
+            self.state[0].1.len()
+        }
     }
 
-    pub fn lookup(&self, key: usize, value: &T) -> &[Arc<Vec<T>>] {
-        debug_assert_ne!(self.pkey,
-                         usize::max_value(),
-                         "lookup on uninitialized index");
-        debug_assert_eq!(key, self.pkey);
-        if let Some(rs) = self.state.get(value) {
-            rs
+    pub fn lookup(&self, columns: &[usize], key: KeyType<T>) -> &[Arc<Vec<T>>] {
+        debug_assert!(!self.state.is_empty(), "lookup on uninitialized index");
+        let state = &self.state[self.state_for(columns).expect("lookup on non-indexed column set")];
+        if let Some(rs) = state.1.lookup(key) {
+            &rs[..]
         } else {
             &[]
         }
@@ -182,6 +315,12 @@ impl<T: Hash + Eq + Clone> IntoIterator for State<T> {
     type Item = <FnvHashMap<T, Vec<Arc<Vec<T>>>> as IntoIterator>::Item;
     type IntoIter = <FnvHashMap<T, Vec<Arc<Vec<T>>>> as IntoIterator>::IntoIter;
     fn into_iter(self) -> Self::IntoIter {
-        self.state.into_iter()
+        for (_, state) in self.state {
+            if let KeyedState::Single(map) = state {
+                return map.into_iter();
+            }
+        }
+        // TODO: allow into_iter without single key (breaks Self::IntoIter type)
+        unimplemented!();
     }
 }
