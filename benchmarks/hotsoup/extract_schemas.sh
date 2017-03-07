90f4b7c9f62be7e6ed5834df8384d9174472e7e4
Malte Schwarzkopf <malte@csail.mit.edu>
2017-03-06 20:05:39 -0500

    Script to extract all HotCRP base schemas ever!

---
+++ benchmarks/hotsoup/extract_schemas.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+repo="https://github.com/kohler/hotcrp.git"
+
+mkdir -p ${dir}/schemas
+
+git clone ${repo} /tmp/hotcrp
+cd /tmp/hotcrp
+
+prev_schema_ver=99999999
+
+for l in $(git log --oneline --follow src/schema.sql | cut -d' ' -f1); do
+  # HotCRP schema versioning started in 80eeb70
+  if [[ $l == "80eeb70" ]]; then
+    break
+  fi
+
+  git checkout $l
+
+  schema_file=$(find . -name schema.sql)
+
+  res=$(grep "values ('allowPaperOption" ${schema_file})
+  cur_schema_ver=$(echo ${res} | cut -d' ' -f 8 | sed -e "s/);//" | tr -d [:space:])
+
+  if [[ ${cur_schema_ver} -lt ${prev_schema_ver} ]]; then
+    # we have advanced to a new schema
+    cp ${schema_file} ${dir}/schemas/hotcrp_${cur_schema_ver}.sql
+    echo "schema ${prev_schema_ver} -> ${cur_schema_ver}"
+    prev_schema_ver=${cur_schema_ver}
+  elif [[ ${cur_schema_ver} -gt ${prev_schema_ver} ]]; then
+    echo "SCHEMA WENT BACKWARDS: ${cur_schema_ver} > ${prev_schema_ver} @ ${l}"
+    exit 1
+  fi
+done
+
+rm -rf /tmp/hotcrp
