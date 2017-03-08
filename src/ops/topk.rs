4f371eeb1f9be378daf959382bc569382de168cf
Jonathan Behrens <behrensj@mit.edu>
2017-03-08 14:33:18 -0500

    Implemented Top K operator

--- src/ops/topk.rs
+++ src/ops/topk.rs
@@ -1,10 +1,8 @@
-use ops;
-use query;
 
-use std::fmt;
-use std::collections::HashSet;
+use flow::data::DataType;
+
+use std::mem;
 use std::collections::HashMap;
-use std::sync;
 use std::sync::Arc;
 
 use flow::prelude::*;
@@ -17,32 +15,28 @@ pub struct TopK {
     us: Option<NodeAddress>,
     cols: usize,
 
-    pkey_in: usize, // column in our input that is our primary key
-    pkey_out: usize, // column in our output that is our primary key
-
     // precomputed datastructures
-    group: HashSet<usize>,
-    colfix: Vec<usize>,
+    group_by: Vec<usize>,
 
     // column to use for ordering
     over: usize,
     k: usize,
 
-    counts: HashMap<Vec<query::DataType>, usize>,
+    counts: HashMap<Vec<DataType>, usize>,
 }
 
 impl TopK {
-    pub fn new(src: NodeAddress, over: usize, group: Vec<usize>, k: usize) -> Self {
+    pub fn new(src: NodeAddress, over: usize, group_by: Vec<usize>, k: usize) -> Self {
+        let mut group_by = group_by;
+        group_by.sort();
+
         TopK {
             src: src,
 
-            pkey_out: usize::max_value(),
-            pkey_in: usize::max_value(),
-
             us: None,
             cols: 0,
-            group: group.into_iter().collect(),
-            colfix: Vec::new(),
+
+            group_by: group_by,
 
             over: over,
             k: k,
@@ -52,50 +46,69 @@ impl TopK {
     }
 
     /// Returns the set of Record structs to be emitted by this node, for some group. In steady
-    /// state operation this will include some number of positives (at most k), and the same number
+    /// state operation this will typically include some number of positives (at most k), and the same number
     /// of negatives.
-    fn apply(&self, count: usize, current_topk: &[Arc<Vec<query::DataType>>], new: Vec<Record>) -> Vec<Record> {
-        let mut extreme = current_topk.iter().map(|r|{(r, true)});
+    fn apply(&self, new_count: usize, current_topk: &[Arc<Vec<DataType>>], new: Vec<Record>) -> Vec<Record> {
+        if new_count <= self.k {
+            return new
+        }
 
-        // // Extreme values are those that are at least as extreme as the current min/max (if any).
-        // // let mut is_extreme_value : Box<Fn(i64) -> bool> = Box::new(|_|true);
-        // let mut extreme_values: Vec<i64> = vec![];
-        // if let Some(&query::DataType::Number(n)) = current {
-        //     extreme_values.push(n);
-        // };
+        let cmp_rows = |a: &Arc<Vec<DataType>>, b: &Arc<Vec<DataType>>|{
+            a[self.over].cmp(&b[self.over])
+        };
 
-        // let is_extreme_value = |x: i64| if let Some(&query::DataType::Number(n)) = current {
-        //     match self.op {
-        //         Extremum::MAX => x >= n,
-        //         Extremum::MIN => x <= n,
-        //     }
-        // } else {
-        //     true
-        // };
+        let mut delta = Vec::new();
+        let mut current:Vec<_> = current_topk.iter().cloned().collect();
+        current.sort_by(&cmp_rows);
+        for r in new.iter() {
+            if let &Record::Negative(ref a) = r {
+                let idx = current.binary_search_by_key(&&a[self.over], |arc|&arc[self.over]);
+                if let Ok(idx) = idx {
+                    current.remove(idx);
+                    delta.push(r.clone())
+                }
+            }
+        }
 
-        // for d in diffs {
-        //     match d {
-        //         DiffType::Insert(v) if is_extreme_value(v) => extreme_values.push(v),
-        //         DiffType::Remove(v) if is_extreme_value(v) => {
-        //             if let Some(i) = extreme_values.iter().position(|x: &i64| *x == v) {
-        //                 extreme_values.swap_remove(i);
-        //             }
-        //         }
-        //         _ => {}
-        //     };
-        // }
+        let mut output_rows:Vec<_> = current.into_iter().map(|a|(a,true)).collect();
+        output_rows.extend(new.into_iter().filter_map(|r|{match r {
+            Record::Positive(a) => Some((a,false)),
+            _ => None,
+        }}));
+        output_rows.sort_by(|a,b| cmp_rows(&a.0, &b.0));
 
-        // let extreme = match self.op {
-        //     Extremum::MIN => extreme_values.into_iter().min(),
-        //     Extremum::MAX => extreme_values.into_iter().max(),
-        // };
+        if output_rows.len() > self.k {
+            // Remove the topk elements from `output_rows`, splitting them off into `rows`. Then
+            // swap and rename so that `output_rows` contains the top K elements, and `bottom_rows`
+            // contains the rest.
+            let i = output_rows.len() - self.k;
+            let mut rows = output_rows.split_off(i);
+            mem::swap(&mut output_rows, &mut rows);
+            let bottom_rows = rows;
 
-        // if let Some(extreme) = extreme {
-        //     return extreme.into();
-        // }
+            // Emit negatives for any elements in `bottom_rows` that were originally in current_topk.
+            delta.extend(bottom_rows.into_iter().filter_map(|p| {
+                if p.1 {
+                    Some(Record::Negative(p.0))
+                } else {
+                    None
+                }
+            }));
+        } else if output_rows.len() < self.k && new_count >= self.k {
+            // TODO: handle this case by querying into the parent.
+            unimplemented!();
+        }
 
-        // TODO: handle this case by querying into the parent.
-        unimplemented!();
+        // Emit positives for any elements in `output_rows` that weren't originally in current_topk.
+        delta.extend(output_rows.into_iter().filter_map(|p| {
+            if !p.1 {
+                Some(Record::Positive(p.0))
+            } else {
+                None
+            }
+        }));
+
+        delta
     }
 }
 
@@ -119,35 +132,9 @@ impl Ingredient for TopK {
     fn on_connected(&mut self, g: &Graph) {
         let srcn = &g[*self.src.as_global()];
         assert!(self.over < srcn.fields().len(),
-                "cannot aggregate over non-existing column");
+                "cannot compute top K over non-existing column");
 
-        // group by all columns
         self.cols = srcn.fields().len();
-        if self.group.len() != 1 {
-            unimplemented!();
-        }
-        // primary key is the first (and only) group by key
-        self.pkey_in = *self.group.iter().next().unwrap();
-        // what output column does this correspond to?
-        // well, the first one given that we currently only have one group by
-        // and that group by comes first.
-        self.pkey_out = 0;
-
-        // build a translation mechanism for going from output columns to input columns
-        let colfix: Vec<_> = (0..self.cols)
-            .into_iter()
-            .filter_map(|col| {
-                if self.group.contains(&col) {
-                    // since the generated value goes at the end,
-                    // this is the n'th output value
-                    Some(col)
-                } else {
-                    // this column does not appear in output
-                    None
-                }
-            })
-            .collect();
-        self.colfix.extend(colfix.into_iter());
     }
 
     fn on_commit(&mut self, us: NodeAddress, remap: &HashMap<NodeAddress, NodeAddress>) {
@@ -177,7 +164,7 @@ impl Ingredient for TopK {
         for rec in rs.iter() {
             let group = rec.iter()
                 .enumerate()
-                .filter_map(|(i, v)| if self.group.contains(&i) {
+                .filter_map(|(i, v)| if self.group_by.iter().any(|col| col == &i) {
                     Some(v)
                 } else {
                     None
@@ -193,7 +180,8 @@ impl Ingredient for TopK {
             // find the current value for this group
             let db = state.get(self.us.as_ref().unwrap().as_local())
                 .expect("grouped operators must have their own state materialized");
-            let old_rs = db.lookup(self.pkey_out, &group[self.pkey_in]);
+
+            let old_rs = db.lookup(&self.group_by[..], &KeyType::from(&group[..]));
 
             // Retrieve then update the number of times in this group
             let count = *self.counts.get(&group).unwrap_or(&0);
@@ -205,68 +193,90 @@ impl Ingredient for TopK {
             self.counts.insert(group, ((count as i64) + count_diff) as usize);
             assert!(count >= old_rs.len());
 
-            out.append(&mut self.apply(count, old_rs, diffs));
-            // match current {
-            //     None => {
-            //         // emit positive, which is group + new.
-            //         let rec: Vec<_> = group.into_iter()
-            //             .filter_map(|v| v)
-            //             .cloned()
-            //             .chain(Some(new.into()).into_iter())
-            //             .collect();
-            //         out.push(ops::Record::Positive(sync::Arc::new(rec)));
-            //     }
-            //     Some(ref current) if new == **current => {
-            //         // no change
-            //     }
-            //     Some(current) => {
-            //         // construct prefix of output record used for both - and +
-            //         let mut rec = Vec::with_capacity(group.len() + 1);
-            //         rec.extend(group.into_iter().filter_map(|v| v).cloned());
-
-            //         // revoke old value
-            //         if old.is_none() {
-            //             // we're generating a zero row
-            //             // revoke old value
-            //             rec.push(current.into_owned());
-            //             out.push(ops::Record::Negative(sync::Arc::new(rec.clone())));
-
-            //             // remove the old value from the end of the record
-            //             rec.pop();
-            //         } else {
-            //             out.push(ops::Record::Negative(old.unwrap().clone()));
-            //         }
-
-            //         // emit new value
-            //         rec.push(new.into());
-            //         out.push(ops::Record::Positive(sync::Arc::new(rec)));
-            //     }
-            // }
+            out.append(&mut self.apply(count + count_diff as usize, old_rs, diffs));
         }
 
         out.into()
     }
 
-    fn suggest_indexes(&self, this: NodeAddress) -> HashMap<NodeAddress, usize> {
-        // index by our primary key
-        Some((this, self.pkey_out)).into_iter().collect()
+    fn suggest_indexes(&self, this: NodeAddress) -> HashMap<NodeAddress, Vec<usize>> {
+        Some((this, self.group_by.clone())).into_iter().collect()
     }
 
     fn resolve(&self, col: usize) -> Option<Vec<(NodeAddress, usize)>> {
-        if col == self.cols - 1 {
-            return None;
-        }
-        Some(vec![(self.src, self.colfix[col])])
+        Some(vec![(self.src, col)])
     }
 
     fn description(&self) -> String {
         format!("TopK({})", self.over)
     }
 
-    fn parent_columns(&self, column: usize) -> Vec<(NodeAddress, Option<usize>)> {
-        if column == self.cols - 1 {
-            return vec![(self.src, None)];
-        }
-        vec![(self.src, Some(self.colfix[column]))]
+    fn parent_columns(&self, col: usize) -> Vec<(NodeAddress, Option<usize>)> {
+        vec![(self.src, Some(col))]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use ops;
+
+    fn setup() -> ops::test::MockGraph {
+        let mut g = ops::test::MockGraph::new();
+        let s = g.add_base("source", &["x", "y", "z"]);
+        g.set_op("topk", &["x", "y", "z"], TopK::new(s, 2, vec![1], 3), true);
+        g
+    }
+
+    #[test]
+    fn it_forwards() {
+        let mut g = setup();
+
+        let r12: Vec<DataType> = vec![1.into(), "z".into(), 12.into()];
+        let r10: Vec<DataType> = vec![2.into(), "z".into(), 10.into()];
+        let r11: Vec<DataType> = vec![3.into(), "z".into(), 11.into()];
+        let r5: Vec<DataType> = vec![4.into(), "z".into(), 5.into()];
+        let r15: Vec<DataType> = vec![5.into(), "z".into(), 15.into()];
+
+        let a = g.narrow_one_row(r12.clone(), true);
+        assert_eq!(a, vec![r12].into());
+
+        let a = g.narrow_one_row(r10.clone(), true);
+        assert_eq!(a, vec![r10.clone()].into());
+
+        let a = g.narrow_one_row(r11.clone(), true);
+        assert_eq!(a, vec![r11].into());
+
+        let a = g.narrow_one_row(r5.clone(), true);
+        assert_eq!(a.len(), 0);
+
+        let a = g.narrow_one_row(r15.clone(), true);
+        assert_eq!(a, vec![(r10, false), (r15, true)].into());
+    }
+
+    #[test]
+    fn it_suggests_indices() {
+        let g = setup();
+        let me = NodeAddress::mock_global(1.into());
+        let idx = g.node().suggest_indexes(me);
+        assert_eq!(idx.len(), 1);
+        assert_eq!(*idx.iter().next().unwrap().1, vec![1])
+    }
+
+    #[test]
+    fn it_resolves() {
+        let g = setup();
+        assert_eq!(g.node().resolve(0), Some(vec![(g.narrow_base_id(), 0)]));
+        assert_eq!(g.node().resolve(1), Some(vec![(g.narrow_base_id(), 1)]));
+        assert_eq!(g.node().resolve(2), Some(vec![(g.narrow_base_id(), 2)]));
+    }
+
+    #[test]
+    fn it_parent_columns() {
+        let g = setup();
+        assert_eq!(g.node().resolve(0), Some(vec![(g.narrow_base_id(), 0)]));
+        assert_eq!(g.node().resolve(1), Some(vec![(g.narrow_base_id(), 1)]));
+        assert_eq!(g.node().resolve(2), Some(vec![(g.narrow_base_id(), 2)]));
     }
 }
