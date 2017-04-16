c3d64c1734b722ce4f58b56f95efd3075913e433
Malte Schwarzkopf <malte@csail.mit.edu>
2017-04-15 23:03:58 -0400

    Log a bit more about reuse

--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -174,6 +174,9 @@ impl SqlIncorporator {
             if existing_qg
                    .signature()
                    .is_generalization_of(&qg.signature()) {
+                trace!(self.log,
+                       "Checking reuse compatibility of {:#?}",
+                       existing_qg);
                 match reuse::check_compatibility(&qg, existing_qg) {
                     Some(reuse) => {
                         // QGs are compatible, we can reuse `existing_qg` as part of `qg`!
@@ -394,6 +397,7 @@ impl SqlIncorporator {
                         }
                     }
                     QueryGraphReuse::ExtendExisting(mq) => {
+                        trace!(self.log, "Extending existing query!");
                         self.extend_existing_query(&query_name, sq, qg, mq, mig)
                     }
                     QueryGraphReuse::ReaderOntoExisting(mn, params) => {
