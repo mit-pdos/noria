c95c811513290e60718a626614ba2464af5feac1
Jonathan Behrens <fintelia@gmail.com>
2017-06-06 15:21:05 -0400

    Fix formatting

--- benchmarks/vote/vote-server.rs
+++ benchmarks/vote/vote-server.rs
@@ -26,9 +26,7 @@ fn main() {
 
     let addr = args.value_of("ADDR").unwrap();
     println!("Attempting to start soup on {}", addr);
-    let g = graph::make(true,
-                        false,
-                        Some((512, time::Duration::from_millis(10))));
+    let g = graph::make(true, false, Some((512, time::Duration::from_millis(10))));
 
     // start processing
     // TODO: what about the node indices?
