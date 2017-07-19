681aa568c9101e4dad026752b6d4b2e5a775c926
Jonathan Behrens <fintelia@gmail.com>
2017-07-19 15:03:13 -0400

    No need to loop on try_recv inside polling loop

--- channel/src/poll.rs
+++ channel/src/poll.rs
@@ -63,11 +63,6 @@ where
     }
 
     /// Execute steps of the polling loop until process_event() returns `StopPolling`.
-    ///
-    /// Note that to prevent packet loss, if multiple packets are retrieved when polling a channel
-    /// then process_event will be called with each in turn. This will happen regardless of the
-    /// value returned by that function. However, once the queue has been drained, the polling loop
-    /// will stop if the return value from any of the packets was `StopPolling`
     pub fn run_polling_loop<F>(&mut self, mut process_event: F)
     where
         F: FnMut(PollEvent<T>) -> ProcessResult,
@@ -100,34 +95,26 @@ where
                     continue;
                 }
 
-                let mut stop_polling = false;
-                loop {
-                    let recv = self.channels[event.token().0 - CHANNEL_OFFSET]
-                        .as_mut()
-                        .unwrap()
-                        .try_recv();
+                let recv = self.channels[event.token().0 - CHANNEL_OFFSET]
+                    .as_mut()
+                    .unwrap()
+                    .try_recv();
 
-                    match recv {
-                        Ok(message) => {
-                            if let StopPolling = process_event(PollEvent::Process(message)) {
-                                stop_polling = true;
-                            }
+                match recv {
+                    Ok(message) => {
+                        if let StopPolling = process_event(PollEvent::Process(message)) {
+                            return;
                         }
-                        Err(TryRecvError::Disconnected) => {
-                            let channel = self.channels[event.token().0 - CHANNEL_OFFSET].take();
-                            let _ = self.poll.deregister(&channel.unwrap());
-                            break;
-                        }
-                        Err(TryRecvError::Empty) => break,
-                        Err(TryRecvError::DeserializationError) => unreachable!(),
                     }
-                }
-
-                if stop_polling {
-                    return;
+                    Err(TryRecvError::Disconnected) => {
+                        let channel = self.channels[event.token().0 - CHANNEL_OFFSET].take();
+                        let _ = self.poll.deregister(&channel.unwrap());
+                        break;
+                    }
+                    Err(TryRecvError::Empty) => break,
+                    Err(TryRecvError::DeserializationError) => unreachable!(),
                 }
             }
         }
-
     }
 }
