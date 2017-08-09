c66f6fe4c53e530685a827095c3cee5dcddda2d8
Malte Schwarzkopf <malte@csail.mit.edu>
2017-08-09 18:03:58 -0400

    New, improved DD RPC client
    It talks to the server, though in a one-sided and not particularly
    useful way, as the server currently can't respond and doesn't yet do
    the computation.

--- benchmarks/vote/clients/differential_dataflow.rs
+++ benchmarks/vote/clients/differential_dataflow.rs
@@ -9,65 +9,85 @@ use std::net::TcpStream;
 use net2::TcpBuilder;
 use bufstream::BufStream;
 
-const ARTICLE_NODE: usize = 1;
-const VOTE_NODE: usize = 2;
 const END_NODE: usize = 4;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum Method {
+pub enum Rpc {
     /// Query the given `view` for all records whose key column matches the given value.
     Query {
         /// The view to query
         view: usize,
         /// The key value to use for the given query's free parameter
-        key: u64,
+        key: i64,
     },
 
-    /// Insert a new record into the given view.
+    /// Insert a new `article` record.
     ///
     /// `args` gives the column values for the new record.
-    Insert {
-        /// The view to insert into
-        view: usize,
-        /// The column values for the new record
-        args: Vec<u64>,
+    InsertArticle {
+        /// Article ID.
+        aid: i64,
+        /// Article title.
+        title: String,
+    },
+
+    /// Insert a new `vote` record.
+    ///
+    /// `args` gives the column values for the new record.
+    InsertVote {
+        /// Article ID.
+        aid: i64,
+        /// User ID.
+        uid: i64,
     },
 
     /// Flush any buffered responses.
     Flush,
 }
 
-impl Abomonation for Method {
+impl Abomonation for Rpc {
     #[inline]
     unsafe fn embalm(&mut self) {
-        if let Method::Query { mut view, mut key } = *self {
+        if let Rpc::Query { mut view, mut key } = *self {
             view.embalm();
             key.embalm();
         }
-        if let Method::Insert {
-            ref mut view,
-            ref mut args,
+        if let Rpc::InsertArticle {
+            ref mut aid,
+            ref mut title,
         } = *self
         {
-            view.embalm();
-            args.embalm();
+            aid.embalm();
+            title.embalm();
+        }
+        if let Rpc::InsertVote {
+            ref mut aid,
+            ref mut uid,
+        } = *self
+        {
+            aid.embalm();
+            uid.embalm();
         }
     }
     #[inline]
     unsafe fn entomb(&self, bytes: &mut Vec<u8>) {
-        if let Method::Query { ref view, ref key } = *self {
+        if let Rpc::Query { ref view, ref key } = *self {
             view.entomb(bytes);
             key.entomb(bytes);
         }
-        if let Method::Insert { ref view, ref args } = *self {
-            view.entomb(bytes);
-            args.entomb(bytes);
+        if let Rpc::InsertArticle { ref aid, ref title } = *self {
+            aid.entomb(bytes);
+            title.entomb(bytes);
+        }
+        if let Rpc::InsertVote { ref aid, ref uid } = *self {
+            aid.entomb(bytes);
+            uid.entomb(bytes);
         }
     }
     #[inline]
     unsafe fn exhume<'a, 'b>(&'a mut self, mut bytes: &'b mut [u8]) -> Option<&'b mut [u8]> {
         match *self {
-            Method::Query {
+            Rpc::Query {
                 ref mut view,
                 ref mut key,
             } => {
@@ -75,30 +95,48 @@ impl Abomonation for Method {
                 key.exhume(bytes);
                 Some(bytes)
             }
-            Method::Insert {
-                ref mut view,
-                ref mut args,
+            Rpc::InsertArticle {
+                ref mut aid,
+                ref mut title,
             } => {
                 let temp = bytes;
-                bytes = if let Some(bytes) = view.exhume(temp) {
+                bytes = if let Some(bytes) = aid.exhume(temp) {
                     bytes
                 } else {
                     return None;
                 };
                 let temp = bytes;
-                bytes = if let Some(bytes) = args.exhume(temp) {
+                bytes = if let Some(bytes) = title.exhume(temp) {
                     bytes
                 } else {
                     return None;
                 };
                 Some(bytes)
             }
-            Method::Flush => Some(bytes),
+            Rpc::InsertVote {
+                ref mut aid,
+                ref mut uid,
+            } => {
+                let temp = bytes;
+                bytes = if let Some(bytes) = aid.exhume(temp) {
+                    bytes
+                } else {
+                    return None;
+                };
+                let temp = bytes;
+                bytes = if let Some(bytes) = uid.exhume(temp) {
+                    bytes
+                } else {
+                    return None;
+                };
+                Some(bytes)
+            }
+            Rpc::Flush => Some(bytes),
         }
     }
 }
 
-fn serialize(method: &Method, stream: &mut BufStream<TcpStream>) -> Result<usize, io::Error> {
+fn serialize(method: &Rpc, stream: &mut BufStream<TcpStream>) -> Result<usize, io::Error> {
     let mut buf = Vec::new();
     unsafe {
         encode(method, &mut buf);
@@ -108,28 +146,56 @@ fn serialize(method: &Method, stream: &mut BufStream<TcpStream>) -> Result<usize
 
 pub struct C(BufStream<TcpStream>);
 impl C {
-    pub fn mput(&mut self, view: usize, mut data: Vec<Vec<DataType>>) {
+    pub fn put_articles(&mut self, mut data: Vec<(i64, String)>) {
         let &mut C(ref mut bs) = self;
         //let n = data.len();
-        let mut method = Method::Insert {
-            view,
-            args: Vec::new(),
+        let mut method = Rpc::InsertArticle {
+            aid: 0,
+            title: String::new(),
         };
         for r in &mut data {
-            let mut rr: Vec<u64> = r.iter()
-                .map(|d| match *d {
-                    DataType::BigInt(i) => i as u64,
-                    DataType::Int(i) => i as u64,
-                    _ => 0u64,
-                })
-                .collect();
-            if let Method::Insert { ref mut args, .. } = method {
+            let mut a_id = &mut r.0;
+            let mut a_title = &mut r.1;
+
+            if let Rpc::InsertArticle {
+                ref mut aid,
+                ref mut title,
+            } = method
+            {
                 use std::mem;
-                mem::swap(args, &mut rr);
+                mem::swap(aid, &mut a_id);
+                mem::swap(title, &mut a_title);
             }
             serialize(&method, bs).unwrap();
         }
-        let m = Method::Flush;
+        let m = Rpc::Flush;
+        serialize(&m, bs).unwrap();
+        bs.flush().unwrap();
+        /*for _ in 0..n {
+            let _: i64 = bincode::deserialize_from(bs, bincode::Infinite).unwrap();
+        }*/
+    }
+
+    pub fn put_votes(&mut self, mut data: Vec<(i64, i64)>) {
+        let &mut C(ref mut bs) = self;
+        //let n = data.len();
+        let mut method = Rpc::InsertVote { aid: 0, uid: 0 };
+        for r in &mut data {
+            let mut v_aid = &mut r.0;
+            let mut v_uid = &mut r.1;
+
+            if let Rpc::InsertVote {
+                ref mut aid,
+                ref mut uid,
+            } = method
+            {
+                use std::mem;
+                mem::swap(aid, &mut v_aid);
+                mem::swap(uid, &mut v_uid);
+            }
+            serialize(&method, bs).unwrap();
+        }
+        let m = Rpc::Flush;
         serialize(&m, bs).unwrap();
         bs.flush().unwrap();
         /*for _ in 0..n {
@@ -144,20 +210,20 @@ impl C {
     ) -> Result<Vec<Vec<Vec<DataType>>>, io::Error> {
         let &mut C(ref mut bs) = self;
         let n = keys.len();
-        let mut method = Method::Query { view, key: 0u64 };
+        let mut method = Rpc::Query { view, key: 0 };
         for q_key in &mut keys {
             let mut q_key = match *q_key {
-                DataType::Int(i) => i as u64,
-                DataType::BigInt(i) => i as u64,
+                DataType::Int(i) => i as i64,
+                DataType::BigInt(i) => i as i64,
                 _ => unreachable!(),
             };
-            if let Method::Query { ref mut key, .. } = method {
+            if let Rpc::Query { ref mut key, .. } = method {
                 use std::mem;
                 mem::swap(key, &mut q_key);
             }
             serialize(&method, bs)?;
         }
-        let m = Method::Flush;
+        let m = Rpc::Flush;
         serialize(&m, bs)?;
         bs.flush()?;
         /*Ok(
@@ -190,18 +256,14 @@ impl Writer for C {
         I: Iterator<Item = (i64, String)>,
         I: ExactSizeIterator,
     {
-        let articles = articles
-            .map(|(aid, title)| vec![aid.into(), title.into()])
-            .collect();
-        self.mput(ARTICLE_NODE, articles);
+        let articles = articles.collect();
+        self.put_articles(articles);
     }
     fn vote(&mut self, ids: &[(i64, i64)]) -> Period {
         let votes = ids.iter()
-            .map(|&(user_id, article_id)| {
-                vec![user_id.into(), article_id.into()]
-            })
+            .map(|&(user_id, article_id)| (user_id, article_id))
             .collect();
-        self.mput(VOTE_NODE, votes);
+        self.put_votes(votes);
         Period::PreMigration
     }
 }
