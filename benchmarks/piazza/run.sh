1a0fd0dfe1c84b357d3874b7d841db77bbbfa506
Lara Timbo <laratimbo7@gmail.com>
2017-10-18 18:39:37 -0400

    directory for results

--- benchmarks/piazza/run.sh
+++ benchmarks/piazza/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir $1
 case "$OSTYPE" in
   darwin*)  cmd="gtime" ;;
   linux*)   cmd="/usr/bin/time" ;;
@@ -8,5 +9,5 @@ esac
 for i in {0..10}
 do
     nuser=$(( $i * 100 ))
-    $cmd -v cargo run --features=binaries --bin=piazza --release -- -u $nuser -p 10000 --populate > results-$nuser.out 2> results-$nuser.log
+    $cmd -v cargo run --features=binaries --bin=piazza --release -- -u $nuser -p 10000 --populate > $1/results-$nuser.out 2> $1/results-$nuser.log
 done
