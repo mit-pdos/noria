ce3ba8eba20a3a27dd0b2998f77a250a4f129c0c
Lara Timbo <laratimbo7@gmail.com>
2017-10-25 16:27:37 -0400

    update

--- benchmarks/piazza/run.sh
+++ benchmarks/piazza/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ $# -lt 3 ]
+  then
+    echo "usage: ./run.sh [dir] [policies]"
+    exit
+fi
+
 mkdir $1
+policies=$2
+queries=$3
+
 case "$OSTYPE" in
   darwin*)  cmd="gtime" ;;
   linux*)   cmd="/usr/bin/time" ;;
@@ -8,6 +17,23 @@ esac
 
 for i in {0..10}
 do
-    nuser=$(( $i * 100 ))
-    $cmd -v cargo run --features=binaries --bin=piazza --release -- -l $nuser -p 10000 --populate > $1/results-$nuser.out 2> $1/results-$nuser.log
+    nuser=$(( $i * 50 ))
+
+    reuse="full"
+    $cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
+        -l $nuser -p 10000 --populate --reuse $reuse --policies $policies -q $queries > $1/results-$nuser-$reuse.out 2> $1/results-$nuser-$reuse.log
+
+    reuse="noreuse"
+    $cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
+        -l $nuser -p 10000 --populate --reuse $reuse --policies $policies -q $queries > $1/results-$nuser-$reuse.out 2> $1/results-$nuser-$reuse.log
+
+    # all posts are private workload
+    reuse="full"
+    $cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
+        -l $nuser -p 10000 --populate --private 1.0 --reuse $reuse --policies $policies -q $queries > $1/results-$nuser-$reuse-private.out 2> $1/results-$nuser-$reuse-private.log
+
+    reuse="noreuse"
+    $cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
+        -l $nuser -p 10000 --populate --private 1.0 --reuse $reuse --policies $policies -q $queries > $1/results-$nuser-$reuse-private.out 2> $1/results-$nuser-$reuse-private.log
+
 done
