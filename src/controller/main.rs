a01165cd9977e966b9f4e5f8fd031feb2b31d331
Malte Schwarzkopf <malte@csail.mit.edu>
2017-12-11 12:48:33 -0500

    Args to configure number of workers on controller
    --local-workers allows running local workers, --workers specifies how
    many remote workers we expect to connect (and thus should wait for).
    Note that this configuration gets stored in Zookeeper, so it is silently
    ignored on subsequent launches. We should probably emit a warning for
    that :-)

--- src/controller/main.rs
+++ src/controller/main.rs
@@ -26,13 +26,33 @@ fn main() {
                 .default_value("127.0.0.1:2181")
                 .help("Zookeeper connection info."),
         )
+        .arg(
+            Arg::with_name("local_workers")
+                .long("local-workers")
+                .takes_value(true)
+                .default_value("1")
+                .help("Number of local workers."),
+        )
+        .arg(
+            Arg::with_name("workers")
+                .short("w")
+                .long("workers")
+                .takes_value(true)
+                .default_value("0")
+                .help("Number of workers we expect to connect."),
+        )
         .get_matches();
 
     let listen_addr = matches.value_of("address").unwrap().parse().unwrap();
     let zookeeper_addr = matches.value_of("zookeeper").unwrap();
+    let local_workers = value_t_or_exit!(usize, args, "local_workers");
+    let remote_workers = value_t_or_exit!(usize, args, "workers");
 
     let authority = ZookeeperAuthority::new(&zookeeper_addr);
     let mut builder = ControllerBuilder::default();
     builder.set_listen_addr(listen_addr);
+    builder.set_local_workers(local_workers);
+    builder.set_nworkers(workers);
+
     builder.build(authority).wait();
 }
