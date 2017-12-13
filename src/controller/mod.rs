d7757208e65ec9a2e1cbaeba3d710c4ee89d5750
Jonathan Behrens <fintelia@gmail.com>
2017-12-13 14:46:38 -0500

    Move ControllerHandle out of controller/mod.rs

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -22,20 +22,17 @@ use std::sync::mpsc::{self, Receiver, Sender};
 use std::{io, time};
 
 use futures::{self, Future, Stream};
-use hyper::{self, Client, Method, StatusCode};
+use hyper::{self, Method, StatusCode};
 use hyper::header::ContentType;
 use hyper::server::{Http, NewService, Request, Response, Service};
 use mio::net::TcpListener;
 use petgraph;
 use petgraph::visit::Bfs;
-use petgraph::graph::NodeIndex;
 use rand;
 use serde::Serialize;
-use serde::de::DeserializeOwned;
 use serde_json;
 use slog;
 use tarpc::sync::client::{self, ClientExt};
-use tokio_core::reactor::Core;
 
 pub mod domain_handle;
 pub mod keys;
@@ -45,12 +42,14 @@ pub(crate) mod recipe;
 pub(crate) mod sql;
 
 mod getter;
+mod handle;
 mod mir_to_flow;
 mod mutator;
 
 use self::domain_handle::DomainHandle;
 use coordination::{CoordinationMessage, CoordinationPayload};
 
+pub use self::handle::ControllerHandle;
 pub use self::mutator::{Mutator, MutatorBuilder, MutatorError};
 pub use self::getter::{Getter, ReadQuery, ReadReply, RemoteGetter, RemoteGetterBuilder};
 pub(crate) use self::getter::LocalOrNot;
@@ -1347,121 +1346,6 @@ impl Drop for ControllerInner {
         }
         if let Some(ref mut local_pool) = self.local_pool {
             local_pool.wait();
-        }
-    }
-}
-
-/// `ControllerHandle` is a handle to a Controller.
-pub struct ControllerHandle<A: Authority> {
-    url: Option<String>,
-    authority: Arc<A>,
-    local: Option<(Sender<ControlEvent>, JoinHandle<()>)>,
-}
-impl<A: Authority> ControllerHandle<A> {
-    /// Creates a `ControllerHandle` that bootstraps a connection to Soup via the configuration
-    /// stored in the `Authority` passed as an argument.
-    pub fn new(authority: A) -> Self {
-        ControllerHandle {
-            url: None,
-            authority: Arc::new(authority),
-            local: None,
-        }
-    }
-
-    fn rpc<Q: Serialize, R: DeserializeOwned>(&mut self, path: &str, request: &Q) -> R {
-        let mut core = Core::new().unwrap();
-        let client = Client::new(&core.handle());
-        loop {
-            if self.url.is_none() {
-                let descriptor: ControllerDescriptor =
-                    serde_json::from_slice(&self.authority.get_leader().unwrap().1).unwrap();
-                self.url = Some(format!("http://{}", descriptor.external_addr));
-            }
-            let url = format!("{}/{}", self.url.as_ref().unwrap(), path);
-
-            let mut r = hyper::Request::new(hyper::Method::Post, url.parse().unwrap());
-            r.set_body(serde_json::to_vec(request).unwrap());
-            let res = core.run(client.request(r)).unwrap();
-            if res.status() != hyper::StatusCode::Ok {
-                self.url = None;
-                continue;
-            }
-
-            let body = core.run(res.body().concat2()).unwrap();
-            return serde_json::from_slice(&body).unwrap();
-        }
-    }
-
-    /// Get a Vec of all known input nodes.
-    ///
-    /// Input nodes are here all nodes of type `Base`. The addresses returned by this function will
-    /// all have been returned as a key in the map from `commit` at some point in the past.
-    pub fn inputs(&mut self) -> BTreeMap<String, NodeIndex> {
-        self.rpc("inputs", &())
-    }
-
-    /// Get a Vec of to all known output nodes.
-    ///
-    /// Output nodes here refers to nodes of type `Reader`, which is the nodes created in response
-    /// to calling `.maintain` or `.stream` for a node during a migration.
-    pub fn outputs(&mut self) -> BTreeMap<String, NodeIndex> {
-        self.rpc("outputs", &())
-    }
-
-    /// Obtain a `RemoteGetterBuilder` that can be sent to a client and then used to query a given
-    /// (already maintained) reader node.
-    pub fn get_getter_builder(&mut self, node: NodeIndex) -> Option<RemoteGetterBuilder> {
-        self.rpc("getter_builder", &node)
-    }
-
-    /// Obtain a `RemoteGetter`.
-    pub fn get_getter(&mut self, node: NodeIndex) -> Option<RemoteGetter> {
-        self.get_getter_builder(node).map(|g| g.build())
-    }
-
-    /// Obtain a MutatorBuild that can be used to construct a Mutator to perform writes and deletes
-    /// from the given base node.
-    pub fn get_mutator_builder(&mut self, base: NodeIndex) -> Result<MutatorBuilder, Box<Error>> {
-        Ok(self.rpc("mutator_builder", &base))
-    }
-
-    /// Obtain a Mutator
-    pub fn get_mutator(&mut self, base: NodeIndex) -> Result<Mutator, Box<Error>> {
-        self.get_mutator_builder(base)
-            .map(|m| m.build("127.0.0.1:0".parse().unwrap()))
-    }
-
-    /// Initiaties log recovery by sending a
-    /// StartRecovery packet to each base node domain.
-    pub fn recover(&mut self) {
-        self.rpc("recover", &())
-    }
-
-    /// Get statistics about the time spent processing different parts of the graph.
-    pub fn get_statistics(&mut self) -> GraphStats {
-        self.rpc("get_statistics", &())
-    }
-
-    /// Install a new recipe on the controller.
-    pub fn install_recipe(&mut self, new_recipe: String) {
-        self.rpc("install_recipe", &new_recipe)
-    }
-
-    /// graphviz description of the dataflow graph
-    pub fn graphviz(&mut self) -> String {
-        self.rpc("graphviz", &())
-    }
-
-    /// Wait for associated local controller to exit.
-    pub fn wait(mut self) {
-        self.local.take().unwrap().1.join().unwrap()
-    }
-}
-impl<A: Authority> Drop for ControllerHandle<A> {
-    fn drop(&mut self) {
-        if let Some((sender, join_handle)) = self.local.take() {
-            let _ = sender.send(ControlEvent::Shutdown);
-            let _ = join_handle.join();
         }
     }
 }
