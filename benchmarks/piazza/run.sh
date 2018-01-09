ee398cae1b6101cd2d71e3ac3578d43e2c3c516d
Lara Timbo <laratimbo7@gmail.com>
2018-01-09 18:49:52 -0500

    Script runs benchmark with different policy setups

--- benchmarks/piazza/run.sh
+++ benchmarks/piazza/run.sh
@@ -1,50 +1,7 @@
-#!/bin/bash
+# basic results for session creation time
+./benchmarks/piazza/experiments/session-creation.sh basic benchmarks/piazza/basic-policies.json benchmarks/piazza/post-queries.sql
 
-if [ $# -lt 3 ]
-  then
-    echo "usage: ./run.sh [dir] [policies]"
-    exit
-fi
+# results for comparing the effect of group policies
+./benchmarks/piazza/experiments/session-creation.sh group benchmarks/piazza/ta-policies.json benchmarks/piazza/post-queries.sql
 
-mkdir $1
-policies=$2
-queries=$3
-
-case "$OSTYPE" in
-  darwin*)  cmd="gtime" ;;
-  linux*)   cmd="/usr/bin/time" ;;
-esac
-
-nuser=5000
-reuse="full"
-name=$nuser-$reuse
-mkdir $1/$name
-mkdir $1/$name/info
-
-$cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
-    -l $nuser -u $nuser -i $1/$name/info/info -p 100000 -c 1000 --populate --reuse $reuse --policies $policies -q $queries > $1/$name/results-$name.out 2> $1/$name/results-$name.log
-
-reuse="noreuse"
-name=$nuser-$reuse
-mkdir $1/$name
-mkdir $1/$name/info
-
-$cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
-    -l $nuser -u $nuser -i $1/$name/info/info -p 100000 -c 1000 --populate --reuse $reuse --policies $policies -q $queries > $1/$name/results-$name.out 2> $1/$name/results-$name.log
-
-# all posts are private workload
-reuse="full"
-name=$nuser-$reuse-private
-mkdir $1/$name
-mkdir $1/$name/info
-
-$cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
-    -l $nuser -u $nuser -i $1/$name/info/info -p 100000 -c 1000 --populate --private 1.0 --reuse $reuse --policies $policies -q $queries > $1/$name/results-$name.out 2> $1/$name/results-$name.log
-
-reuse="noreuse"
-name=$nuser-$reuse-private
-mkdir $1/$name
-mkdir $1/$name/info
-
-$cmd -v cargo run --manifest-path benchmarks/Cargo.toml --bin=piazza --release -- \
-    -l $nuser -u $nuser -i $1/$name/info/info -p 100000 -c 1000 --populate --private 1.0 --reuse $reuse --policies $policies -q $queries > $1/$name/results-$name.out 2> $1/$name/results-$name.log
+./benchmarks/piazza/experiments/session-creation.sh nogroup benchmarks/piazza/ta-policies-nogroups.json benchmarks/piazza/post-queries.sql
