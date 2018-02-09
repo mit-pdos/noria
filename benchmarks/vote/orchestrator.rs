d9891c81c592aee508bf3a05a0b4ed2449c78f0b
Jon Gjengset <jon@thesquareplanet.com>
2018-02-08 19:47:11 -0500

    AMI with new vote

--- benchmarks/vote/orchestrator.rs
+++ benchmarks/vote/orchestrator.rs
@@ -24,7 +24,7 @@ use std::io::prelude::*;
 use std::error::Error;
 use std::borrow::Cow;
 
-const SOUP_AMI: &str = "ami-8f373df5";
+const SOUP_AMI: &str = "ami-1bfdf761";
 
 #[derive(Clone, Copy)]
 struct ClientParameters<'a> {
