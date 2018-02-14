db795c5dee5ca580c6f82205327a36def9c2c272
Malte Schwarzkopf <malte@csail.mit.edu>
2018-02-14 13:25:21 -0500

    Disable empty parent optimization
    This optimization forces full materialization of known-empty views even
    if we earlier decided to make them partial (and continue to claim that
    they are). Empty bases can still skip the replay; we'll probably want
    some more sophisticated logic that also avoids replays if we know that
    a node necessarily starts off empty (e.g., because a parent joins an
    empty view).
    
    Fixes RTM's strange partial-does-not-discard bug. We pass the new test
    now also.

--- src/controller/migrate/materialization/mod.rs
+++ src/controller/migrate/materialization/mod.rs
@@ -588,16 +588,12 @@ impl Materializations {
             debug!(self.log, "new stateless node: {:?}", n);
         }
 
-        if graph
-            .neighbors_directed(ni, petgraph::EdgeDirection::Incoming)
-            .filter(|&ni| !graph[ni].is_source())
-            .all(|n| empty.contains(&n))
-        {
-            // all parents are empty, so we can materialize it immediately
-            info!(self.log, "no need to replay empty view"; "node" => ni.index());
+        if n.is_base() {
+            // a new base must be empty, so we can materialize it immediately
+            info!(self.log, "no need to replay empty new base"; "node" => ni.index());
             empty.insert(ni);
 
-            // we need to make sure the domain constructs reader backlog handles!
+            // yet, we need to make sure the domain constructs reader backlog handles!
             let prep = n.with_reader(|r| {
                 r.key().map(|key| {
                     use dataflow::payload::InitialState;
@@ -617,9 +613,7 @@ impl Materializations {
             return;
         }
 
-        // if this node doesn't need to be materialized, then we're done. note that this check
-        // needs to happen *after* the empty parents check so that we keep tracking whether or not
-        // nodes are empty.
+        // if this node doesn't need to be materialized, then we're done.
         has_state = !index_on.is_empty();
         n.with_reader(|r| {
             if r.is_materialized() {
