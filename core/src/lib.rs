9dd7f51fd7f04414fa3a441d0668c34609bd3a49
Martin Ek <mail@ekmartin.com>
2018-02-19 14:52:08 +0100

    Use bincode instead of serde_json for PersistentState

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -6,6 +6,7 @@
 #![deny(unused_extern_crates)]
 
 extern crate arccstr;
+extern crate bincode;
 extern crate chrono;
 extern crate fnv;
 extern crate nom_sql;
@@ -16,7 +17,6 @@ extern crate rusqlite;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
 
 pub mod addressing;
 pub mod data;
