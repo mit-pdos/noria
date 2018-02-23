41822ea090753a893c1482c881bb2b4a12c2a732
Malte Schwarzkopf <malte@csail.mit.edu>
2018-02-23 00:13:15 -0500

    More correct ExactMatch/ExtendExisting reuse
    Previously, column order did not matter in some of our data structures
    used for reuse decisions (specifically, in the query graph signature).
    This is problematic because it can cause us to incorrectly conclude that
    two queries are identical even though they output the columns in a
    different order. If that is the case, we should fall into the
    `ExtendExisting` reuse case, add a reprojection operator, and add a new
    reader. This makes that change and extends the unit test for reuse with
    an explicit check to ensure we actually do it.
    
    This likely goes some way to fixing the incorrect and overeager use of
    `ExactMatch` observed by @larat7.

--- src/controller/sql/mod.rs
+++ src/controller/sql/mod.rs
@@ -824,7 +824,7 @@ mod tests {
             let qid = query_id_hash(
                 &["articles", "users"],
                 &[&Column::from("articles.author"), &Column::from("users.id")],
-                &[&Column::from("articles.title"), &Column::from("users.name")],
+                &[&Column::from("users.name"), &Column::from("articles.title")],
             );
             // join node
             let new_join_view = get_node(&inc, mig, &format!("q_{:x}_n0", qid));
@@ -988,7 +988,7 @@ mod tests {
 
             // Add the same query again
             let ncount = mig.graph().node_count();
-            let res = inc.add_query("SELECT name, id FROM users WHERE users.id = 42;", None, mig);
+            let res = inc.add_query("SELECT id, name FROM users WHERE users.id = 42;", None, mig);
             assert!(res.is_ok());
             // should have added no more nodes
             let qfp = res.unwrap();
@@ -996,6 +996,16 @@ mod tests {
             assert_eq!(mig.graph().node_count(), ncount);
             // should have ended up with the same leaf node
             assert_eq!(qfp.query_leaf, leaf);
+
+            // Add the same query again, but project columns in a different order
+            let ncount = mig.graph().node_count();
+            let res = inc.add_query("SELECT name, id FROM users WHERE users.id = 42;", None, mig);
+            assert!(res.is_ok());
+            // should have added two more nodes (project and reader)
+            let qfp = res.unwrap();
+            assert_eq!(mig.graph().node_count(), ncount + 2);
+            // should NOT have ended up with the same leaf node
+            assert_ne!(qfp.query_leaf, leaf);
         });
     }
 
@@ -1257,8 +1267,8 @@ mod tests {
                     &Column::from("votes.aid"),
                 ],
                 &[
-                    &Column::from("articles.title"),
                     &Column::from("users.name"),
+                    &Column::from("articles.title"),
                     &Column::from("votes.uid"),
                 ],
             );
@@ -1357,8 +1367,8 @@ mod tests {
                     &Column::from("nested_users.id"),
                 ],
                 &[
-                    &Column::from("articles.title"),
                     &Column::from("nested_users.name"),
+                    &Column::from("articles.title"),
                 ],
             );
             // join node
