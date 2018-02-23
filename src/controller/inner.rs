e65b7f1138855f5f9891203d0845f9e5471a48c6
Malte Schwarzkopf <malte@csail.mit.edu>
2018-02-23 17:24:04 -0500

    Resolve node IDs via recipe, then graph traversal
    Our recipe and SQL layers actually implement aliasing and deduplicate
    identical queries, so it is important that the RPCs handlers for
    obtaining read and write handles first check the recipe, and only
    trigger the lower-level graph traversal method for finding base and
    reader nodes of a matching name. Both methods are required: some tests
    bypass the recipe layer, and some reuse cases create no data-flow
    graph nodes.
    
    Fixes failures that occurred when an application tries to execute the
    exact same query twice, but with different query names (it would get
    no read handle for the second one, since no data-flow node was added).

--- src/controller/inner.rs
+++ src/controller/inner.rs
@@ -436,7 +436,17 @@ impl ControllerInner {
     /// Obtain a `RemoteGetterBuilder` that can be sent to a client and then used to query a given
     /// (already maintained) reader node called `name`.
     pub fn getter_builder(&self, name: &str) -> Option<RemoteGetterBuilder> {
-        let node = *self.outputs().get(name)?;
+        // first try to resolve the node via the recipe, which handles aliasing between identical
+        // queries.
+        let node = match self.recipe.node_addr_for(name) {
+            Ok(ni) => ni,
+            Err(_) => {
+                // if the recipe doesn't know about this query, traverse the graph.
+                // we need this do deal with manually constructed graphs (e.g., in tests).
+                *self.outputs().get(name)?
+            }
+        };
+
         self.find_getter_for(node).map(|r| {
             let domain = self.ingredients[r].domain();
             let shards = (0..self.domains[&domain].shards())
@@ -457,7 +467,10 @@ impl ControllerInner {
     /// Obtain a MutatorBuild that can be used to construct a Mutator to perform writes and deletes
     /// from the given named base node.
     pub fn mutator_builder(&self, base: &str) -> Option<MutatorBuilder> {
-        let ni = *self.inputs().get(base)?;
+        let ni = match self.recipe.node_addr_for(base) {
+            Ok(ni) => ni,
+            Err(_) => *self.inputs().get(base)?,
+        };
         let node = &self.ingredients[ni];
 
         trace!(self.log, "creating mutator"; "for" => base);
