c802d6ebb40cf6739d71dd72821c004f8079623c
Malte Schwarzkopf <malte@csail.mit.edu>
2018-03-08 00:21:10 -0500

    SizeOf trait + impl for DataType & Vec<DataType>
    Allows tracking the shallow and deep size of `DataType` objects, and of
    records (which are `Vec<DataType>`). Starting point for keeping track of
    the memory footprints of individual operators' state, which we need for
    eviction.

--- core/src/data.rs
+++ core/src/data.rs
@@ -591,6 +591,46 @@ impl Into<Records> for Vec<(Vec<DataType>, bool)> {
     }
 }
 
+trait SizeOf {
+    fn deep_size_of(&self) -> usize;
+    fn size_of(&self) -> usize;
+}
+
+impl SizeOf for DataType {
+    fn deep_size_of(&self) -> usize {
+        use std::mem::size_of_val;
+
+        let inner = match *self {
+            DataType::Text(ref t) => size_of_val(t) + t.to_bytes().len(),
+            DataType::Timestamp(ref t) => size_of_val(t),
+            _ => 0usize,
+        };
+
+        self.size_of() + inner
+    }
+
+    fn size_of(&self) -> usize {
+        use std::mem::size_of;
+
+        // doesn't include data if stored externally
+        size_of::<DataType>()
+    }
+}
+
+impl SizeOf for Vec<DataType> {
+    fn deep_size_of(&self) -> usize {
+        use std::mem::size_of_val;
+
+        size_of_val(self) + self.iter().fold(0usize, |acc, d| acc + d.deep_size_of())
+    }
+
+    fn size_of(&self) -> usize {
+        use std::mem::{size_of, size_of_val};
+
+        size_of_val(self) + size_of::<DataType>() * self.len()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -933,5 +973,33 @@ mod tests {
         assert_ne!(hash(&long), hash(&real));
         assert_ne!(hash(&long), hash(&time));
         assert_ne!(hash(&long), hash(&shrt6));
+    }
+
+    #[test]
+    fn data_type_mem_size() {
+        use std::convert::TryFrom;
+        use std::mem::{size_of, size_of_val};
+
+        let txt: DataType = DataType::Text(ArcCStr::try_from("hi").unwrap());
+        let shrt = DataType::Int(5);
+        let long = DataType::BigInt(5);
+        let time = DataType::Timestamp(NaiveDateTime::from_timestamp(0, 42_000_000));
+
+        let rec = vec![DataType::Int(5), "asdfasdfasdfasdf".into(), "asdf".into()];
+
+        // DataType should always use 16 bytes itself
+        assert_eq!(size_of::<DataType>(), 16);
+        assert_eq!(size_of_val(&txt), 16);
+        assert_eq!(size_of_val(&txt), txt.size_of());
+        assert_eq!(txt.deep_size_of(), txt.size_of() + 8 + 2); // DataType + ArcCStr's ptr + 2 chars
+        assert_eq!(size_of_val(&shrt), 16);
+        assert_eq!(size_of_val(&long), 16);
+        assert_eq!(size_of_val(&time), 16);
+        assert_eq!(size_of_val(&time), time.size_of());
+        assert_eq!(time.deep_size_of(), time.size_of() + 12); // DataType + NaiveDateTime
+
+        assert_eq!(size_of_val(&rec), 24);
+        assert_eq!(rec.size_of(), 24 + 3 * 16);
+        assert_eq!(rec.deep_size_of(), 24 + 3 * 16 + (8 + 16));
     }
 }
