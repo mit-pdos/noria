115e119b81a3e964b821037bbaed945cd058c5a1
Jon Gjengset <jon@thesquareplanet.com>
2018-03-24 12:33:46 -0400

    Submitting a story

--- lobsters/mysql/src/main.rs
+++ lobsters/mysql/src/main.rs
@@ -287,14 +287,41 @@ impl trawler::LobstersClient for MysqlTrawler {
                         .map(move |_| sent.elapsed()),
                 )
             }
-            LobstersRequest::Login(uid) => {
-                // TODO
-                Box::new(futures::future::ok(time::Duration::new(0, 0)))
-            }
-            LobstersRequest::Logout(..) => {
-                // TODO
-                Box::new(futures::future::ok(time::Duration::new(0, 0)))
-            }
+            LobstersRequest::Login(uid) => Box::new(
+                this.c
+                    .get_conn()
+                    .and_then(move |c| {
+                        c.drop_exec(
+                            "\
+                             SELECT  `users`.* \
+                             FROM `users` \
+                             WHERE `users`.`username` = ? \
+                             ORDER BY `users`.`id` ASC LIMIT 1",
+                            (format!("user{}", uid),),
+                        )
+                    })
+                    .map_err(|e| {
+                        eprintln!("{:?}", e);
+                    })
+                    .map(move |_| sent.elapsed()),
+            ),
+            LobstersRequest::Logout(uid) => Box::new(
+                this.c
+                    .get_conn()
+                    .and_then(|c| {
+                        c.drop_exec(
+                            "\
+                             SELECT users.* \
+                             FROM users WHERE users.session_token = ? \
+                             ORDER BY users.id ASC LIMIT 1",
+                            ("KMQEEJjXymcyFj3j7Qn3c3kZ5AFcghUxscm6J9c0a3XBTMjD2OA9PEoecxyt",),
+                        )
+                    })
+                    .map_err(|e| {
+                        eprintln!("{:?}", e);
+                    })
+                    .map(move |_| sent.elapsed()),
+            ),
             LobstersRequest::Story(id) => {
                 Box::new(
                     this.c
@@ -768,8 +795,190 @@ impl trawler::LobstersClient for MysqlTrawler {
             }
 
             LobstersRequest::Submit { id, user, title } => {
-                // TODO
-                Box::new(futures::future::ok(time::Duration::new(0, 0)))
+                Box::new(
+                    this.c
+                        .get_conn()
+                        .and_then(|c| {
+                            c.drop_exec(
+                                "\
+                                 SELECT users.* \
+                                 FROM users WHERE users.session_token = ? \
+                                 ORDER BY users.id ASC LIMIT 1",
+                                ("KMQEEJjXymcyFj3j7Qn3c3kZ5AFcghUxscm6J9c0a3XBTMjD2OA9PEoecxyt",),
+                            )
+                        })
+                        .and_then(|c| {
+                            c.start_transaction(my::TransactionOptions::new())
+                                .and_then(|t| {
+                                    t.drop_query(
+                                 "SELECT keystores.* FROM keystores WHERE keystores.key = 'traffic:date' ORDER BY keystores.key ASC LIMIT 1 FOR UPDATE; \
+                                 SELECT keystores.* FROM keystores WHERE keystores.key = 'traffic:hits' ORDER BY keystores.key ASC LIMIT 1 FOR UPDATE; \
+                                 UPDATE keystores SET value = 1521590012 WHERE keystores.key = 'traffic:date';",
+                            )
+                                })
+                                .and_then(|t| t.commit())
+                        })
+                        .and_then(move |c| {
+                            // check that tags exist
+                            c.drop_query(
+                                "SELECT  1 AS one FROM `tags` \
+                                 INNER JOIN `taggings` ON `tags`.`id` = `taggings`.`tag_id` \
+                                 WHERE `taggings`.`story_id` IS NULL \
+                                 AND (1=0) \
+                                 AND `tags`.`tag` IN ('test') LIMIT 1",
+                            )
+                        })
+                        .and_then(|c| {
+                            // check that tags are active
+                            c.first::<_, my::Row>(
+                                "SELECT  `tags`.* FROM `tags` \
+                                 WHERE `tags`.`inactive` = 0 AND `tags`.`tag` IN ('test') \
+                                 ORDER BY `tags`.`id` ASC LIMIT 1",
+                            )
+                        })
+                        .map(|(c, tag)| (c, tag.unwrap().get::<u32, _>("id")))
+                        .and_then(move |(c, tag)| {
+                            // check that story id isn't already assigned
+                            c.drop_exec(
+                                "SELECT  1 AS one FROM `stories` \
+                                 WHERE `stories`.`short_id` = ? LIMIT 1",
+                                (::std::str::from_utf8(&id[..]).unwrap(),),
+                            ).map(move |c| (c, tag))
+                        })
+                        .map(|c| {
+                            // TODO: check for similar stories if there's a url
+                            // SELECT  `stories`.*
+                            // FROM `stories`
+                            // WHERE `stories`.`url` IN (
+                            //  'https://google.com/test',
+                            //  'http://google.com/test',
+                            //  'https://google.com/test/',
+                            //  'http://google.com/test/',
+                            //  ... etc
+                            // )
+                            // AND (is_expired = 0 OR is_moderated = 1)
+                            // ORDER BY id DESC LIMIT 1
+                            c
+                        })
+                        .map(|c| {
+                            // TODO
+                            // real impl queries `tags` and `users` again here..?
+                            c
+                        })
+                        .and_then(move |(c, tag)| {
+                            // TODO: real impl checks *new* short_id and duplicate urls *again*
+                            // TODO: sometimes submit url
+                            c.start_transaction(my::TransactionOptions::new())
+                                .and_then(move |t| {
+                                    t.prep_exec(
+                                        "INSERT INTO `stories` \
+                                         (`created_at`, `user_id`, `title`, \
+                                         `description`, `short_id`, `upvotes`, `hotness`, \
+                                         `markeddown_description`) \
+                                         VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                                        (
+                                            "2018-03-24 15:43:27",
+                                            user,
+                                            title,
+                                            "to infinity", // lorem ipsum?
+                                            ::std::str::from_utf8(&id[..]).unwrap(),
+                                            1,
+                                            -19216.2884921,
+                                            "<p>to infinity</p>\n",
+                                        ),
+                                    )
+                                })
+                                .and_then(|q| {
+                                    let story = q.last_insert_id().unwrap();
+                                    q.drop_result().map(move |t| (t, story))
+                                })
+                                .and_then(move |(t, story)| {
+                                    t.drop_exec(
+                                        "INSERT INTO `taggings` (`story_id`, `tag_id`) \
+                                         VALUES (?, ?)",
+                                        (story, tag),
+                                    ).map(move |t| (t, story))
+                                })
+                                .and_then(move |(t, story)| {
+                                    t.drop_query(&format!(
+                                        "INSERT INTO keystores (`key`, `value`) \
+                                         VALUES \
+                                         ('user:{}:stories_submitted', 1) \
+                                         ON DUPLICATE KEY UPDATE `value` = `value` + 1",
+                                        user
+                                    )
+                                    ).map(move |t| (t, story))
+                                })
+                                .and_then(move |(t, story)| {
+                                    t.drop_query(&format!(
+                                        "SELECT  `keystores`.* \
+                                        FROM `keystores` \
+                                        WHERE `keystores`.`key` = 'user:{}:stories_submitted' \
+                                        ORDER BY `keystores`.`key` ASC LIMIT 1",
+                                        user
+                                    )
+                                    ).map(move |t| (t, story))
+                                })
+                                .and_then(move |(t, story)| {
+                                    t.drop_exec(
+                                        "SELECT  `votes`.* FROM `votes` \
+                                        WHERE `votes`.`user_id` = ? \
+                                        AND `votes`.`story_id` = ? \
+                                        AND `votes`.`comment_id` IS NULL \
+                                        ORDER BY `votes`.`id` ASC LIMIT 1",
+                                        (user, story)
+                                    ).map(move |t| (t, story))
+                                })
+                                .and_then(move |(t, story)| {
+                                    t.drop_exec(
+                                        "INSERT INTO `votes` (`user_id`, `story_id`, `vote`) \
+                                        VALUES (?, ?, 1)",
+                                        (user, story)
+                                    ).map(move |t| (t, story))
+                                })
+                                .and_then(move |(t, story)| {
+                                    t.drop_exec(
+                                        "SELECT `comments`.`upvotes`, `comments`.`downvotes` \
+                                        FROM `comments` \
+                                        WHERE `comments`.`story_id` = ? \
+                                        AND (user_id <> ?)",
+                                        (story, user)
+                                    ).map(move |t| (t, story))
+                                })
+                                .and_then(move |(t, story)| {
+                                    // why oh why is story hotness *updated* here?!
+                                    t.drop_exec(
+                                        &format!(
+                                            "UPDATE `stories` \
+                                             SET `hotness` = {}
+                                             WHERE `stories`.`id` = ?",
+                                            -19216.5479744,
+                                        ),
+                                        (story,),
+                                    )
+                                })
+                                .and_then(|t| t.commit())
+                        })
+                        // TODO
+                        //
+                        //  SELECT  `read_ribbons`.* \
+                        //  FROM `read_ribbons` \
+                        //  WHERE `read_ribbons`.`user_id` = 12 \
+                        //  AND `read_ribbons`.`story_id` = 5747 \
+                        //  ORDER BY `read_ribbons`.`id` ASC LIMIT 1
+                        //
+                        //  BEGIN
+                        //
+                        //  INSERT INTO `read_ribbons` \
+                        //  (`created_at`, `updated_at`, `user_id`, `story_id`) \
+                        //  VALUES ('2018-03-24 15:55:58', '2018-03-24 15:55:58', 12, 5747)
+                        //
+                        //  COMMIT
+                        .map_err(|e| {
+                            eprintln!("{:?}", e);
+                        })
+                        .map(move |_| sent.elapsed()),
+                )
             }
             LobstersRequest::Comment {
                 id,
