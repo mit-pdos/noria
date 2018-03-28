fa893ea69a608f9d9e709189528804d6ceb57d8d
Jon Gjengset <jon@thesquareplanet.com>
2018-03-28 12:06:41 -0400

    avoid empty /recent failing

--- lobsters/mysql/src/main.rs
+++ lobsters/mysql/src/main.rs
@@ -233,6 +233,7 @@ impl trawler::LobstersClient for MysqlTrawler {
                         // https://github.com/lobsters/lobsters/blob/50b4687aeeec2b2d60598f63e06565af226f93e3/app/models/story_repository.rb#L41
                         // but it *basically* just looks for stories in the past few days
                         // because all our stories are for the same day, we add a LIMIT
+                        // also note the `NOW()` hack to support dbs primed a while ago
                         c.query(
                             "SELECT `stories`.`id`, \
                              `stories`.`upvotes`, \
@@ -241,7 +242,8 @@ impl trawler::LobstersClient for MysqlTrawler {
                              FROM `stories` \
                              WHERE `stories`.`merged_story_id` IS NULL \
                              AND `stories`.`is_expired` = 0 \
-                             AND `stories`.`created_at` > NOW() - INTERVAL 3 DAY \
+                             AND (`stories`.`created_at` > NOW() - INTERVAL 3 DAY \
+                             OR `stories`.`created_at` <= NOW() - INTERVAL 3 DAY) \
                              AND CAST(upvotes AS signed) - CAST(downvotes AS signed) <= 5 \
                              ORDER BY stories.id DESC, stories.created_at DESC \
                              LIMIT 25",
