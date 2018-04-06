96d8338375eb26bdb689e743f35845c5714e12ce
Martin Ek <mail@ekmartin.com>
2018-04-06 21:03:50 +0200

    Hash skip list -> hash linked list

--- core/src/local/state.rs
+++ core/src/local/state.rs
@@ -8,7 +8,7 @@ use local::single_state::SingleState;
 
 use bincode;
 use rand::{self, Rng};
-use rocksdb::{self, BlockBasedOptions, BlockBasedIndexType, MemtableFactory, SliceTransform, DB};
+use rocksdb::{self, MemtableFactory, SliceTransform, DB};
 
 pub enum State {
     InMemory(MemoryState),
@@ -174,20 +174,14 @@ impl PersistentState {
         opts.create_if_missing(true);
         let transform = SliceTransform::create("key", Self::transform_fn, None);
         opts.set_prefix_extractor(transform);
-        // Use a bloom filter for the SS-tables (on disk) as well:
-        let mut block_opts = BlockBasedOptions::default();
-        block_opts.set_index_type(BlockBasedIndexType::HashSearch);
-        opts.set_block_based_table_factory(&block_opts);
 
         // Assigns the number of threads for RocksDB's low priority background pool:
         opts.increase_parallelism(threads);
 
-        // Use a hash skiplist since we're doing prefix seeks:
+        // Use a hash linked list since we're doing prefix seeks.
         opts.set_allow_concurrent_memtable_write(false);
-        opts.set_memtable_factory(MemtableFactory::HashSkipList {
+        opts.set_memtable_factory(MemtableFactory::HashLinkList {
             bucket_count: 1_000_000,
-            height: 4,
-            branching_factor: 4,
         });
 
         let full_name = format!("{}.db", name);
