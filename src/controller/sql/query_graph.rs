abea89923e7ca158500ed2f16bf7637219597f68
Malte Schwarzkopf <malte@csail.mit.edu>
2018-04-07 22:57:03 -0400

    Handle INNER JOIN when building query graph

--- src/controller/sql/query_graph.rs
+++ src/controller/sql/query_graph.rs
@@ -540,7 +540,9 @@ pub fn to_query_graph(st: &SelectStatement) -> Result<QueryGraph, String> {
                     .entry((left_table.clone(), right_table.clone()))
                     .or_insert_with(|| match jc.operator {
                         JoinOperator::LeftJoin => QueryGraphEdge::LeftJoin(vec![join_pred]),
-                        JoinOperator::Join => QueryGraphEdge::Join(vec![join_pred]),
+                        JoinOperator::Join | JoinOperator::InnerJoin => {
+                            QueryGraphEdge::Join(vec![join_pred])
+                        }
                         _ => unimplemented!(),
                     });
             }
