5f9368bc7fee22dbe86d08bc4aba66a824801eff
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2018-04-11 13:17:06 -0400

    Use more prepared statements for Frontpage
    Makes the queries amenable to WHERE-IN collapsing.

--- lobsters/mysql/src/endpoints/frontpage.rs
+++ lobsters/mysql/src/endpoints/frontpage.rs
@@ -2,6 +2,7 @@ use futures::Future;
 use futures::future::Either;
 use my;
 use my::prelude::*;
+use std::iter;
 use std::collections::HashSet;
 use trawler::UserId;
 
@@ -16,11 +17,11 @@ where
         Some(uid) => Either::A(
             // logged-in front page
             c.and_then(move |c| {
-                c.query(&format!(
+                c.prep_exec(
                     "SELECT `tag_filters`.* FROM `tag_filters` \
-                     WHERE `tag_filters`.`user_id` = {}",
-                    uid
-                ))
+                     WHERE `tag_filters`.`user_id` = ?",
+                    (uid,),
+                )
             }).and_then(|tags| {
                     tags.reduce_and_drop(Vec::new(), |mut tags, tag| {
                         tags.push(tag.get::<u32, _>("tag_id").unwrap());
@@ -100,8 +101,8 @@ where
             )).map(move |c| (c, stories))
         })
         .and_then(|(c, stories)| {
-            let stories = stories
-                .into_iter()
+            let stories_str = stories
+                .iter()
                 .map(|id| format!("{}", id))
                 .collect::<Vec<_>>()
                 .join(",");
@@ -109,22 +110,22 @@ where
                 "SELECT `suggested_titles`.* \
                  FROM `suggested_titles` \
                  WHERE `suggested_titles`.`story_id` IN ({})",
-                stories
-            )).map(move |c| (c, stories))
+                stories_str
+            )).map(move |c| (c, stories, stories_str))
         })
-        .and_then(|(c, stories)| {
+        .and_then(|(c, stories, stories_str)| {
             c.drop_query(&format!(
                 "SELECT `suggested_taggings`.* \
                  FROM `suggested_taggings` \
                  WHERE `suggested_taggings`.`story_id` IN ({})",
-                stories
-            )).map(move |c| (c, stories))
+                stories_str
+            )).map(move |c| (c, stories, stories_str))
         })
-        .and_then(|(c, stories)| {
+        .and_then(|(c, stories, stories_str)| {
             c.query(&format!(
                 "SELECT `taggings`.* FROM `taggings` \
                  WHERE `taggings`.`story_id` IN ({})",
-                stories
+                stories_str
             )).map(move |t| (t, stories))
         })
         .and_then(|(taggings, stories)| {
@@ -152,30 +153,51 @@ where
             None => Either::A(main.map(|(c, _)| c)),
             Some(uid) => Either::B(
                 main.and_then(move |(c, stories)| {
-                    c.drop_query(&format!(
-                        "SELECT `votes`.* FROM `votes` \
-                         WHERE `votes`.`user_id` = {} \
-                         AND `votes`.`story_id` IN ({}) \
-                         AND `votes`.`comment_id` IS NULL",
-                        uid, stories,
-                    )).map(move |c| (c, stories))
-                }).and_then(move |(c, stories)| {
-                        c.drop_query(&format!(
-                            "SELECT `hidden_stories`.* \
-                             FROM `hidden_stories` \
-                             WHERE `hidden_stories`.`user_id` = {} \
-                             AND `hidden_stories`.`story_id` IN ({})",
-                            uid, stories,
-                        )).map(move |c| (c, stories))
+                    let story_params = stories.iter().map(|_| "?").collect::<Vec<_>>().join(",");
+                    let sc = stories.clone();
+                    let values: Vec<_> = iter::once(&uid as &_)
+                        .chain(stories.iter().map(|s| s as &_))
+                        .collect();
+                    c.drop_exec(
+                        &format!(
+                            "SELECT `votes`.* FROM `votes` \
+                             WHERE `votes`.`user_id` = ? \
+                             AND `votes`.`story_id` IN ({}) \
+                             AND `votes`.`comment_id` IS NULL",
+                            story_params
+                        ),
+                        values,
+                    ).map(move |c| (c, story_params, sc))
+                }).and_then(move |(c, story_params, stories)| {
+                        let sc = stories.clone();
+                        let values: Vec<_> = iter::once(&uid as &_)
+                            .chain(stories.iter().map(|s| s as &_))
+                            .collect();
+                        c.drop_exec(
+                            &format!(
+                                "SELECT `hidden_stories`.* \
+                                 FROM `hidden_stories` \
+                                 WHERE `hidden_stories`.`user_id` = ? \
+                                 AND `hidden_stories`.`story_id` IN ({})",
+                                story_params
+                            ),
+                            values,
+                        ).map(move |c| (c, story_params, sc))
                     })
-                    .and_then(move |(c, stories)| {
-                        c.drop_query(&format!(
-                            "SELECT `saved_stories`.* \
-                             FROM `saved_stories` \
-                             WHERE `saved_stories`.`user_id` = {} \
-                             AND `saved_stories`.`story_id` IN ({})",
-                            uid, stories,
-                        ))
+                    .and_then(move |(c, story_params, stories)| {
+                        let values: Vec<_> = iter::once(&uid as &_)
+                            .chain(stories.iter().map(|s| s as &_))
+                            .collect();
+                        c.drop_exec(
+                            &format!(
+                                "SELECT `saved_stories`.* \
+                                 FROM `saved_stories` \
+                                 WHERE `saved_stories`.`user_id` = ? \
+                                 AND `saved_stories`.`story_id` IN ({})",
+                                story_params,
+                            ),
+                            values,
+                        )
                     }),
             ),
         }.map(|c| (c, true)),
