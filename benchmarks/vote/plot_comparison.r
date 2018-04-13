36868bcab013e3076c12fa605e83990ff8695ce1
Jon Gjengset <jon@thesquareplanet.com>
2018-04-13 19:15:38 -0400

    Make plotting interact nicely with many runs

--- benchmarks/vote/plot_comparison.r
+++ benchmarks/vote/plot_comparison.r
@@ -12,7 +12,7 @@ for (arg in args) {
 	skew = a[[5]]
 
 	con <- file(arg, open = "r")
-	cpu = 0
+	cpu = list()
 	actual = 0
 	clients = 0
 	in_results = TRUE
@@ -21,7 +21,7 @@ for (arg in args) {
 		if (grepl("load average", line)) {
 			l <- strsplit(line, " ")
 			l <- l[[1]]
-			cpu = as.numeric(sub(",", "", l[[12]]))
+			cpu = append(cpu, list(as.numeric(sub(",", "", l[[12]]))))
 
 		} else if (startsWith(line, "# ")) {
 			if (startsWith(line, "# actual ops")) {
@@ -38,7 +38,12 @@ for (arg in args) {
 		}
 	} 
 
-	ts <- ts %>% group_by(op, pct) %>% summarize(sjrn = max(sjrn), rmt = max(rmt))
+	ts <- ts %>% group_by(op, pct) %>% summarize(sjrn_max = max(sjrn),
+						     sjrn_min = min(sjrn),
+						     sjrn = median(sjrn),
+						     rmt_max = max(rmt),
+						     rmt_min = min(rmt),
+						     rmt = median(rmt))
 
 	ts$server = server
 	ts$articles = articles
@@ -46,7 +51,10 @@ for (arg in args) {
 	ts$actual = actual
 	ts$readp = read_perc
 	ts$skew = skew
-	ts$cpu = cpu
+	cpu <- unlist(cpu)
+	ts$cpu = median(cpu)
+	ts$cpu_min = min(cpu)
+	ts$cpu_max = max(cpu)
 
 	# expected batch size when batch time 1ms
 	# ops/s = (target / clients)
@@ -56,28 +64,36 @@ for (arg in args) {
 	# rmt = s/batch, want s/op, so /= ops/batch => *= batch/ops
 	ts$rmt_norm = (ts$rmt * clients * 1000.0) / target
 
-	ts <- data.frame(ts)
+	ts <- as.data.frame(ts)
 
 	t <- rbind(t, ts)
 	close(con)
 }
 #t
 t = t[t$pct != 100,]
+t = t[t$pct != 99,]
 t$server <- as.factor(t$server)
 t$actual <- t$actual/1000000.0
 t$target <- t$target/1000000.0
 t$rmt <- t$rmt/1000.0
+t$rmt_min <- t$rmt_min/1000.0
+t$rmt_max <- t$rmt_max/1000.0
 t$sjrn <- t$sjrn/1000.0
+t$sjrn_min <- t$sjrn_min/1000.0
+t$sjrn_max <- t$sjrn_max/1000.0
 mx_rmt = max(t[t$pct == 50,]$rmt)
 mx_rmt_norm = max(t[t$pct == 50,]$rmt_norm)
 t$sjrn <- pmin(t$sjrn, 10*mx_rmt) # otherwise ggplot tries to plot all the way to 100k
+t$sjrn_min <- pmin(t$sjrn_min, 10*mx_rmt)
+t$sjrn_max <- pmin(t$sjrn_max, 10*mx_rmt)
+t
 
 library(ggplot2)
-p <- ggplot(data=t, aes(x=target, y=rmt, color=server, linetype=pct, shape=server))
+p <- ggplot(data=t, aes(x=target, y=rmt, color=server, linetype=pct, shape=server, ymin=rmt_min, ymax=rmt_max))
 p <- p + coord_trans(x = "identity", y = "identity", limy=c(0, mx_rmt))
 p <- p + facet_grid(skew ~ op)
-p <- p + scale_linetype_manual(breaks=c(50,95,99), values=c("dotted","dashed","solid"))
-p <- p + geom_point(size = 0.7, alpha = 0.8) + geom_line()
+p <- p + scale_linetype_manual(breaks=c(50,95,99), values=c("solid","dotted","dashed"))
+p <- p + geom_point(size = 0.7, alpha = 0.8) + geom_errorbar(width = 0.1) + geom_line()
 p <- p + xlab("target load (Mops/s)") + ylab("batch processing time (ms)") + ggtitle("Batch processing time")
 ggsave('plot-batch.png',plot=p,width=10,height=4)
 
@@ -85,27 +101,27 @@ p <- ggplot(data=t, aes(x=target, y=rmt_norm, color=server, linetype=pct, shape=
 #p <- p + coord_trans(x = "identity", y = "log10", limy=c(1, mx_rmt_norm))
 p <- p + scale_y_log10()
 p <- p + facet_grid(skew ~ op)
-p <- p + scale_linetype_manual(breaks=c(50,95,99), values=c("dotted","dashed","solid"))
+p <- p + scale_linetype_manual(breaks=c(50,95,99), values=c("solid","dotted","dashed"))
 p <- p + geom_point(size = 0.7, alpha = 0.8) + geom_line()
 p <- p + xlab("target load (Mops/s)") + ylab("op processing time (µs)") + ggtitle("Estimated per-operation latency")
 ggsave('plot-op-norm.png',plot=p,width=10,height=4)
 
 # for sojourn time, read and write should basically be the same
-worst <- data.frame(t %>% group_by(target, pct, server, skew) %>% summarize(sjrn = max(sjrn)))
-p <- ggplot(data=worst, aes(x=target, y=sjrn, color=server, linetype=pct, shape=server))
+worst <- data.frame(t %>% group_by(target, pct, server, skew) %>% summarize(sjrn = max(sjrn), sjrn_min = max(sjrn_min), sjrn_max = max(sjrn_max)))
+p <- ggplot(data=worst, aes(x=target, y=sjrn, color=server, linetype=pct, shape=server, ymin=sjrn_min, ymax=sjrn_max))
 p <- p + coord_trans(x = "identity", y = "identity", limy=c(0, 1.2*mx_rmt))
 p <- p + facet_wrap(~ skew)
-p <- p + scale_linetype_manual(breaks=c(50,95,99), values=c("dotted","dashed","solid"))
-p <- p + geom_point(size = 0.7, alpha = 0.8) + geom_line()
+p <- p + scale_linetype_manual(breaks=c(50,95,99), values=c("solid","dotted","dashed"))
+p <- p + geom_point(size = 0.7, alpha = 0.8) + geom_errorbar(width = 0.1) + geom_line()
 p <- p + xlab("target load (Mops/s)") + ylab("sojourn time (ms)") + ggtitle("Sojourn time")
 ggsave('plot-sjrn.png',plot=p,width=10,height=4)
 
 t <- t[t$pct == 50,]
 t <- t[t$op == "read",]
-p <- ggplot(data=t, aes(x=target, y=cpu, color=server, shape=server))
+p <- ggplot(data=t, aes(x=target, y=cpu, color=server, shape=server, ymin=cpu_min, ymax=cpu_max))
 p <- p + facet_wrap(~ skew)
 p <- p + geom_hline(yintercept=16)
-p <- p + geom_point(size = 0.7, alpha = 0.8) + geom_line()
+p <- p + geom_point(size = 0.7, alpha = 0.8) + geom_errorbar(width = 0.1) + geom_line()
 p <- p + xlab("target load (Mops/s)") + ylab("cpu load") + ggtitle("Server CPU load")
 ggsave('plot-cpu.png',plot=p,width=10,height=4)
 
