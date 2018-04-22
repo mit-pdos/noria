c47a241872a104834784c0318dd0feda835b2b5e
Malte Schwarzkopf <malte@csail.mit.edu>
2018-04-21 23:29:35 -0400

    Find parent column IDs using table and column name
    Rather than swatting bugs related to slight divergences in meta-data on
    columns one by one, this should, in one fell swoop, introduce a general
    solution. Any caller of `column_id_for_column` in the query planning
    code now compares name and table only, ignoring differences in the alias
    and function fields. Code that does not use
    `MirNode::column_id_for_column` still has to be careful.

--- mir/src/node.rs
+++ mir/src/node.rs
@@ -226,7 +226,10 @@ impl MirNode {
             },
             MirNodeType::Reuse { ref node } => node.borrow().column_id_for_column(c),
             // otherwise, just look up in the column set
-            _ => match self.columns.iter().position(|cc| cc == c) {
+            _ => match self.columns
+                .iter()
+                .position(|cc| cc.name == c.name && cc.table == c.table)
+            {
                 None => {
                     panic!(
                         "tried to look up non-existent column {:?} on node \"{}\" (columns: {:?})",
