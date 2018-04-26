7c4a237a2ef02ae8ea9ac4b54e3aef35652e78d3
Jon Gjengset <jon@thesquareplanet.com>
2018-04-26 13:49:03 -0400

    Don't require db-schema to not have newlines

--- lobsters/mysql/src/main.rs
+++ lobsters/mysql/src/main.rs
@@ -69,12 +69,17 @@ impl trawler::LobstersClient for MysqlTrawler {
                 .and_then(|c| c.drop_query(&format!("CREATE DATABASE {}", db)))
                 .and_then(|c| c.drop_query(&format!("USE {}", db))),
         ).unwrap();
+        let mut current_q = String::new();
         for q in include_str!("../db-schema.sql").lines() {
-            if q.starts_with("--") {
+            if q.starts_with("--") || q.is_empty() {
                 continue;
             }
-            core.run(c.get_conn().and_then(|c| c.drop_query(q)))
-                .unwrap();
+            current_q.push_str(q);
+            if current_q.ends_with(';') {
+                core.run(c.get_conn().and_then(|c| c.drop_query(&current_q)))
+                    .unwrap();
+                current_q.clear();
+            }
         }
     }
 
