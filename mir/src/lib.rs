ae39e8daa08cbf553abd9bc844c52d5440677487
Malte Schwarzkopf <malte@csail.mit.edu>
2018-07-05 19:47:59 -0400

    New MIR Column data structure
    Start work on a new canonical column representation for MIR that can
    capture column equivalence (e.g., between join columns, alias-renamed
    columns, columns of views and base tables, etc.).
    
    Importantly a `mir::Column` compares equal to all columns listed in
    its `aliases` vector.

--- mir/src/lib.rs
+++ mir/src/lib.rs
@@ -8,7 +8,10 @@ extern crate regex;
 extern crate slog;
 
 use std::cell::RefCell;
+use std::cmp::Ordering;
 use std::rc::Rc;
+
+use nom_sql::FunctionExpression;
 
 use basics::*;
 
@@ -21,6 +24,71 @@ pub mod visualize;
 
 pub type MirNodeRef = Rc<RefCell<node::MirNode>>;
 
+#[derive(Clone, Debug, Eq, Hash)]
+pub struct Column {
+    pub table: Option<String>,
+    pub name: String,
+    pub function: Option<Box<FunctionExpression>>,
+    pub aliases: Vec<Column>,
+}
+
+impl From<nom_sql::Column> for Column {
+    fn from(c: nom_sql::Column) -> Column {
+        Column {
+            aliases: match &c.alias {
+                Some(_) => vec![Column::from(nom_sql::Column {
+                    name: c.name.to_owned(),
+                    table: c.table.clone(),
+                    alias: None,
+                    function: c.function.clone(),
+                })],
+                None => vec![],
+            },
+            table: c.table,
+            name: match c.alias {
+                Some(a) => a,
+                None => c.name,
+            },
+            function: c.function,
+        }
+    }
+}
+
+impl PartialEq for Column {
+    fn eq(&self, other: &Column) -> bool {
+        (self.name == other.name && self.table == other.table && self.function == other.function)
+            || self.aliases.contains(other) || other.aliases.contains(self)
+    }
+}
+
+impl Ord for Column {
+    fn cmp(&self, other: &Column) -> Ordering {
+        if self.table.is_some() && other.table.is_some() {
+            match self.table.cmp(&other.table) {
+                Ordering::Equal => self.name.cmp(&other.name),
+                x => x,
+            }
+        } else {
+            self.name.cmp(&other.name)
+        }
+    }
+}
+
+impl PartialOrd for Column {
+    fn partial_cmp(&self, other: &Column) -> Option<Ordering> {
+        if self.table.is_some() && other.table.is_some() {
+            match self.table.cmp(&other.table) {
+                Ordering::Equal => Some(self.name.cmp(&other.name)),
+                x => Some(x),
+            }
+        } else if self.table.is_none() && other.table.is_none() {
+            Some(self.name.cmp(&other.name))
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub enum FlowNode {
     New(NodeIndex),
@@ -31,5 +99,62 @@ impl FlowNode {
         match *self {
             FlowNode::New(na) | FlowNode::Existing(na) => na,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn column_from_nomsql_column() {
+        let nsc = nom_sql::Column::from("t.col");
+        assert_eq!(
+            Column::from(nsc),
+            Column {
+                table: Some("t".into()),
+                name: "col".into(),
+                function: None,
+                aliases: vec![],
+            }
+        );
+
+        let nsc_with_alias = nom_sql::Column {
+            name: "colwa".into(),
+            table: Some("t".into()),
+            alias: Some("al".into()),
+            function: None,
+        };
+        assert_eq!(
+            Column::from(nsc_with_alias),
+            Column {
+                table: Some("t".into()),
+                name: "al".into(),
+                function: None,
+                aliases: vec![Column {
+                    table: Some("t".into()),
+                    name: "colwa".into(),
+                    function: None,
+                    aliases: vec![],
+                }],
+            }
+        );
+    }
+
+    #[test]
+    fn column_equality() {
+        let c = Column::from(nom_sql::Column::from("t.col"));
+        let ac = Column {
+            table: Some("t".into()),
+            name: "al".into(),
+            function: None,
+            aliases: vec![c.clone()],
+        };
+
+        // column is equal to itself
+        assert_eq!(c, c);
+
+        // column is equal to its aliases
+        assert_eq!(ac, c);
     }
 }
