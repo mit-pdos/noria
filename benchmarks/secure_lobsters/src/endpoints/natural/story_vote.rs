7df9a32898b04694d2a096cdbb3a19f7c1184a77
Alana Marzoev <alanamarzoev@gmail.com>
2018-12-23 22:09:46 -0500

    sec

---
+++ benchmarks/secure_lobsters/src/endpoints/natural/story_vote.rs
@@ -0,0 +1,59 @@
+use futures::Future;
+use my;
+use my::prelude::*;
+use trawler::{StoryId, UserId, Vote};
+
+pub(crate) fn handle<F>(
+    c: F,
+    acting_as: Option<UserId>,
+    story: StoryId,
+    v: Vote,
+) -> Box<Future<Item = (my::Conn, bool), Error = my::errors::Error>>
+where
+    F: 'static + Future<Item = my::Conn, Error = my::errors::Error>,
+{
+    let user = acting_as.unwrap();
+    Box::new(
+        c.and_then(move |c| {
+            c.prep_exec(
+                "SELECT `stories`.* \
+                 FROM `stories` \
+                 WHERE `stories`.`short_id` = ?",
+                (::std::str::from_utf8(&story[..]).unwrap(),),
+            ).and_then(|result| result.collect_and_drop::<my::Row>())
+                .map(|(c, mut story)| (c, story.swap_remove(0)))
+        }).and_then(move |(c, story)| {
+                let id = story.get::<u32, _>("id").unwrap();
+                c.drop_exec(
+                    "SELECT  `votes`.* \
+                     FROM `votes` \
+                     WHERE `votes`.`user_id` = ? \
+                     AND `votes`.`story_id` = ? \
+                     AND `votes`.`comment_id` IS NULL",
+                    (user, id),
+                ).map(move |c| (c, id))
+            })
+            .and_then(move |(c, story)| {
+                // TODO: do something else if user has already voted
+                // TODO: technically need to re-load story under transaction
+
+                // NOTE: MySQL technically does everything inside this and_then in a transaction,
+                // but let's be nice to it
+                c.drop_exec(
+                    "INSERT INTO `votes` \
+                     (`user_id`, `story_id`, `vote`) \
+                     VALUES \
+                     (?, ?, ?)",
+                    (
+                        user,
+                        story,
+                        match v {
+                            Vote::Up => 1,
+                            Vote::Down => 0,
+                        },
+                    ),
+                )
+            })
+            .map(|c| (c, false)),
+    )
+}
---
+++ benchmarks/secure_lobsters/src/endpoints/original/mod.rs
@@ -0,0 +1,35 @@
+pub(crate) mod comment;
+pub(crate) mod comment_vote;
+pub(crate) mod comments;
+pub(crate) mod frontpage;
+pub(crate) mod recent;
+pub(crate) mod story;
+pub(crate) mod story_vote;
+pub(crate) mod submit;
+pub(crate) mod user;
+
+use futures::Future;
+use my;
+use my::prelude::*;
+
+pub(crate) fn notifications(
+    c: my::Conn,
+    uid: u32,
+) -> impl Future<Item = my::Conn, Error = my::errors::Error> {
+    c.drop_exec(
+        "SELECT COUNT(*) \
+                     FROM `replying_comments_for_count`
+                     WHERE `replying_comments_for_count`.`user_id` = ? \
+                     GROUP BY `replying_comments_for_count`.`user_id` \
+                     ",
+        (uid,),
+    )
+    .and_then(move |c| {
+        c.drop_exec(
+            "SELECT `keystores`.* \
+             FROM `keystores` \
+             WHERE `keystores`.`key` = ?",
+            (format!("user:{}:unread_messages", uid),),
+        )
+    })
+}
