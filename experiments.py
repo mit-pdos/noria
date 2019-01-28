6c7bb046f40714d446f4c324af5aef54acd8ebff
Alana Marzoev <alanamarzoev@gmail.com>
2019-01-28 12:20:26 -0500

    experiments script

---
+++ experiments.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+nusers = [100, 1000, 10000]
+nclasses = [100, 500, 1000]
+nposts = [1, 10, 100]
+
+
+for users in nusers:
+    for classes in nclasses:
+        for posts in nposts:
+            postcount = posts * classes
+            subprocess.call("gtime cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p {} -u {} -c {} --policies noria-benchmarks/piazza/basic-policies.json > exp-{}-{}-{}.txt".format(users, postcount, users, classes, users, postcount, classes), shell=True)
