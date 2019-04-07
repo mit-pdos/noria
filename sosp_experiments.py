071270b0ba2870a6761e22d4e4102b4473626ea6
Alana Marzoev <alanamarzoev@gmail.com>
2019-04-07 11:05:53 -0400

    read thruput exp

--- sosp_experiments.py
+++ sosp_experiments.py
@@ -6,25 +6,42 @@ import numpy as np
 
 
 # Read txput experiments:
-# 5k users, 1mill posts, 1k classes. partially materialized. read txput & mem OH. 10 classes per user. basic policies.
-cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 10 --policies noria-benchmarks/piazza/basic-policies.json --partial > exp1.txt"
+# partial basic
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/basic-policies.json > 5kpartialbasic.txt"
 subprocess.call(cmd, shell=True)
 
-# 5k users, 1mill posts, 1k classes. partially materialized. read txput & mem OH. 10 classes per user. basic policies.
-cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 10 --policies noria-benchmarks/piazza/basic-policies.json --partial > exp2.txt"
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 1000 -p 1000000 -u 1000 -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/basic-policies.json > 1kpartialbasic.txt"
 subprocess.call(cmd, shell=True)
 
-# 5k users, 1mill posts, 1k classes. fully materialized. read txput & mem OH. 1000 classes per user. complex policies.
-cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 1000 --policies noria-benchmarks/piazza/complex-policies.json > exp3.txt"
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 3000 -p 1000000 -u 3000 -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/basic-policies.json > 3kpartialbasic.txt"
 subprocess.call(cmd, shell=True)
 
-# 5k users, 1mill posts, 1k classes. fully materialized. read txput & mem OH. 1000 classes per user. complex policies.
-cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 1000 --policies noria-benchmarks/piazza/complex-policies.json > exp4.txt"
+# full basic
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 10 --policies noria-benchmarks/piazza/basic-policies.json > 5kfullbasic.txt"
 subprocess.call(cmd, shell=True)
 
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 1000 -p 1000000 -u 1000 -c 1000 -m 10 --policies noria-benchmarks/piazza/basic-policies.json > 1kfullbasic.txt"
+subprocess.call(cmd, shell=True)
 
-# Write txput experiments:
-# 5k users, 1mill posts, 1k classes. fully materialized. write txput & mem OH. 10 classes per user.
-# 5k users, 1mill posts, 1k classes. fully materialized. write txput & mem OH. 10 classes per user.
-# 5k users, 1mill posts, 1k classes. fully materialized. write txput & mem OH. 1000 classes per user.
-# 5k users, 1mill posts, 1k classes. fully materialized. write txput & mem OH. 1000 classes per user.
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 3000 -p 1000000 -u 3000 -c 1000 -m 10 --policies noria-benchmarks/piazza/basic-policies.json > 3kfullbasic.txt"
+subprocess.call(cmd, shell=True)
+
+# full complex
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 10 --policies noria-benchmarks/piazza/complex-policies.json > 5kfullcomplex.txt"
+subprocess.call(cmd, shell=True)
+
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 1000 -p 1000000 -u 1000 -c 1000 -m 10 --policies noria-benchmarks/piazza/complex-policies.json > 1kfullcomplex.txt"
+subprocess.call(cmd, shell=True)
+
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 3000 -p 1000000 -u 3000 -c 1000 -m 10 --policies noria-benchmarks/piazza/complex-policies.json > 3kfullcomplex.txt"
+subprocess.call(cmd, shell=True)
+
+# partial complex
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 5000 -p 1000000 -u 5000 -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/complex-policies.json > 5kpartialcomplex.txt"
+subprocess.call(cmd, shell=True)
+
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 1000 -p 1000000 -u 1000 -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/complex-policies.json > 1kpartialcomplex.txt"
+subprocess.call(cmd, shell=True)
+
+cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l 3000 -p 1000000 -u 3000 -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/complex-policies.json > 3kpartialcomplex.txt"
+subprocess.call(cmd, shell=True)
