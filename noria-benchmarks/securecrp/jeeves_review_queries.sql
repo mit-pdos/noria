b78e02e2d58684001f636c5a15c718a5b19020bc
Samyu Yagati <samyuyagati19@gmail.com>
2019-04-08 17:22:56 -0400

    Added (in comments) test case for column alias bug in make_union_node_sec

---
+++ noria-benchmarks/securecrp/jeeves_review_queries.sql
@@ -0,0 +1,11 @@
+QUERY ReviewList: SELECT Paper.id, Review.reviewer, Review.contents, Review.score_novelty, Review.score_presentation, Review.score_technical, Review.score_confidence, Paper.author as author
+           FROM Paper
+--	   WHERE Review.paper = ?;
+	   JOIN Review
+	   ON (Paper.id = Review.paper);
+
+-- Below query is broken because a Union is attempted with two tables that use "id" as column name and one table that uses "paper" (but has id as an alias), and checking for matching columns is done by name (without reference to aliases).
+--QUERY ReviewList: SELECT Paper.id, Review.reviewer, Review.contents, Review.score_novelty, Review.score_presentation, Review.score_technical, Review.score_confidence, Paper.author as author
+--           FROM Review
+--	   JOIN Paper
+--	   ON (Paper.id = Review.paper);
