a87084992805744c7e21b6c1fe22fb5a0bacc9a8
Alana Marzoev <alanamarzoev@gmail.com>
2019-04-09 15:18:38 -0400

    catch up to where everything was before, minus write policies

---
+++ noria-benchmarks/piazza/postcount-queries.sql
@@ -0,0 +1,1 @@
+QUERY post_count: select p_cid, count(p_id) from Post where p_cid = ? group by p_cid;
---
+++ sosp_experiments.py
@@ -0,0 +1,43 @@
+import subprocess
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# EXPERIMENT 1:
+# read throughput as a function of the number of users. 1k to 5k users. 1 million posts, 1000 classes. 90% public posts. users enrolled in 10 classes each.
+nusers = [100, 200, 500]
+policies = ["basic-policies"]
+queries = ["post", "postcount"]
+
+for users in nusers:
+    for mode in policies:
+        for query in queries:
+            cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/{}.json -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}-{}_queries-partial_READ.txt".format(users, users, mode, query, users, mode, query)
+            subprocess.call(cmd, shell=True)
+            cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 --policies noria-benchmarks/piazza/{}.json -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}-{}_queries-full_READ.txt".format(users, users, mode, query, users, mode, query)
+            subprocess.call(cmd, shell=True)
+
+
+# EXPERIMENT 2 (and 3):
+# 2: write throughput as a function of the number of users.
+# 3: write throughput as a function of query complexity. (no policies, basic policies, complex policies)
+all_policies = ["basic-policies", "no"]
+for users in nusers:
+    for mode in all_policies:
+        for query in queries:
+            cmd = "/usr/bin/time perflock cargo run --release --bin=piazza-write --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 --partial --policies noria-benchmarks/piazza/{}.json -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}-{}_queries-partial_WRITE.txt".format(users, users, mode, query, users, mode, query)
+            subprocess.call(cmd, shell=True)
+            cmd = "/usr/bin/time perflock cargo run --release --bin=piazza-write --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 --policies noria-benchmarks/piazza/{}.json -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}-{}_queries-full_WRITE.txt".format(users, users, mode, query, users, mode, query)
+            subprocess.call(cmd, shell=True)
+            cmd = "/usr/bin/time perflock cargo run --release --bin=piazza-write --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}_policies-{}_queries-full_WRITE.txt".format(users, users, query, users, mode, query)
+            subprocess.call(cmd, shell=True)
+
+
+# EXPERIMENT 4:
+# memory overhead as a function of number of users. (with groups + without)
+for users in nusers:
+    for query in ["post"]:
+        for mode in policies:
+            cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 --policies noria-benchmarks/piazza/{}.json -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}-{}_queries-full_MEMORY.txt".format(users, users, mode, query, users, mode, query)
+            subprocess.call(cmd, shell=True)
