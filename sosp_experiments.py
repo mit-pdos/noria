ff30cf2e282766be2096394a7ec4f491e5bd3ab7
Alana Marzoev <alanamarzoev@gmail.com>
2019-04-10 13:19:35 -0400

    postcount exp

--- sosp_experiments.py
+++ sosp_experiments.py
@@ -6,9 +6,9 @@ import numpy as np
 
 # EXPERIMENT 1:
 # read throughput as a function of the number of users. 1k to 5k users. 1 million posts, 1000 classes. 90% public posts. users enrolled in 10 classes each.
-nusers = [100, 200, 500]
+nusers = [1000, 2000, 5000]
 policies = ["basic-policies"]
-queries = ["post", "postcount"]
+queries = ["postcount"]
 
 for users in nusers:
     for mode in policies:
@@ -36,8 +36,8 @@ for users in nusers:
 
 # EXPERIMENT 4:
 # memory overhead as a function of number of users. (with groups + without)
-for users in nusers:
-    for query in ["post"]:
-        for mode in policies:
-            cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 --policies noria-benchmarks/piazza/{}.json -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}-{}_queries-full_MEMORY.txt".format(users, users, mode, query, users, mode, query)
-            subprocess.call(cmd, shell=True)
+# for users in nusers:
+#     for query in ["post"]:
+#         for mode in policies:
+#             cmd = "/usr/bin/time perflock cargo run --release --bin=piazza --manifest-path noria-benchmarks/Cargo.toml -- -l {} -p 1000000 -u {} -c 1000 -m 10 --policies noria-benchmarks/piazza/{}.json -q noria-benchmarks/piazza/{}-queries.sql > {}_users-{}-{}_queries-full_MEMORY.txt".format(users, users, mode, query, users, mode, query)
+#             subprocess.call(cmd, shell=True)
