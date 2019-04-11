e53e4f208ac21675b4fb29568597b9fecda57fc0
Jon Gjengset <jon@thesquareplanet.com>
2019-04-11 14:30:23 -0400

    Make purge-stress better

--- noria-benchmarks/vote-purge-stress/main.rs
+++ noria-benchmarks/vote-purge-stress/main.rs
@@ -6,7 +6,7 @@ extern crate hdrhistogram;
 extern crate noria;
 extern crate rand;
 
-use clap::{App, Arg};
+use clap::{value_t_or_exit, App, Arg};
 use futures::Future;
 use hdrhistogram::Histogram;
 use noria::{Builder, DurabilityMode, PersistenceParameters, SyncHandle};
@@ -17,25 +17,58 @@ CREATE TABLE Article (id int, title varchar(255), PRIMARY KEY(id));
 CREATE TABLE Vote (article_id int, user int);
 
 # read queries
-QUERY ArticleWithVoteCount: SELECT Article.id, title, VoteCount.votes AS votes \
+QUERY SHALLOW_ArticleWithVoteCount: SELECT Article.id, title, VoteCount.votes AS votes \
             FROM Article \
             LEFT JOIN (SELECT Vote.article_id, COUNT(user) AS votes \
                        FROM Vote GROUP BY Vote.article_id) AS VoteCount \
             ON (Article.id = VoteCount.article_id) WHERE Article.id = ?;";
 
 fn main() {
-    let verbose = false;
+    let args = App::new("purge-stress")
+        .about("Benchmarks the latency of full replays in a user-curated news aggregator")
+        .arg(
+            Arg::with_name("flush-timeout")
+                .long("flush-timeout")
+                .takes_value(true)
+                .default_value("100000")
+                .help("Time to wait before processing a merged packet, in nanoseconds."),
+        )
+        .arg(
+            Arg::with_name("replay-timeout")
+                .long("replay-timeout")
+                .takes_value(true)
+                .default_value("100000")
+                .help("Time to batch replay requests for, in nanoseconds."),
+        )
+        .arg(
+            Arg::with_name("no-purge")
+                .long("no-purge")
+                .help("Disable purging"),
+        )
+        .arg(Arg::with_name("verbose").long("verbose").short("v"))
+        .get_matches();
 
     let mut builder = Builder::default();
-    if verbose {
+    if args.is_present("verbose") {
         builder.log_with(noria::logger_pls());
     }
 
     builder.set_persistence(PersistenceParameters {
         mode: DurabilityMode::MemoryOnly,
+        flush_timeout: Duration::new(0, value_t_or_exit!(args, "flush-timeout", u32)),
         ..Default::default()
     });
     builder.set_sharding(None);
+    builder.set_partial_replay_batch_timeout(Duration::new(
+        0,
+        value_t_or_exit!(args, "replay-timeout", u32),
+    ));
+
+    let (recipe, view) = if args.is_present("no-purge") {
+        (RECIPE.replace("SHALLOW_", ""), "ArticleWithVoteCount")
+    } else {
+        (RECIPE.to_string(), "SHALLOW_ArticleWithVoteCount")
+    };
 
     let mut rt = tokio::runtime::Runtime::new().unwrap();
     let ex = rt.executor();
@@ -44,13 +77,15 @@ fn main() {
             builder
                 .start_local()
                 .map(move |wh| SyncHandle::from_executor(ex, wh))
-                .and_then(|mut graph| graph.handle().install_recipe(RECIPE).map(move |_| graph)),
+                .and_then(move |mut graph| {
+                    graph.handle().install_recipe(&recipe).map(move |_| graph)
+                }),
         )
         .unwrap();
 
     let mut a = g.table("Article").unwrap().into_sync();
     let mut v = g.table("Vote").unwrap().into_sync();
-    let mut r = g.view("ArticleWithVoteCount").unwrap().into_sync();
+    let mut r = g.view(view).unwrap().into_sync();
 
     // seed articles
     a.insert(vec![1.into(), "Hello world #1".into()]).unwrap();
