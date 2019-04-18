1b416b9b2962a9fcd3c4285071a66e7133dae266
Jon Gjengset <jon@thesquareplanet.com>
2019-04-18 10:48:10 -0400

    Run multiple iterations at once

--- noria-benchmarks/piazza/run.sh
+++ noria-benchmarks/piazza/run.sh
@@ -14,6 +14,6 @@ for l in 0.25 0.5 0.75 1.0; do
 for setup in "partial" "shallow-readers" "full"; do
 	prefix="piazza-${setup}-$(echo "$l" | sed 's/\./_/')l"
 	echo "==> $setup (logged-in fraction: $l)"
-	perflock /usr/bin/time -v cargo r -q --release --bin piazza -- -m "$setup" -u 1000 -l $l -g "${prefix}.gv" > "${prefix}.log" 2> "${prefix}.err"
+	perflock cargo r -q --release --bin piazza -- -m "$setup" --iter 3 -u 1000 -l $l -g "${prefix}.gv" > "${prefix}.log" 2> "${prefix}.err"
 done
 done
