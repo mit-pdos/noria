a25dbe921340900922301d16ef171d2d4cafe4c1
Gina Yuan <gina.yuan@gmail.com>
2019-06-26 11:32:14 -0400

    No need to restore upstream egress txs since they will not be touched

--- noria-server/src/controller/migrate/mod.rs
+++ noria-server/src/controller/migrate/mod.rs
@@ -645,7 +645,6 @@ impl<'a> Migration<'a> {
         // Note: workers and domains are sorted for determinism
         let mut wis = reset_wis(mainline);
         let mut changed_domains = changed_domains.into_iter().collect::<Vec<DomainIndex>>();
-        let mut replicated_egress = HashSet::new();
 
         // These aren't really new nodes since they've already been assigned local indexes and
         // domains. All we want to do is place the new domain, and fix routing/materializations.
@@ -656,9 +655,6 @@ impl<'a> Migration<'a> {
             uninformed_domain_nodes.insert(*domain, nodes.iter().map(|&n| (n, true)).collect());
             changed_domains.push(*domain);
             for &ni in nodes {
-                if mainline.ingredients[ni].is_egress() {
-                    replicated_egress.insert(ni);
-                }
                 new.insert(ni);
             }
         }
@@ -758,7 +754,6 @@ impl<'a> Migration<'a> {
             &mut mainline.domains,
             &mainline.workers,
             &new,
-            &replicated_egress,
             &self.linked,
         );
 
