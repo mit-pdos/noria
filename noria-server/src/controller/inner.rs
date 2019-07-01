mit-pdos/noria

commit bf27863803b060519e04c89c6a9a657104d228bf

Make statistics work again

noria-server/src/controller/inner.rs
@@ -201,6 +201,9 @@ impl ControllerInner {
                 return Ok(Ok(json::to_string(&self.graphviz(true)).unwrap()));
             }
             (&Method::GET, "/get_statistics") => {
+                return Ok(Ok(format!("{:#?}", self.get_statistics())));
+            }
+            (&Method::POST, "/get_statistics") => {
                 return Ok(Ok(json::to_string(&self.get_statistics()).unwrap()));
             }
             _ => {}
@@ -859,6 +862,8 @@ impl ControllerInner {
 
     /// Get statistics about the time spent processing different parts of the graph.
     fn get_statistics(&mut self) -> GraphStats {
+        trace!(self.log, "asked to get statistics");
+        let log = &self.log;
         let workers = &self.workers;
         let replies = &mut self.replies;
         // TODO: request stats from domains in parallel.
@@ -866,6 +871,7 @@ impl ControllerInner {
             .domains
             .iter_mut()
             .flat_map(|(&di, s)| {
+                trace!(log, "requesting stats from domain"; "di" => di.index());
                 s.send_to_healthy(box Packet::GetStatistics, workers)
                     .unwrap();
                 replies

noria/src/controller.rs
@@ -500,6 +500,14 @@ where
         &mut self.handle
     }
 
+    /// Get statistics about the time spent processing different parts of the graph.
+    ///
+    /// See [`ControllerHandle::statistics`].
+    pub fn statistics(&mut self) -> Result<stats::GraphStats, failure::Error> {
+        let fut = self.handle.statistics();
+        self.run(fut)
+    }
+
     /// Enumerate all known base tables.
     ///
     /// See [`ControllerHandle::inputs`].

commit 42ac55e7f73ee2cecd89413d1a9211742e7c0911

Return statistics in JSON format again
bf27863 broke the UI by returning Debug (not JSON) text.

noria-server/src/controller/inner.rs
@@ -220,7 +220,7 @@ impl ControllerInner {
                 return Ok(Ok(json::to_string(&self.graphviz(true)).unwrap()));
             }
             (&Method::GET, "/get_statistics") => {
-                return Ok(Ok(format!("{:#?}", self.get_statistics())));
+                return Ok(Ok(json::to_string(&self.get_statistics()).unwrap()));
             }
             (&Method::POST, "/get_statistics") => {
                 return Ok(Ok(json::to_string(&self.get_statistics()).unwrap()));