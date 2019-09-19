c3d68aa14295e4255d3462781fd2c7c8b57426f4
Jackie Bredenberg <jamb@mit.edu>
2019-09-19 10:03:36 -0400

    Added filter functionality, updated tests

--- noria-server/dataflow/src/ops/grouped/filteraggregate.rs
+++ noria-server/dataflow/src/ops/grouped/filteraggregate.rs
@@ -1,5 +1,9 @@
+use std::sync;
+
+pub use nom_sql::Operator;
 use ops::grouped::GroupedOperation;
 use ops::grouped::GroupedOperator;
+use ops::filter::{FilterCondition, Value};
 
 use prelude::*;
 
@@ -21,6 +25,7 @@ impl FilterAggregation {
     pub fn over(
         self,
         src: NodeIndex,
+        filter: &[Option<FilterCondition>],
         over: usize,
         group_by: &[usize],
     ) -> GroupedOperator<FilterAggregator> {
@@ -32,6 +37,7 @@ impl FilterAggregation {
             src,
             FilterAggregator {
                 op: self,
+                filter: sync::Arc::new(Vec::from(filter)),
                 over,
                 group: group_by.into(),
             },
@@ -56,6 +62,7 @@ impl FilterAggregation {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct FilterAggregator {
     op: FilterAggregation,
+    filter: sync::Arc<Vec<Option<FilterCondition>>>,
     over: usize,
     group: Vec<usize>,
 }
@@ -75,22 +82,54 @@ impl GroupedOperation for FilterAggregator {
     }
 
     fn to_diff(&self, r: &[DataType], pos: bool) -> Self::Diff {
-        match self.op {
-            FilterAggregation::COUNT if pos => 1,
-            FilterAggregation::COUNT => -1,
-            FilterAggregation::SUM => {
-                let v = match r[self.over] {
-                    DataType::Int(n) => i128::from(n),
-                    DataType::UnsignedInt(n) => i128::from(n),
-                    DataType::BigInt(n) => i128::from(n),
-                    DataType::UnsignedBigInt(n) => i128::from(n),
-                    DataType::None => 0,
-                    ref x => unreachable!("tried to aggregate over {:?} on {:?}", x, r),
-                };
-                if pos {
-                    v
-                } else {
-                    0i128 - v
+        let passes_filter = self.filter.iter().enumerate().all(|(i, fi)| {
+            // check if this filter matches
+            let d = &r[i];
+            if let Some(ref cond) = *fi {
+                match *cond {
+                    FilterCondition::Comparison(ref op, ref f) => {
+                        let v = match *f {
+                            Value::Constant(ref dt) => dt,
+                            Value::Column(c) => &r[c],
+                        };
+                        match *op {
+                            Operator::Equal => d == v,
+                            Operator::NotEqual => d != v,
+                            Operator::Greater => d > v,
+                            Operator::GreaterOrEqual => d >= v,
+                            Operator::Less => d < v,
+                            Operator::LessOrEqual => d <= v,
+                            Operator::In => unreachable!(),
+                            _ => unimplemented!(),
+                        }
+                    }
+                    FilterCondition::In(ref fs) => fs.contains(d),
+                }
+            } else {
+                // everything matches no condition
+                true
+            }
+        });
+        if !passes_filter {
+            0
+        } else {
+            match self.op {
+                FilterAggregation::COUNT if pos => 1,
+                FilterAggregation::COUNT => -1,
+                FilterAggregation::SUM => {
+                    let v = match r[self.over] {
+                        DataType::Int(n) => i128::from(n),
+                        DataType::UnsignedInt(n) => i128::from(n),
+                        DataType::BigInt(n) => i128::from(n),
+                        DataType::UnsignedBigInt(n) => i128::from(n),
+                        DataType::None => 0,
+                        ref x => unreachable!("tried to aggregate over {:?} on {:?}", x, r),
+                    };
+                    if pos {
+                        v
+                    } else {
+                        0i128 - v
+                    }
                 }
             }
         }
@@ -115,14 +154,15 @@ impl GroupedOperation for FilterAggregator {
     fn description(&self, detailed: bool) -> String {
         if !detailed {
             return String::from(match self.op {
-                FilterAggregation::COUNT => "+",
-                FilterAggregation::SUM => "𝛴",
+                FilterAggregation::COUNT => "+σ",
+                FilterAggregation::SUM => "𝛴σ",
             });
         }
 
+        // TODO could include information about filter condition
         let op_string = match self.op {
-            FilterAggregation::COUNT => "|*|".into(),
-            FilterAggregation::SUM => format!("𝛴({})", self.over),
+            FilterAggregation::COUNT => "|σ(*)|".into(),
+            FilterAggregation::SUM => format!("𝛴(σ({}))", self.over),
         };
         let group_cols = self
             .group
@@ -150,7 +190,17 @@ mod tests {
         g.set_op(
             "identity",
             &["x", "ys"],
-            FilterAggregation::COUNT.over(s.as_global(), 1, &[0]),
+            FilterAggregation::COUNT.over(
+                s.as_global(),
+                &[
+                    None,
+                    Some(FilterCondition::Comparison(
+                        Operator::Equal,
+                        Value::Constant(2.into()),
+                    )),
+                ],
+                1,
+                &[0]),
             mat,
         );
         g
@@ -162,7 +212,18 @@ mod tests {
         g.set_op(
             "identity",
             &["x", "z", "ys"],
-            FilterAggregation::COUNT.over(s.as_global(), 1, &[0, 2]),
+            FilterAggregation::COUNT.over(
+                s.as_global(),
+                &[
+                    None,
+                    Some(FilterCondition::Comparison(
+                        Operator::Equal,
+                        Value::Constant(2.into()),
+                    )),
+                    None,
+                ],
+                1,
+                &[0, 2]),
             mat,
         );
         g
@@ -172,11 +233,11 @@ mod tests {
     fn it_describes() {
         let s = 0.into();
 
-        let c = FilterAggregation::COUNT.over(s, 1, &[0, 2]);
-        assert_eq!(c.description(true), "|*| γ[0, 2]");
+        let c = FilterAggregation::COUNT.over(s, &[None, None, None], 1, &[0, 2]);
+        assert_eq!(c.description(true), "|σ(*)| γ[0, 2]");
 
-        let s = FilterAggregation::SUM.over(s, 1, &[2, 0]);
-        assert_eq!(s.description(true), "𝛴(1) γ[2, 0]");
+        let s = FilterAggregation::SUM.over(s, &[None, None, None], 1, &[2, 0]);
+        assert_eq!(s.description(true), "𝛴(σ(1)) γ[2, 0]");
     }
 
     #[test]
@@ -186,7 +247,7 @@ mod tests {
 
         let u: Record = vec![1.into(), 1.into()].into();
 
-        // first row for a group should emit +1 for that group
+        // this should get filtered out
         let rs = c.narrow_one(u, true);
         assert_eq!(rs.len(), 1);
         let mut rs = rs.into_iter();
@@ -194,7 +255,7 @@ mod tests {
         match rs.next().unwrap() {
             Record::Positive(r) => {
                 assert_eq!(r[0], 1.into());
-                assert_eq!(r[1], 1.into());
+                assert_eq!(r[1], 0.into());
             }
             _ => unreachable!(),
         }
@@ -214,7 +275,7 @@ mod tests {
             _ => unreachable!(),
         }
 
-        let u: Record = vec![1.into(), 2.into()].into();
+        let u: Record = vec![2.into(), 2.into()].into();
 
         // second row for a group should emit -1 and +2
         let rs = c.narrow_one(u, true);
@@ -223,20 +284,20 @@ mod tests {
 
         match rs.next().unwrap() {
             Record::Negative(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 1.into());
             }
             _ => unreachable!(),
         }
         match rs.next().unwrap() {
             Record::Positive(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 2.into());
             }
             _ => unreachable!(),
         }
 
-        let u = (vec![1.into(), 1.into()], false);
+        let u = (vec![2.into(), 2.into()], false);
 
         // negative row for a group should emit -2 and +1
         let rs = c.narrow_one_row(u, true);
@@ -245,14 +306,14 @@ mod tests {
 
         match rs.next().unwrap() {
             Record::Negative(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 2.into());
             }
             _ => unreachable!(),
         }
         match rs.next().unwrap() {
             Record::Positive(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 1.into());
             }
             _ => unreachable!(),
@@ -261,9 +322,9 @@ mod tests {
         let u = vec![
             (vec![1.into(), 1.into()], false),
             (vec![1.into(), 1.into()], true),
-            (vec![1.into(), 2.into()], true),
-            (vec![2.into(), 2.into()], false),
-            (vec![2.into(), 2.into()], true),
+            (vec![1.into(), 2.into()], true),  // these are the
+            (vec![2.into(), 2.into()], false), // only rows that
+            (vec![2.into(), 2.into()], true),  // aren't ignored
             (vec![2.into(), 3.into()], true),
             (vec![2.into(), 1.into()], true),
             (vec![3.into(), 3.into()], true),
@@ -271,32 +332,25 @@ mod tests {
 
         // multiple positives and negatives should update aggregation value by appropriate amount
         let rs = c.narrow_one(u, true);
-        assert_eq!(rs.len(), 5); // one - and one + for each group, except 3 which is new
-                                 // group 1 lost 1 and gained 2
+        assert_eq!(rs.len(), 3); // +- for 1, + for 3
+
+        // group 1 lost 0 and gained 1
         assert!(rs.iter().any(|r| if let Record::Negative(ref r) = *r {
+            r[0] == 1.into() && r[1] == 0.into()
+        } else {
+            false
+        }));
+        assert!(rs.iter().any(|r| if let Record::Positive(ref r) = *r {
             r[0] == 1.into() && r[1] == 1.into()
         } else {
             false
         }));
-        assert!(rs.iter().any(|r| if let Record::Positive(ref r) = *r {
-            r[0] == 1.into() && r[1] == 2.into()
-        } else {
-            false
-        }));
-        // group 2 lost 1 and gained 3
-        assert!(rs.iter().any(|r| if let Record::Negative(ref r) = *r {
-            r[0] == 2.into() && r[1] == 1.into()
-        } else {
-            false
-        }));
-        assert!(rs.iter().any(|r| if let Record::Positive(ref r) = *r {
-            r[0] == 2.into() && r[1] == 3.into()
-        } else {
-            false
-        }));
+
+        // group 2 lost 1 and gained 1
+
         // group 3 lost 0 and gained 1
         assert!(rs.iter().any(|r| if let Record::Positive(ref r) = *r {
-            r[0] == 3.into() && r[1] == 1.into()
+            r[0] == 3.into() && r[1] == 0.into()
         } else {
             false
         }));
@@ -309,7 +363,7 @@ mod tests {
 
         let u: Record = vec![1.into(), 1.into(), 2.into()].into();
 
-        // first row for a group should emit +1 for the group (1, 1)
+        // first row filtered, so should emit 0 for the group (1, 2)
         let rs = c.narrow_one(u, true);
         assert_eq!(rs.len(), 1);
         let mut rs = rs.into_iter();
@@ -318,14 +372,14 @@ mod tests {
             Record::Positive(r) => {
                 assert_eq!(r[0], 1.into());
                 assert_eq!(r[1], 2.into());
-                assert_eq!(r[2], 1.into());
+                assert_eq!(r[2], 0.into());
             }
             _ => unreachable!(),
         }
 
-        let u: Record = vec![2.into(), 1.into(), 2.into()].into();
+        let u: Record = vec![2.into(), 2.into(), 2.into()].into();
 
-        // first row for a second group should emit +1 for that new group
+        // first row for a group should emit +1 for that new group
         let rs = c.narrow_one(u, true);
         assert_eq!(rs.len(), 1);
         let mut rs = rs.into_iter();
@@ -339,7 +393,7 @@ mod tests {
             _ => unreachable!(),
         }
 
-        let u: Record = vec![1.into(), 1.into(), 2.into()].into();
+        let u: Record = vec![2.into(), 2.into(), 2.into()].into();
 
         // second row for a group should emit -1 and +2
         let rs = c.narrow_one(u, true);
@@ -348,7 +402,7 @@ mod tests {
 
         match rs.next().unwrap() {
             Record::Negative(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 2.into());
                 assert_eq!(r[2], 1.into());
             }
@@ -356,14 +410,14 @@ mod tests {
         }
         match rs.next().unwrap() {
             Record::Positive(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 2.into());
                 assert_eq!(r[2], 2.into());
             }
             _ => unreachable!(),
         }
 
-        let u = (vec![1.into(), 1.into(), 2.into()], false);
+        let u = (vec![2.into(), 2.into(), 2.into()], false);
 
         // negative row for a group should emit -2 and +1
         let rs = c.narrow_one_row(u, true);
@@ -372,7 +426,7 @@ mod tests {
 
         match rs.next().unwrap() {
             Record::Negative(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 2.into());
                 assert_eq!(r[2], 2.into());
             }
@@ -380,7 +434,7 @@ mod tests {
         }
         match rs.next().unwrap() {
             Record::Positive(r) => {
-                assert_eq!(r[0], 1.into());
+                assert_eq!(r[0], 2.into());
                 assert_eq!(r[1], 2.into());
                 assert_eq!(r[2], 1.into());
             }
