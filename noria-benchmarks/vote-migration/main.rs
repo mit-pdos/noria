0293be30ddaa8d2ae75b68ae6dce4660ab8dca9c
Jon Gjengset <jon@thesquareplanet.com>
2019-09-26 18:14:51 -0400

    Bring forward vote-migration

--- noria-benchmarks/vote-migration/main.rs
+++ noria-benchmarks/vote-migration/main.rs
@@ -1,21 +1,16 @@
 const WRITE_BATCH_SIZE: usize = 1000;
 
-#[macro_use]
-extern crate clap;
-extern crate futures;
-extern crate noria;
-extern crate rand;
-extern crate zipf;
-
 #[path = "../vote/clients/localsoup/graph.rs"]
 mod graph;
 
+use clap::value_t_or_exit;
 use noria::{DataType, TableOperation};
 use rand::{distributions::Distribution, Rng};
 use std::io::prelude::*;
 use std::sync::mpsc;
-use std::sync::{Arc, Barrier};
+use std::sync::Arc;
 use std::{fs, thread, time};
+use tokio::sync::Barrier;
 
 const NANOS_PER_SEC: u64 = 1_000_000_000;
 
@@ -52,7 +47,7 @@ impl Reporter {
     }
 }
 
-fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::File>) {
+async fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches<'_>, w: Option<fs::File>) {
     let narticles = value_t_or_exit!(args, "narticles", usize);
     let runtime = time::Duration::from_secs(value_t_or_exit!(args, "runtime", u64));
     let migrate_after = time::Duration::from_secs(value_t_or_exit!(args, "migrate", u64));
@@ -67,11 +62,11 @@ fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::
 
     // make the graph!
     eprintln!("Setting up soup");
-    let mut g = s.start_sync(persistence_params).unwrap();
+    let mut g = s.start(persistence_params).await.unwrap();
     eprintln!("Getting accessors");
-    let mut articles = g.graph.table("Article").unwrap().into_sync();
-    let mut votes = g.graph.table("Vote").unwrap().into_sync();
-    let mut read_old = g.graph.view("ArticleWithVoteCount").unwrap().into_sync();
+    let mut articles = g.graph.table("Article").await.unwrap();
+    let mut votes = g.graph.table("Vote").await.unwrap();
+    let mut read_old = g.graph.view("ArticleWithVoteCount").await.unwrap();
 
     // prepopulate
     eprintln!("Prepopulating with {} articles", narticles);
@@ -79,20 +74,22 @@ fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::
         .perform_all(
             (0..narticles).map(|i| vec![(i as i32).into(), format!("Article #{}", i + 1).into()]),
         )
+        .await
         .unwrap();
 
     let (stat, stat_rx) = mpsc::channel();
     let barrier = Arc::new(Barrier::new(3));
+    let done = Arc::new(Barrier::new(5));
 
     // start writer that just does a bunch of old writes
     eprintln!("Starting old writer");
-    let w1 = {
+    {
         let stat = stat.clone();
+        let done = done.clone();
         let barrier = barrier.clone();
-        thread::spawn(move || {
-            let mut rng = rand::thread_rng();
+        tokio::spawn(async move {
             let zipf = ZipfDistribution::new(narticles, 1.08).unwrap();
-            barrier.wait();
+            barrier.wait().await;
 
             let mut reporter = Reporter::new(every);
             let start = time::Instant::now();
@@ -100,11 +97,12 @@ fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::
                 votes
                     .perform_all((0..WRITE_BATCH_SIZE).map(|i| {
                         // always generate both so that we aren't artifically faster with one
-                        let id_uniform = rng.gen_range(0, narticles);
-                        let id_zipf = zipf.sample(&mut rng);
+                        let id_uniform = rand::thread_rng().gen_range(0, narticles);
+                        let id_zipf = zipf.sample(&mut rand::thread_rng());
                         let id = if skewed { id_zipf } else { id_uniform };
                         TableOperation::from(vec![DataType::from(id), i.into()])
                     }))
+                    .await
                     .unwrap();
 
                 if let Some((took, count)) = reporter.report(WRITE_BATCH_SIZE) {
@@ -113,30 +111,34 @@ fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::
                     stat.send(("OLD", count_per_s)).unwrap();
                 }
             }
-        })
-    };
+
+            done.wait().await;
+        });
+    }
 
     // start a read that just reads old forever
     eprintln!("Starting old reader");
-    let r1 = {
+    {
+        let done = done.clone();
         let barrier = barrier.clone();
-        thread::spawn(move || {
-            let mut rng = rand::thread_rng();
+        tokio::spawn(async move {
             let zipf = ZipfDistribution::new(narticles, 1.08).unwrap();
-            barrier.wait();
+            barrier.wait().await;
             let start = time::Instant::now();
             while start.elapsed() < runtime {
-                let id_uniform = rng.gen_range(0, narticles);
-                let id_zipf = zipf.sample(&mut rng);
+                let id_uniform = rand::thread_rng().gen_range(0, narticles);
+                let id_zipf = zipf.sample(&mut rand::thread_rng());
                 let id = if skewed { id_zipf } else { id_uniform };
-                read_old.lookup(&[DataType::from(id)], false).unwrap();
-                thread::sleep(time::Duration::new(0, 10_000));
+                read_old.lookup(&[DataType::from(id)], false).await.unwrap();
+                tokio::timer::delay(time::Instant::now() + time::Duration::from_micros(10)).await;
             }
-        })
-    };
+
+            done.wait().await;
+        });
+    }
 
     // wait for other threads to be ready
-    barrier.wait();
+    barrier.wait().await;
     let start = time::Instant::now();
 
     let stats = thread::spawn(move || {
@@ -152,35 +154,36 @@ fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::
 
     // we now need to wait for migrate_after
     eprintln!("Waiting for migration time...");
-    thread::sleep(migrate_after);
+    tokio::timer::delay(time::Instant::now() + migrate_after).await;
 
     // all right, migration time
     eprintln!("Starting migration");
     stat.send(("MIG START", 0.0)).unwrap();
-    g.transition();
+    g.transition().await;
     stat.send(("MIG FINISHED", 0.0)).unwrap();
-    let mut ratings = g.graph.table("Rating").unwrap().into_sync();
-    let mut read_new = g.graph.view("ArticleWithScore").unwrap().into_sync();
+    let mut ratings = g.graph.table("Rating").await.unwrap();
+    let mut read_new = g.graph.view("ArticleWithScore").await.unwrap();
 
     // start writer that just does a bunch of new writes
     eprintln!("Starting new writer");
-    let w2 = {
+    {
+        let done = done.clone();
         let stat = stat.clone();
         let barrier = barrier.clone();
-        thread::spawn(move || {
-            let mut rng = rand::thread_rng();
+        tokio::spawn(async move {
             let zipf = ZipfDistribution::new(narticles, 1.08).unwrap();
-            barrier.wait();
+            barrier.wait().await;
 
             let mut reporter = Reporter::new(every);
             while start.elapsed() < runtime {
                 ratings
                     .perform_all((0..WRITE_BATCH_SIZE).map(|i| {
-                        let id_uniform = rng.gen_range(0, narticles);
-                        let id_zipf = zipf.sample(&mut rng);
+                        let id_uniform = rand::thread_rng().gen_range(0, narticles);
+                        let id_zipf = zipf.sample(&mut rand::thread_rng());
                         let id = if skewed { id_zipf } else { id_uniform };
                         TableOperation::from(vec![DataType::from(id), i.into(), 5.into()])
                     }))
+                    .await
                     .unwrap();
 
                 if let Some((took, count)) = reporter.report(WRITE_BATCH_SIZE) {
@@ -189,30 +192,32 @@ fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::
                     stat.send(("NEW", count_per_s)).unwrap();
                 }
             }
-        })
-    };
+
+            done.wait().await;
+        });
+    }
 
     // start reader that keeps probing new read view
     eprintln!("Starting new read probe");
-    let r2 = {
+    {
         let stat = stat.clone();
+        let done = done.clone();
         let barrier = barrier.clone();
-        thread::spawn(move || {
+        tokio::spawn(async move {
             let n = 100;
             let mut hits = 0;
-            let mut rng = rand::thread_rng();
             let zipf = ZipfDistribution::new(narticles, 1.08).unwrap();
             let mut reporter = Reporter::new(every);
-            barrier.wait();
+            barrier.wait().await;
             while start.elapsed() < runtime {
                 let ids = (0..n)
                     .map(|_| {
-                        let id_uniform = rng.gen_range(0, narticles);
-                        let id_zipf = zipf.sample(&mut rng);
+                        let id_uniform = rand::thread_rng().gen_range(0, narticles);
+                        let id_zipf = zipf.sample(&mut rand::thread_rng());
                         vec![DataType::from(if skewed { id_zipf } else { id_uniform })]
                     })
                     .collect();
-                if let Ok(rss) = read_new.multi_lookup(ids, false) {
+                if let Ok(rss) = read_new.multi_lookup(ids, false).await {
                     hits += rss.into_iter().filter(|rs| !rs.is_empty()).count();
                 } else {
                     // miss, or view not yet ready
@@ -222,27 +227,27 @@ fn one(s: &graph::Builder, skewed: bool, args: &clap::ArgMatches, w: Option<fs::
                     stat.send(("HITF", hits as f64 / count as f64)).unwrap();
                     hits = 0;
                 }
-                thread::sleep(time::Duration::new(0, 10_000_000));
+                tokio::timer::delay(time::Instant::now() + time::Duration::from_millis(10)).await;
             }
-        })
-    };
+
+            done.wait().await;
+        });
+    }
 
     // fire them both off!
-    barrier.wait();
+    barrier.wait().await;
 
     // everything finishes!
     eprintln!("Waiting for experiment to end...");
-    w1.join().unwrap();
-    w2.join().unwrap();
-    r1.join().unwrap();
-    r2.join().unwrap();
+    done.wait().await;
 
     stat.send(("FIN", 0.0)).unwrap();
     drop(stat);
     stats.join().unwrap();
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     use clap::{App, Arg};
 
     let args = App::new("vote")
@@ -345,7 +350,8 @@ fn main() {
                 fs::File::create(format!("vote-no-partial-stupid-{}M.zipf1.08.log", mills))
                     .unwrap(),
             ),
-        );
+        )
+        .await;
         eprintln!("==> partial with reuse (uniform)");
         s.partial = true;
         s.stupid = false;
@@ -354,7 +360,8 @@ fn main() {
             false,
             &args,
             Some(fs::File::create(format!("vote-partial-reuse-{}M.uniform.log", mills)).unwrap()),
-        );
+        )
+        .await;
         eprintln!("==> partial with reuse (zipf)");
         s.partial = true;
         s.stupid = false;
@@ -363,7 +370,8 @@ fn main() {
             true,
             &args,
             Some(fs::File::create(format!("vote-partial-reuse-{}M.zipf1.08.log", mills)).unwrap()),
-        );
+        )
+        .await;
 
         if !args.is_present("all") {
             return;
@@ -380,7 +388,8 @@ fn main() {
             Some(
                 fs::File::create(format!("vote-no-partial-stupid-{}M.uniform.log", mills)).unwrap(),
             ),
-        );
+        )
+        .await;
         eprintln!("==> full with reuse (uniform)");
         s.partial = false;
         s.stupid = false;
@@ -391,7 +400,8 @@ fn main() {
             Some(
                 fs::File::create(format!("vote-no-partial-reuse-{}M.uniform.log", mills)).unwrap(),
             ),
-        );
+        )
+        .await;
         eprintln!("==> full with reuse (zipf)");
         s.partial = false;
         s.stupid = false;
@@ -402,7 +412,8 @@ fn main() {
             Some(
                 fs::File::create(format!("vote-no-partial-reuse-{}M.zipf1.08.log", mills)).unwrap(),
             ),
-        );
+        )
+        .await;
         eprintln!("==> partial no reuse (uniform)");
         s.partial = true;
         s.stupid = true;
@@ -411,7 +422,8 @@ fn main() {
             false,
             &args,
             Some(fs::File::create(format!("vote-partial-stupid-{}M.uniform.log", mills)).unwrap()),
-        );
+        )
+        .await;
         eprintln!("==> partial no reuse (zipf)");
         s.partial = true;
         s.stupid = true;
@@ -420,11 +432,12 @@ fn main() {
             true,
             &args,
             Some(fs::File::create(format!("vote-partial-stupid-{}M.zipf1.08.log", mills)).unwrap()),
-        );
+        )
+        .await;
     } else {
         let skewed = args.is_present("skewed");
         s.partial = !args.is_present("full");
         s.stupid = args.is_present("stupid");
-        one(&s, skewed, &args, None);
+        one(&s, skewed, &args, None).await;
     }
 }
