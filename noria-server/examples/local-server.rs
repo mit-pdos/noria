5257fa05298f433fef0e351e632291e9fa507eed
Jon Gjengset <jon@thesquareplanet.com>
2019-09-26 18:53:49 -0400

    Bring forward local-server example

--- noria-server/examples/local-server.rs
+++ noria-server/examples/local-server.rs
@@ -1,12 +1,8 @@
-extern crate futures;
-extern crate noria_server;
-
 use noria_server::Builder;
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
-use std::thread;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
-
-fn main() {
+#[tokio::main]
+async fn main() {
     // inline recipe definition
     let sql = "# base tables
                CREATE TABLE Article (aid int, title varchar(255), \
@@ -35,24 +31,25 @@ fn main() {
     builder.log_with(noria_server::logger_pls());
     builder.set_persistence(persistence_params);
 
-    let mut blender = builder.start_simple().unwrap();
-    blender.install_recipe(sql).unwrap();
-    println!("{}", blender.graphviz().unwrap());
+    let mut blender = builder.start_local().await.unwrap();
+    blender.install_recipe(sql).await.unwrap();
+    println!("{}", blender.graphviz().await.unwrap());
 
     // Get mutators and getter.
-    let mut article = blender.table("Article").unwrap().into_sync();
-    let mut vote = blender.table("Vote").unwrap().into_sync();
-    let mut awvc = blender.view("ArticleWithVoteCount").unwrap().into_sync();
+    let mut article = blender.table("Article").await.unwrap();
+    let mut vote = blender.table("Vote").await.unwrap();
+    let mut awvc = blender.view("ArticleWithVoteCount").await.unwrap();
 
     println!("Creating article...");
     let aid = 1;
     // Make sure the article exists:
-    if awvc.lookup(&[aid.into()], true).unwrap().is_empty() {
+    if awvc.lookup(&[aid.into()], true).await.unwrap().is_empty() {
         println!("Creating new article...");
         let title = "test title";
         let url = "http://pdos.csail.mit.edu";
         article
             .insert(vec![aid.into(), title.into(), url.into()])
+            .await
             .unwrap();
     }
 
@@ -64,12 +61,12 @@ fn main() {
         .as_secs() as i64;
 
     // Double-voting has no effect on final count due to DISTINCT
-    vote.insert(vec![aid.into(), uid.into()]).unwrap();
-    vote.insert(vec![aid.into(), uid.into()]).unwrap();
+    vote.insert(vec![aid.into(), uid.into()]).await.unwrap();
+    vote.insert(vec![aid.into(), uid.into()]).await.unwrap();
 
     println!("Finished writing! Let's wait for things to propagate...");
-    thread::sleep(Duration::from_millis(1000));
+    tokio::timer::delay(Instant::now() + Duration::from_secs(1)).await;
 
     println!("Reading...");
-    println!("{:#?}", awvc.lookup(&[aid.into()], true))
+    println!("{:#?}", awvc.lookup(&[aid.into()], true).await)
 }
