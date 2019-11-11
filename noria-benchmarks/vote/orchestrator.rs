6b06c658f1c055c7f8617b395d7064e25b168a9b
Jon Gjengset <jon@thesquareplanet.com>
2019-11-11 12:00:02 -0500

    Updated noria AMI

--- noria-benchmarks/vote/orchestrator.rs
+++ noria-benchmarks/vote/orchestrator.rs
@@ -17,7 +17,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::{io, thread, time};
 
-const SOUP_AMI: &str = "ami-0fe49768bcb2d68f4";
+const SOUP_AMI: &str = "ami-0e93c3b2927b16d34";
 
 #[derive(Clone, Copy)]
 struct ClientParameters<'a> {
