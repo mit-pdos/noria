cb6581b0958f7d75878ed6f8ef01794bde8633f6
Jon Gjengset <jon@thesquareplanet.com>
2020-04-16 14:50:31 -0400

    Let dup take advantage of iterators
    Super high frequency loop, and collect should be more efficient than
    individual pushes, especially for `ExactSizeIterator`.

--- server/src/worker/readers.rs
+++ server/src/worker/readers.rs
@@ -104,17 +104,31 @@ pub(super) async fn listen(
     }
 }
 
-fn dup<'a>(rs: impl IntoIterator<Item = &'a Vec<DataType>>) -> Vec<Vec<DataType>> {
-    let rs = rs.into_iter();
-    let mut outer = Vec::with_capacity(rs.size_hint().0);
-    for r in rs {
-        let mut inner = Vec::with_capacity(r.len());
-        for v in r {
-            inner.push(v.deep_clone())
-        }
-        outer.push(inner);
-    }
-    outer
+// I _really_ wish we didn't have to do this.
+// I think there exists an allocation-free fast path when you don't miss on anything: in that case
+// you can just serialize the references directly into the response channel while still pinning the
+// evmap handle. That is pretty complicated to achieve without punching through several layers of
+// stack though, so we use this for the time being.
+//
+// NOTE: the bounds here aren't necessary, but we want to make sure that this is as fast as we can
+// possibly make it, so we enforce that as much as we can through the types.
+fn dup<'a, I>(rs: I) -> Vec<Vec<DataType>>
+where
+    I: IntoIterator<Item = &'a Vec<DataType>>,
+    I::IntoIter: ExactSizeIterator,
+{
+    rs.into_iter()
+        .map(|r| {
+            // NOTE: this _should_ pre-allocate the Vec since r: ExactSizeIterator
+            r.into_iter()
+                .map(|v| {
+                    // NOTE: we deep clone here to avoid contending with other threads on the ref
+                    // count in any Arc<String> that may be contained within v.
+                    v.deep_clone()
+                })
+                .collect()
+        })
+        .collect()
 }
 
 fn handle_message(
